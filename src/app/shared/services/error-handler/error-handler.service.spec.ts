7600df4fb88271a288a0c3f89f4ae9432ab77e11
Martin317 <martinbrunori316@gmail.com>
2019-08-09 17:38:05 -0300

    AM-121 / Solo para el modulo funds

---
+++ src/app/shared/services/error-handler/error-handler.service.spec.ts
@@ -0,0 +1,37 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ErrorHandlerService } from './error-handler.service';
+import { ToastService } from '../toast/toast.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { HttpErrorResponse } from '@angular/common/http';
+
+describe('ErrorHandlerService', () => {
+  let toastServiceSpy: any;
+  let service: ErrorHandlerService;
+  let toastService: ToastService;
+  beforeEach(() => {
+    toastServiceSpy = jasmine.createSpyObj('ToastService', ['showToast']);
+    TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot()],
+      providers: [{ provide: ToastService, useValue: toastServiceSpy }]
+    });
+    service = TestBed.get(ErrorHandlerService);
+    toastService = TestBed.get(ToastService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should call showToast ToastService from error, status null', () => {
+    const response = new HttpErrorResponse({});
+    service.handle(response);
+    expect(toastService.showToast).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call showToast ToastService from error, status 401', () => {
+    const response = new HttpErrorResponse({ status: 401, error: 'error' });
+    service.handle(response);
+    expect(toastService.showToast).toHaveBeenCalledTimes(1);
+  });
+});
--- src/app/shared/services/xhr-response-handler/xhr-response-handler.service.spec.ts
+++ src/app/shared/services/xhr-response-handler/xhr-response-handler.service.spec.ts
@@ -1,41 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-
 import { XhrResponseHandlerService } from './xhr-response-handler.service';
-import { ToastService } from '../toast/toast.service';
-import { HttpErrorResponse } from '@angular/common/http';
 import { TranslateModule } from '@ngx-translate/core';
 
 describe('XhrResponseHandlerService', () => {
-  let toastServiceSpy: any;
   let service: XhrResponseHandlerService;
-  let toastService: ToastService;
   beforeEach(() => {
-    toastServiceSpy = jasmine.createSpyObj('ToastService', ['showToast']);
-    TestBed.configureTestingModule({
-      imports: [
-        TranslateModule.forRoot()
-      ],
-      providers: [
-        { provide: ToastService, useValue: toastServiceSpy }
-      ]
+    TestBed.configureTestingModule({ 
+      imports: [TranslateModule.forRoot()]
     });
     service = TestBed.get(XhrResponseHandlerService);
-    toastService = TestBed.get(ToastService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-  });
-
-  it('should call showToast ToastService from error, status null', () => {
-    const response = new HttpErrorResponse({});
-    service.error()(response);
-    expect(toastService.showToast).toHaveBeenCalledTimes(1);
-  });
-
-  it('should call showToast ToastService from error, status 401', () => {
-    const response = new HttpErrorResponse({ status: 401, error: 'error' });
-    service.error()(response);
-    expect(toastService.showToast).toHaveBeenCalledTimes(1);
   });
 });
