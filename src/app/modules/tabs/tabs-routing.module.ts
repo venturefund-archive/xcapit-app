ae54cb0b0cd213f2be1b4cc2328c94f2bf908393
Maximiliano Cesar Nivoli <mnivoli@wallcryptostreet.com>
2019-12-04 15:56:44 -0300

    AM-256/AM-257, fix route guard

--- src/app/modules/tabs/tabs-routing.module.ts
+++ src/app/modules/tabs/tabs-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TabsComponent } from './tabs/tabs.component';
+import { AuthGuard } from '../usuarios/shared-usuarios/guards/auth/auth.guard';
 
 
 const routes: Routes = [
   {
     path: 'tabs',
     component: TabsComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: 'funds',
