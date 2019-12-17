04c1d9c29de2f2bb420725c4f53b401d3a3ee31f
Martin317 <martinbrunori316@gmail.com>
2019-12-17 10:19:32 -0300

    Cambios login e xcapit-input label

--- src/app/modules/usuarios/login/login.page.ts
+++ src/app/modules/usuarios/login/login.page.ts
@@ -21,63 +21,66 @@ import { SubscriptionsService } from '../../subscriptions/shared-subscriptions/s
     </div>
     <ion-content class="ion-padding">
       <div class="main">
-        <ion-grid class="ion-no-padding">
-          <ion-row>
-            <ion-col
-              size-sm="8"
-              offset-sm="2"
-              size-md="6"
-              offset-md="3"
-              size-lg="4"
-              offset-lg="4"
-            >
-              <app-auth-form [isLogin]="true" (send)="this.loginUser($event)">
-                <div class="auth-button ion-padding-top ion-margin-top">
-                  <ion-button
-                    appTrackClickUnauth
-                    name="Login"
-                    expand="block"
-                    size="large"
-                    type="submit"
-                    class="main__login_button"
-                    color="xcprimary"
-                    [disabled]="
-                      !this.loginForm.form.valid ||
-                      (this.submitButtonService.isDisabled | async)
-                    "
-                  >
-                    {{ 'usuarios.login.login_button_text' | translate }}
-                  </ion-button>
-                </div>
-                <div class="auth-link ion-text-center ion-padding-top">
-                  <ion-button
-                    appTrackClickUnauth
-                    name="Go To Register"
-                    fill="clear"
-                    size="small"
-                    type="button"
-                    color="xcprimary"
-                    [routerLink]="['/users/register']"
-                  >
-                    {{ 'usuarios.login.register_link' | translate }}
-                  </ion-button>
-                  <ion-button
-                    appTrackClickUnauth
-                    name="Reset Password"
-                    fill="clear"
-                    size="small"
-                    type="button"
-                    color="xcprimary"
-                    [routerLink]="['/users/reset-password']"
-                  >
-                    {{ 'usuarios.login.reset_password_link' | translate }}
-                  </ion-button>
-                </div>
-              </app-auth-form>
-            </ion-col>
-          </ion-row>
-        </ion-grid>
-      </div>
+
+          <ion-grid class="ion-no-padding">
+            <ion-row>
+              <ion-col
+                size-xs="10"
+                offset-xs="1"
+                size-sm="8"
+                offset-sm="2"
+                size-md="6"
+                offset-md="3"
+                size-lg="4"
+                offset-lg="4"
+              >
+                <app-auth-form [isLogin]="true" (send)="this.loginUser($event)">
+                  <div class="auth-button ion-padding-top ion-margin-top">
+                    <ion-button
+                      appTrackClickUnauth
+                      name="Login"
+                      expand="block"
+                      size="large"
+                      type="submit"
+                      class="main__login_button"
+                      color="xcprimary"
+                      [disabled]="
+                        !this.loginForm.form.valid ||
+                        (this.submitButtonService.isDisabled | async)
+                      "
+                    >
+                      {{ 'usuarios.login.login_button_text' | translate }}
+                    </ion-button>
+                  </div>
+                  <div class="auth-link ion-text-center ion-padding-top">
+                    <ion-button
+                      appTrackClickUnauth
+                      name="Go To Register"
+                      fill="clear"
+                      size="small"
+                      type="button"
+                      color="xcprimary"
+                      [routerLink]="['/users/register']"
+                    >
+                      {{ 'usuarios.login.register_link' | translate }}
+                    </ion-button>
+                    <ion-button
+                      appTrackClickUnauth
+                      name="Reset Password"
+                      fill="clear"
+                      size="small"
+                      type="button"
+                      color="xcprimary"
+                      [routerLink]="['/users/reset-password']"
+                    >
+                      {{ 'usuarios.login.reset_password_link' | translate }}
+                    </ion-button>
+                  </div>
+                </app-auth-form>
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+        </div>
     </ion-content>
   `,
   styleUrls: ['./login.page.scss']
