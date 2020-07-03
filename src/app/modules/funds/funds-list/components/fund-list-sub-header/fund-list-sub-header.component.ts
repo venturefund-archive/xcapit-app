d017315642302f627af1c44ad050c6cfdc057dcc
Martin Brunori <martinbrunori@MacBook-Pro.local>
2020-07-03 14:39:05 -0300

    :bug: fix: no data to show

--- src/app/modules/funds/funds-list/components/fund-list-sub-header/fund-list-sub-header.component.ts
+++ src/app/modules/funds/funds-list/components/fund-list-sub-header/fund-list-sub-header.component.ts
@@ -8,69 +8,69 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-fund-list-sub-header',
   template: `
-    <div class="fl__total">
-      <div class="ux-font-lato ux-fweight-regular ux-fsize-14">
-        <ion-text>
-          {{ 'funds.funds_list.sub_header.total_title' | translate }}
-        </ion-text>
+      <div class="fl__total">
+          <div class="ux-font-lato ux-fweight-regular ux-fsize-14">
+              <ion-text>
+                  {{ 'funds.funds_list.sub_header.total_title' | translate }}
+              </ion-text>
+          </div>
+          <div *ngIf="totalBalance?.start_balance === 0"
+               class="fl__total__amount ux-font-lato ux-fweight-regular ux-fsize-14">
+              <ion-text>
+                  {{ 'funds.funds_list.sub_header.not_balance_found' | translate }}
+              </ion-text>
+          </div>
+          <div *ngIf="totalBalance?.start_balance != 0"
+               class="fl__total__amount ux-font-gilroy ux-fweight-extrabold ux-fsize-40"
+          >
+              <app-ux-loading-block
+                      *ngIf="!totalBalance"
+                      minSize="25px"
+              ></app-ux-loading-block>
+              <ion-text>
+                  {{ this.totalBalance?.total_balance | number: '1.2-6' }}
+                  {{ this.totalBalance?.currency }}
+              </ion-text>
+              <ion-button
+                      appTrackClick
+                      name="Change Total Currency"
+                      (click)="this.changeCurrency()"
+                      fill="clear"
+                      size="small"
+                      class="fl__total__amount__edit"
+                      [disabled]="!totalBalance"
+              >
+                  <ion-icon
+                          slot="icon-only"
+                          name="ux-pencil"
+                          color="uxlight"
+                  ></ion-icon>
+              </ion-button>
+          </div>
+          <div class="fl__total__detail" *ngIf="totalBalance?.start_balance != 0">
+              <ion-badge class="ux-font-gilroy ux-fweight-extrabold ux-fsize-10">
+                  <ion-icon
+                          name="ux-triangle-up"
+                          *ngIf="this.totalBalance?.total_profit > 0"
+                  ></ion-icon>
+                  <ion-icon
+                          name="ux-triangle-down"
+                          *ngIf="this.totalBalance?.total_profit < 0"
+                  ></ion-icon>
+                  {{
+                  this.totalBalance?.total_profit | absoluteValue | number: '1.2-2'
+                  }}
+                  %
+              </ion-badge>
+              <div
+                      class="fl__total__detail__text ux-font-lato ux-fweight-regular ux-fsize-12"
+              >
+                  <ion-text>
+                      {{ 'funds.funds_list.sub_header.total_detail' | translate }}
+                  </ion-text>
+              </div>
+          </div>
       </div>
-      <div *ngIf="totalBalance?.start_balance === 0"
-           class="fl__total__amount ux-font-lato ux-fweight-regular ux-fsize-14">
-          <ion-text>
-              {{ 'funds.funds_list.sub_header.not_balance_found' | translate }}
-          </ion-text>
-      </div>
-      <div *ngIf="totalBalance?.start_balance != 0"
-           class="fl__total__amount ux-font-gilroy ux-fweight-extrabold ux-fsize-40"
-      >
-        <app-ux-loading-block
-          *ngIf="!totalBalance"
-          minSize="25px"
-        ></app-ux-loading-block>
-        <ion-text>
-          {{ this.totalBalance?.total_balance | number: '1.2-6' }}
-          {{ this.totalBalance?.currency }}
-        </ion-text>
-        <ion-button
-          appTrackClick
-          name="Change Total Currency"
-          (click)="this.changeCurrency()"
-          fill="clear"
-          size="small"
-          class="fl__total__amount__edit"
-          [disabled]="!totalBalance"
-        >
-          <ion-icon
-            slot="icon-only"
-            name="ux-pencil"
-            color="uxlight"
-          ></ion-icon>
-        </ion-button>
-      </div>
-      <div class="fl__total__detail" *ngIf="totalBalance?.start_balance != 0">
-        <ion-badge class="ux-font-gilroy ux-fweight-extrabold ux-fsize-10">
-          <ion-icon
-            name="ux-triangle-up"
-            *ngIf="this.totalBalance?.total_profit > 0"
-          ></ion-icon>
-          <ion-icon
-            name="ux-triangle-down"
-            *ngIf="this.totalBalance?.total_profit < 0"
-          ></ion-icon>
-          {{
-            this.totalBalance?.total_profit | absoluteValue | number: '1.2-2'
-          }}
-          %
-        </ion-badge>
-        <div
-          class="fl__total__detail__text ux-font-lato ux-fweight-regular ux-fsize-12"
-        >
-          <ion-text>
-            {{ 'funds.funds_list.sub_header.total_detail' | translate }}
-          </ion-text>
-        </div>
-      </div>
-    </div>
   `,
   styleUrls: ['./fund-list-sub-header.component.scss']
 })
@@ -82,7 +82,8 @@ export class FundListSubHeaderComponent implements OnInit {
     private apiFunds: ApiFundsService,
     private modalController: ModalController,
     private translate: TranslateService
-  ) {}
+  ) {
+  }
 
   ngOnInit() {
     this.getTotalBalance('BTC');
