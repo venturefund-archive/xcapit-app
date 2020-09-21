660a5b4cba9da76bda1aa5ed320bfca4e05e5e5c
Martin Brunori <martinbrunori@MacBook-Pro.local>
2020-09-21 20:05:38 -0300

    :wrench: chore: New env vars file without docker

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,6 @@
 FROM node:14-alpine as builder
-ARG ENVIRONMENT
-ARG PRODUCTION
-ARG API_URL
 WORKDIR /usr/src/app
 COPY . .
-ENV ENVIRONMENT=$ENVIRONMENT
-ENV PRODUCTION=$PRODUCTION
-ENV API_URL=$API_URL
 ## Install build toolchain, install node deps and compile native add-ons
 RUN npm install
 RUN npm run build:prod:pwa:xcapit
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
-  production: $ENV.PRODUCTION,
-  environment: $ENV.ENVIRONMENT,
-  apiUrl: $ENV.API_URL,
-  whitelistedDomains: ['preprod.xcapit.com', 'xcapit.com', 'app.xcapit.com', 'app.xcapit.com:443', 'xcapit.com:443', 'preprod.xcapit.com:443'],
-  firebase: {
+   apiUrl: 'https://localhost:9050/v1/api',
+   production: true,
+   environment: 'PREPROD',
+   whitelistedDomains: ['preprod.xcapit.com', 'xcapit.com', 'app.xcapit.com', 'app.xcapit.com:443', 'xcapit.com:443', 'preprod.xcapit.com:443'],
+   firebase: {
     apiKey: 'AIzaSyCU-F8osRaeWGwTxdAJmDhWFfkjZqzUG7s',
     authDomain: 'test-pwa-2019.firebaseapp.com',
     databaseURL: 'https://test-pwa-2019.firebaseio.com',
@@ -11,7 +11,6 @@ export const environment = {
     storageBucket: 'test-pwa-2019.appspot.com',
     messagingSenderId: '1059796815977',
     appId: '1:1059796815977:web:00d4a1e1de77f5eb8592f8',
-    vapidKey:
-      'BBQTZJVBY9gH70xupGlC91sfxy-BvpKwlHkE0Gk-XN12NrPmirbm5Hf7FS-r65XnRRHNzQekOcLRfRlvMq9maEA'
+    vapidKey: 'BBQTZJVBY9gH70xupGlC91sfxy-BvpKwlHkE0Gk-XN12NrPmirbm5Hf7FS-r65XnRRHNzQekOcLRfRlvMq9maEA'
   }
 };
