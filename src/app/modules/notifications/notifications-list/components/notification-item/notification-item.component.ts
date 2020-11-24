48c85200900b55bbaddfc7b23fdc26f12f2ab18b
elnegro <elnegro@MacBook-Pro-de-Macbook.local>
2020-11-23 23:48:22 -0300

    First commit

--- src/app/modules/notifications/notifications-list/components/notification-item/notification-item.component.ts
+++ src/app/modules/notifications/notifications-list/components/notification-item/notification-item.component.ts
@@ -1,27 +1,32 @@
 import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-notification-item',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <ion-item-sliding>
-      <ion-item button (click)="this.notificationClicked()">
-        <ion-label>
-          <h2>{{ this.notification | notification: 'title' }}</h2>
-          <p>{{ this.notification | notification: 'body' }}</p>
-        </ion-label>
-        <ion-note slot="end" class="ion-text-center">
-          {{ this.notification?.internalDate.toLocaleDateString() }}
-          <br />
-          {{ this.notification?.internalDate.toLocaleTimeString() }}
-        </ion-note>
+    <div class="ni">
+      <ion-item class="ni__body" [ngClass]="{ni__new: !this.notification.read}">
+        <div class="ni__body__icon">
+          
+        </div>
+          
+        <div style="width: 100%;">
+          <ion-label>
+            <div class="ni__body__title">{{ this.notification.title }}</div>
+            <div class="ni__body__message">{{ this.notification.message }}</div>
+          </ion-label>
+        </div>
+        <div class="ni__body__date">
+            {{ this.createdTime }}
+        </div>
       </ion-item>
-      <ion-item-options side="end">
+      <!--ion-item-options side="end">
         <ion-item-option color="danger" expandable (click)="this.remove()">
           <ion-icon slot="icon-only" name="trash"></ion-icon>
         </ion-item-option>
-      </ion-item-options>
-    </ion-item-sliding>
+      </ion-item-options-->
+    </div>
   `,
   styleUrls: ['./notification-item.component.scss']
 })
@@ -35,11 +40,32 @@ export class NotificationItemComponent {
   @Output()
   clickNotification = new EventEmitter<any>();
 
+  createdTime: any;
+
+  ngOnInit() {
+    this.createdTime = this.getCreatedTime(this.notification.created);
+  }
+
   remove() {
     this.clickRemove.emit(this.notification.id);
   }
 
   notificationClicked() {
     this.clickNotification.emit(this.notification.id);
+  }
+
+  getCreatedTime(date) {
+    const a = moment(date);
+    const b = moment(new Date());
+    
+    if (b.diff(a, 'days') > 0) {
+      return b.diff(a, 'days') + 'd';
+    } else if (b.diff(a, 'hours') > 0) {
+      return b.diff(a, 'hours') + 'h';
+    } else if (b.diff(a, 'minutes') > 0){
+      return b.diff(a, 'minutes') + 'm';
+    } else {
+      return b.diff(a, 'seconds') + 's';
+    }
   }
 }
--- src/app/modules/notifications/notifications-list/notifications-list.page.spec.ts
+++ src/app/modules/notifications/notifications-list/notifications-list.page.spec.ts
@@ -4,17 +4,25 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NotificationsListPage } from './notifications-list.page';
 import { TranslateModule } from '@ngx-translate/core';
 import { NotificationsStorageService } from '../shared-notifications/services/notifications-storage/notifications-storage.service';
+import { NotificationsService } from '../shared-notifications/services/notifications/notifications.service';
 import { NavController } from '@ionic/angular';
-import { ReplaySubject } from 'rxjs';
+import { of, ReplaySubject } from 'rxjs';
 import { navControllerMock } from '../../../../testing/spies/nav-controller-mock.spec';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('NotificationsListPage', () => {
   let component: NotificationsListPage;
   let fixture: ComponentFixture<NotificationsListPage>;
   let notificationsStorageSpy: any;
+  let notificationsService: NotificationsService;
+  let notificationsServiceMock: any;
   let navControllerSpy: any;
 
   beforeEach(async(() => {
+    notificationsServiceMock = {
+      getNotifications: () => of([]),
+      markAsRead: () => of(null)
+    };
     notificationsStorageSpy = jasmine.createSpyObj(
       'NotificationsStorageService',
       ['remove']
@@ -23,7 +31,10 @@ describe('NotificationsListPage', () => {
     navControllerSpy = jasmine.createSpyObj('NavController', navControllerMock);
     TestBed.configureTestingModule({
       declarations: [NotificationsListPage],
-      imports: [TranslateModule.forRoot()],
+      imports: [
+        TranslateModule.forRoot(),
+        HttpClientTestingModule,
+      ],
       providers: [
         {
           provide: NotificationsStorageService,
@@ -32,6 +43,10 @@ describe('NotificationsListPage', () => {
         {
           provide: NavController,
           useValue: navControllerSpy
+        },
+        {
+          provide: NotificationsService,
+          useValue: notificationsServiceMock
         }
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
@@ -42,6 +57,7 @@ describe('NotificationsListPage', () => {
     fixture = TestBed.createComponent(NotificationsListPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    notificationsService = TestBed.inject(NotificationsService);
   });
 
   it('should create', () => {
@@ -51,6 +67,13 @@ describe('NotificationsListPage', () => {
   it('should set notifications$ on ngOnInit', () => {
     component.ngOnInit();
     expect(component.notifications$).toBeTruthy();
+  });
+
+  it('should call getNotifications on ngOnInit', () => {
+    const spy = spyOn(notificationsService, 'getNotifications');
+    spy.and.returnValue(of([]));
+    component.ngOnInit();
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 
   it('should call remove on notificationsStorageService when removeNotification is called', () => {
@@ -70,4 +93,11 @@ describe('NotificationsListPage', () => {
       `/notifications/view/${notificationId}`
     ]);
   });
+
+  it('should call markAsRead on ionViewWillLeave', () => {
+    const spy = spyOn(notificationsService, 'markAsRead');
+    spy.and.returnValue(of(null));
+    component.ionViewWillLeave();
+    expect(spy).toHaveBeenCalledTimes(1);
+  })
 });
