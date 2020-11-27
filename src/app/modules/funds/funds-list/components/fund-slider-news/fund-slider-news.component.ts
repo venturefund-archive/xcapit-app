aa977650a8649b160c76264bddda8ff7782d0d28
Franco Schillage <franco@xcapit.com>
2020-11-27 16:29:55 +0000

    :sparkles: feat: SX-501 - App inicio: Noticias

---
+++ src/app/modules/funds/funds-list/components/fund-slider-news/fund-slider-news.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+
+const { Browser } = Plugins;
+
+@Component({
+  selector: 'app-fund-slider-news',
+  template: `
+    <ion-slides pager="false" [options]="slideOpts">
+      <ion-slide class="fsn" *ngFor="let article of news" (click)="this.goToWeb(article.slug)">
+        <div class="fsn__main">
+          <img src="{{ article.image.url }}" alt="" />
+          <div class="fsn__main__info">
+            <div>
+              <ion-label
+                color="uxdark"
+                class="ux-font-gilroy ux-fweight-bold ux-fsize-16"
+                >{{ article.name }}</ion-label
+              >
+            </div>
+            <div>
+              <ion-label
+                color="uxregular"
+                class="ux-font-lato ux-fweight-semibold ux-fsize-12"
+                >{{ article.summary }}</ion-label
+              >
+            </div>
+          </div>
+        </div>
+      </ion-slide>
+    </ion-slides>
+  `,
+  styleUrls: ['./fund-slider-news.component.scss'],
+})
+export class FundSliderNewsCardComponent implements OnInit {
+  @Input() news: any;
+  slideOpts = {
+    initialSlide: 0,
+  };
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+  async goToWeb(slug){
+    await Browser.open({ toolbarColor:"red", url: `https://www.xcapit.com/xcapit-academy/${slug}` });
+  }
+}
--- src/app/modules/funds/funds-list/funds-list.module.ts
+++ src/app/modules/funds/funds-list/funds-list.module.ts
@@ -4,20 +4,21 @@ import { Routes, RouterModule } from '@angular/router';
 import { FundsListPage } from './funds-list.page';
 import { SharedFundsModule } from '../shared-funds/shared-funds.module';
 import { FundListSubHeaderComponent } from './components/fund-list-sub-header/fund-list-sub-header.component';
-
+import { FundSliderNewsCardComponent } from './components/fund-slider-news/fund-slider-news.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: FundsListPage
-  }
+    component: FundsListPage,
+  },
 ];
 
 @NgModule({
-  imports: [
-    SharedFundsModule,
-    RouterModule.forChild(routes)
+  imports: [SharedFundsModule, RouterModule.forChild(routes)],
+  declarations: [
+    FundsListPage,
+    FundListSubHeaderComponent,
+    FundSliderNewsCardComponent,
   ],
-  declarations: [FundsListPage, FundListSubHeaderComponent]
 })
 export class FundsListPageModule {}
