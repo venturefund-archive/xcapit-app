29240f50b463554e3bccf496dd7aeda4a53fab6f
diegomeineri <diegomeineri1995@gmail.com>
2021-03-09 17:12:46 -0300

    :sparkles: feat (apikeys): add no-apikeys template in manage-apikeys

--- src/app/modules/apikeys/manage-apikeys/manage-apikeys.page.ts
+++ src/app/modules/apikeys/manage-apikeys/manage-apikeys.page.ts
@@ -11,21 +11,35 @@ import { ApiApikeysService } from '../shared-apikeys/services/api-apikeys/api-ap
         <ion-buttons slot="start">
           <ion-back-button defaultHref="/tabs/funds"></ion-back-button>
         </ion-buttons>
-        <ion-button color="uxprimary" slot="end">
-          <ion-icon defaultHref="/apikeys/register" name="add-sharp"></ion-icon>
-        </ion-button>
+        <ion-buttons [routerLink]="['/apikeys/register']" slot="end" class="nr__add-button" >
+          <ion-icon class="icon"style="zoom:6.0;" name="add-sharp"></ion-icon>
+        </ion-buttons>
         <ion-title class="ion-text-center">{{
-          'apikeys.apikeys_list.header' | translate
+          'apikeys.manage_apikeys.header' | translate
         }}</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content class="ion-padding">
-    </ion-content>
+      <ion-content class="ion-padding-top">
+        <div class="nr__image-container">
+          <img
+            class="nr__image-container__image"
+            src="assets/img/apikeys/no-apikey.svg"
+            alt="no-apikey"
+          />
+        </div>
+        <div class="nr__subtitle ion-padding-start ion-padding-end">
+          <ion-text
+            class="ux-font-lato ux-fweight-regular ux-fsize-15"
+            color="uxsemidark"
+          >
+            {{ 'apikeys.manage_apikeys.subtitle' | translate }}
+          </ion-text>
+        </div>
+      </ion-content>
   `,
   styleUrls: ['./manage-apikeys.page.scss'],
 })
 export class ManageApikeysPage implements OnInit {
-
   apikeys: any;
 
   constructor(
