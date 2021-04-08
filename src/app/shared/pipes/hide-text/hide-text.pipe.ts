cb14a52226319e97c85fe82051f94449b0ee67c4
diegomeineri <diegomeineri1995@gmail.com>
2021-04-08 14:23:59 -0300

    :sparkles: feat (funds): add pipe

--- src/app/shared/pipes/hide-text/hide-text.pipe.ts
+++ src/app/shared/pipes/hide-text/hide-text.pipe.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Pipe, PipeTransform } from '@angular/core';
 import { LocalStorageService } from '../../services/local-storage/local-storage.service';
 
@@ -6,9 +7,14 @@ import { LocalStorageService } from '../../services/local-storage/local-storage.
   pure: false,
 })
 export class HideTextPipe implements PipeTransform {
-  public activated: boolean = false;
+  public activated: boolean ;
 
-  constructor(private localStorageService: LocalStorageService) {}
+  constructor(private localStorageService: LocalStorageService) {
+    this.activated = this.localStorageService.getHideFunds()
+
+  }
+
+  
 
   activate() {
     this.activated = true;
@@ -20,9 +26,10 @@ export class HideTextPipe implements PipeTransform {
     this.transform;
   }
 
-  transform(value: string): string {
-    if ((this.activated = true)) {
-      return value ? value.replace(value, '****') : value;
+  transform(value: string, activated : boolean): string {
+    if ((this.activated === true)) {
+      console.log("me estoy activando")
+      return value ? value.replace(value, '****') : value ;
     } else {
       return value;
     }
