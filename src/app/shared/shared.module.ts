8dd0ea05119cd8c1fb3d08f161632d4a9c2bdaa4
Federico Marquez <federico@xcapit.com>
2021-04-26 11:27:42 -0300

    :wrench: chore (shared): MR conflict resolve attempt 3

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -33,6 +33,7 @@ import { UxListInvertedComponent } from './components/ux-list-inverted/ux-list-i
 import { UxCheckboxComponent } from './components/ux-checkbox/ux-checkbox.component';
 import { UxTextareaComponent } from './components/ux-textarea/ux-textarea.component';
 import { UxDateRangeComponent } from './components/ux-date-range/ux-date-range.component';
+import { NeedHelpComponent } from './components/need-help/need-help.component';
 
 @NgModule({
   declarations: [
