b6afeed974c6080ab8d7b017b7a810449f169f68
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2021-05-04 17:32:32 -0300

    :white_check_mark: test (usuarios): Add refresh interceptor's tests

--- src/app/modules/usuarios/shared-usuarios/services/refresh-token-interceptor/refresh-token-interceptor.service.spec.ts
+++ src/app/modules/usuarios/shared-usuarios/services/refresh-token-interceptor/refresh-token-interceptor.service.spec.ts
@@ -1,23 +1,57 @@
-import { TestBed } from '@angular/core/testing';
-
-import { RefreshTokenInterceptorService } from './refresh-token-interceptor.service';
+import {RefreshTokenInterceptorService} from './refresh-token-interceptor.service';
 import {AuthService} from '../auth/auth.service';
+import {of, throwError} from 'rxjs';
+import {HttpErrorResponse, HttpRequest} from '@angular/common/http';
+
 
 describe('RefreshTokenInterceptorService', () => {
-  let service: RefreshTokenInterceptorService;
-  let authServiceSpy: any;
 
-  beforeEach(() => {
-    authServiceSpy = jasmine.createSpyObj('AuthService', ['checkRefreshToken', 'storedToken']);
-    TestBed.configureTestingModule({
-      providers: [
-        { provide: AuthService, useValue: authServiceSpy }
-      ]
+    it('should be created', () => {
+        expect(new RefreshTokenInterceptorService({} as AuthService)).toBeTruthy();
     });
-    service = TestBed.inject(RefreshTokenInterceptorService);
-  });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    describe('intercept', () => {
+
+        const httpRequestSpy = jasmine.createSpyObj('HttpRequest', ['clone']);
+        const httpHandlerSpy = jasmine.createSpyObj('HttpHandler', ['handle']);
+        httpRequestSpy.headers = {set: () => ''};
+        httpRequestSpy.clone = () => httpRequestSpy;
+
+        it('should catch first error and resend the request', async () => {
+            const fakeAuthService = {
+                checkRefreshToken: () => Promise.resolve(true),
+                storedToken: () => Promise.resolve('asdf')
+            };
+            const service = new RefreshTokenInterceptorService(fakeAuthService as AuthService);
+            httpHandlerSpy.handle.and.returnValues(throwError(new HttpErrorResponse({status: 401})), of(true));
+
+            const req = await service.intercept(httpRequestSpy, httpHandlerSpy).toPromise();
+
+            expect(req).toBeTruthy();
+        });
+
+        it('should catch first error and raise the second one', async () => {
+            const fakeAuthService = {checkRefreshToken: () => Promise.resolve(false)};
+            const service = new RefreshTokenInterceptorService(fakeAuthService as AuthService);
+            httpHandlerSpy.handle.and.returnValue(throwError(new HttpErrorResponse({status: 401})));
+
+            await expectAsync(service.intercept(httpRequestSpy, httpHandlerSpy).toPromise()).toBeRejected();
+        });
+
+        it('should catch first error and re raise', async () => {
+            const service = new RefreshTokenInterceptorService({} as AuthService);
+            httpHandlerSpy.handle.and.returnValue(throwError(new HttpErrorResponse({status: 400})));
+
+            await expectAsync(service.intercept(httpRequestSpy, httpHandlerSpy).toPromise()).toBeRejected();
+        });
+
+        it('should add authorization header', async () => {
+            const testToken = 'testToken';
+            const service = new RefreshTokenInterceptorService({} as AuthService);
+
+            const result = service.modifiedRequest(new HttpRequest('GET', '/'), testToken);
+
+            expect(result.headers.get('authorization')).toEqual(`Bearer ${testToken}`);
+        });
+    });
 });
