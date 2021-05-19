792b267ea33fd33d4d6eeb7c8e8239345e76139e
Federico Marquez <federico@xcapit.com>
2021-05-19 14:02:24 -0300

    :white_check_mark: test (Fiat-ramps): Added tests for new changes

--- src/app/modules/fiat-ramps/success-page/success-page.page.spec.ts
+++ src/app/modules/fiat-ramps/success-page/success-page.page.spec.ts
@@ -1,15 +1,18 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule, NavController } from '@ionic/angular';
-
 import { SuccessPagePage } from './success-page.page';
 import { StorageOperationService } from '../shared-ramps/services/operation/storage-operation.service';
 import { navControllerMock } from '../../../../testing/spies/nav-controller-mock.spec';
 import { RouterTestingModule } from '@angular/router/testing';
 import { DummyComponent } from 'src/testing/dummy.component.spec';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { of } from 'rxjs';
+import { ToastService } from 'src/app/shared/services/toast/toast.service';
+import { ClipboardService } from 'src/app/shared/services/clipboard/clipboard.service';
+import { TrackClickDirectiveTestHelper } from 'src/testing/track-click-directive-test.helper';
+import { TrackClickDirective } from 'src/app/shared/directives/track-click/track-click.directive';
 
 const storageData = {
   valid: {
@@ -50,6 +53,9 @@ describe('SuccessPagePage', () => {
   let storageOperationServiceMock: any;
   let storageOperationService: any;
   let navControllerSpy: any;
+  let toastServiceSpy: any;
+  let clipboardServiceSpy: any;
+  let trackClickDirectiveHelper: TrackClickDirectiveTestHelper<SuccessPagePage>;
 
   beforeEach(
     waitForAsync(() => {
@@ -59,19 +65,26 @@ describe('SuccessPagePage', () => {
         valid: storageData.valid.valid,
         clear: () => of({}),
       };
+      toastServiceSpy = jasmine.createSpyObj('ToastService', ['showToast']);
+      clipboardServiceSpy = jasmine.createSpyObj('ClipboardService', ['write']);
+      clipboardServiceSpy.write.and.returnValue(Promise.resolve());
 
       TestBed.configureTestingModule({
-        declarations: [SuccessPagePage],
+        declarations: [SuccessPagePage, TrackClickDirective],
         schemas: [CUSTOM_ELEMENTS_SCHEMA],
-
         imports: [
-          RouterTestingModule.withRoutes([{ path: 'fiat-ramps/operations', component: DummyComponent }]),
+          RouterTestingModule.withRoutes([
+            { path: 'operation-detail/provider/:provider_id/operation/:operation_id', component: DummyComponent },
+          ]),
           HttpClientTestingModule,
           IonicModule,
           TranslateModule.forRoot(),
         ],
         providers: [
+          TrackClickDirective,
           { provide: StorageOperationService, useValue: storageOperationServiceMock },
+          { provide: ToastService, useValue: toastServiceSpy },
+          { provide: ClipboardService, useValue: clipboardServiceSpy },
           { provide: NavController, useValue: navControllerSpy },
         ],
       }).compileComponents();
@@ -83,6 +96,7 @@ describe('SuccessPagePage', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
     storageOperationService = TestBed.inject(StorageOperationService);
+    trackClickDirectiveHelper = new TrackClickDirectiveTestHelper(fixture);
   });
 
   it('should create', () => {
@@ -93,5 +107,54 @@ describe('SuccessPagePage', () => {
     spyOn(window, 'open');
     component.launchChat();
     expect(window.open).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call write with cbu when copyToClipboard is called', async () => {
+    component.cbu = 'test';
+    const expectedArg = { url: 'test' };
+    fixture.detectChanges();
+    component.copyToClipboard();
+    fixture.whenStable().then(() => {
+      expect(clipboardServiceSpy.write).toHaveBeenCalledWith(expectedArg);
+    });
+  });
+
+  it('should call showToast when copyToClipboard is called', async () => {
+    component.cbu = '';
+    fixture.detectChanges();
+    component.copyToClipboard();
+    fixture.whenStable().then(() => {
+      expect(toastServiceSpy.showToast).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it('should call instant when copyToClipboard is called', async () => {
+    const spy = spyOn(TranslateService.prototype, 'instant');
+    component.cbu = '';
+    fixture.detectChanges();
+    component.copyToClipboard();
+    fixture.whenStable().then(() => {
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it('should call trackEvent on trackService when Copy CBU to Clipboard Button clicked', () => {
+    const el = trackClickDirectiveHelper.getByElementByName('ion-button', 'Copy CBU to Clipboard');
+    const directive = trackClickDirectiveHelper.getDirective(el);
+    const spy = spyOn(directive, 'clickEvent');
+    el.nativeElement.click();
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call trackEvent on trackService when Add Voucher Button clicked', () => {
+    component.operationData = { id: 1 };
+    fixture.detectChanges();
+    const el = trackClickDirectiveHelper.getByElementByName('ion-button', 'Add Voucher');
+    const directive = trackClickDirectiveHelper.getDirective(el);
+    const spy = spyOn(directive, 'clickEvent');
+    el.nativeElement.click();
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 });
