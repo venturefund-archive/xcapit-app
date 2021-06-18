cae164724629f25501f055852b036fb5f6f52f98
Martin Brunori <martinbrunori@ip-192-168-1-162.us-east-2.compute.internal>
2021-06-18 15:49:23 -0300

    :sparkles: feat: Hide unfinished features

--- src/app/modules/fiat-ramps/shared-ramps/constants/providers.ts
+++ src/app/modules/fiat-ramps/shared-ramps/constants/providers.ts
@@ -1,11 +1,11 @@
 export const PROVIDERS = [
-  {
-    id: 1,
-    alias: 'kripton',
-    name: 'KriptonMarket',
-    logoRoute: '../../assets/img/provider-logos/KriptonMarket.svg',
-    newOperationRoute: '/fiat-ramps/new-operation',
-  },
+  // {
+  //   id: 1,
+  //   alias: 'kripton',
+  //   name: 'KriptonMarket',
+  //   logoRoute: '../../assets/img/provider-logos/KriptonMarket.svg',
+  //   newOperationRoute: '/fiat-ramps/new-operation',
+  // },
   {
     id: 2,
     alias: 'paxful',
