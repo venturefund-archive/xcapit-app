2909063e5d1fd91ed5bebe13d98291e18dcc6488
diegomeineri95 <diego@xcapit.com>
2021-06-22 14:52:36 -0300

    :white_check_mark: test (select-license): add tests and some fixs

--- src/app/modules/payments/select-license/components/item-license/item-license.component.ts
+++ src/app/modules/payments/select-license/components/item-license/item-license.component.ts
@@ -1,33 +1,34 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 import { LicenseType } from '../../enums/license_type.enum';
 
 @Component({
   selector: 'app-item-license',
   template: ` <div class="ilc">
     <div class="ilc__content">
-      <div *ngIf="this.license.type == licenseType.premium" class="image">
+      <div *ngIf="this.license?.type == licenseType.premium" class="image">
         <img src="../assets/ux-icons/ux-corone.png" alt="premium logo" />
       </div>
       <div
-        *ngIf="this.license.type == licenseType.free || this.license.type == licenseType.paid"
+        *ngIf="this.license?.type == licenseType.free || this.license?.type == licenseType.paid"
         class="ilc__content__name ux-font-gilroy ux-fweight-bold ux-fsize-14 "
       >
         <input class="input_radio" type="radio" name="select" id="select" value="select" />
-        <label class="ilc__content__name__license" for="select">{{ this.license.name }}</label>
+        <label class="ilc__content__name__license" for="select">{{ this.license?.name }}</label>
       </div>
       <div
-        *ngIf="this.license.type == licenseType.premium"
+        *ngIf="this.license?.type == licenseType.premium"
         class="ilc__content__name ux-font-gilroy ux-fweight-bold ux-fsize-14 "
       >
-        <label class="ilc__content__name__premium" for="select">{{ this.license.name }}</label>
+        <label class="ilc__content__name__premium" for="select">{{ this.license?.name }}</label>
       </div>
       <div class="ilc__content__description">
         <div class="ux-font-lato ux-fweight-regular ux-fsize-14">
-          <ion-text class="info_text">{{ this.license.info | translate }}</ion-text>
+          <ion-text class="info_text">{{ this.license?.info | translate }}</ion-text>
         </div>
       </div>
       <div
-        *ngIf="this.license.type == licenseType.free || this.license.type == licenseType.paid"
+        *ngIf="this.license?.type == licenseType.free || this.license?.type == licenseType.paid"
         class="ilc__content__price"
       >
         <div>
@@ -36,25 +37,26 @@ import { LicenseType } from '../../enums/license_type.enum';
           }}</ion-text>
           <ion-text
             class="ux-font-lato ux-fweight-regular ux-fsize-12"
-            *ngIf="this.license.type == licenseType.paid && this.license.state == 'payment.licenses.annual'"
+            *ngIf="this.license?.type == licenseType.paid && this.license?.state == 'payment.licenses.annual'"
             class="name_text ux-font-lato ux-fsize-14"
             >/{{ 'payment.licenses.annual' | translate }}</ion-text
           >
           <ion-text
             class="ux-font-lato ux-fweight-regular ux-fsize-12"
-            *ngIf="this.license.type == licenseType.paid && this.license.state == 'payment.licenses.monthly'"
+            *ngIf="this.license?.type == licenseType.paid && this.license?.state == 'payment.licenses.monthly'"
             class="name_text ux-font-lato ux-fsize-14"
             >/{{ 'payment.licenses.monthly' | translate }}</ion-text
           >
         </div>
       </div>
-      <div *ngIf="this.license.type == licenseType.premium">
+      <div *ngIf="this.license?.type == licenseType.premium">
         <ion-button
           class="ilc__content__button ux-fweight-bold ux-fsize-14"
-          name="Contact Us"
+          name="contact"
           appTrackClick
           fill="clear"
           size="small"
+          (click)="goToContactUs()"
           >{{ 'payment.licenses.contact' | translate }}</ion-button
         >
       </div>
@@ -64,9 +66,14 @@ import { LicenseType } from '../../enums/license_type.enum';
 })
 export class ItemLicenseComponent implements OnInit {
   @Input() license: any;
+
   licenseType = LicenseType;
 
-  constructor() {}
+  constructor(private navController: NavController) {}
 
   ngOnInit() {}
+
+  goToContactUs() {
+    this.navController.navigateForward(['/payment/contact-license']).then();
+  }
 }
