9df30a67dbb4c970b745936e30e78227cd26e94e
Federico Marquez <federico@xcapit.com>
2021-06-24 11:16:33 -0300

    :sparkles: feat (Wallets): Proof of concept ready for testing

--- src/app/modules/wallets/test-wallet/test-wallet.page.ts
+++ src/app/modules/wallets/test-wallet/test-wallet.page.ts
@@ -4,7 +4,19 @@ import { BwcService } from '../shared-wallets/services/bwc/bwc.service';
 
 @Component({
   selector: 'app-test-wallet',
-  template: ` <ion-button (click)="this.createWallet()">Crear Wallet</ion-button> `,
+  template: `
+    <ion-header>
+      <ion-toolbar color="uxprimary" class="ux_toolbar">
+        <ion-buttons slot="start">
+          <ion-back-button defaultHref="/profiles/success"></ion-back-button>
+        </ion-buttons>
+        <ion-title class="ion-text-center">{{ 'apikeys.insert_key.header' | translate }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <ion-button (click)="this.createWallet()">Crear nueva wallet</ion-button>
+    </ion-content>
+  `,
   styleUrls: ['./test-wallet.page.scss'],
 })
 export class TestWalletPage implements OnInit {
