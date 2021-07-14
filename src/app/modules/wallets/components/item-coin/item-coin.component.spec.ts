a020e09a50cc9c581810efb281541cb36fc20567
diegomeineri95 <diego@xcapit.com>
2021-07-14 16:55:33 -0300

    :bug: fix (item-coin): add waitForAsync in test, lint fix

--- src/app/modules/wallets/components/item-coin/item-coin.component.spec.ts
+++ src/app/modules/wallets/components/item-coin/item-coin.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { ItemCoinComponent } from './item-coin.component';
@@ -7,16 +7,18 @@ describe('ItemCoinComponent', () => {
   let component: ItemCoinComponent;
   let fixture: ComponentFixture<ItemCoinComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ItemCoinComponent],
-      imports: [IonicModule.forRoot()],
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [ItemCoinComponent],
+        imports: [IonicModule.forRoot()],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(ItemCoinComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(ItemCoinComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
