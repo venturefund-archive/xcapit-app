a66faeb5e937dc089f17f89faac91230fc661579
Federico Marquez <federico@xcapit.com>
2021-07-15 16:48:21 -0300

    :wrench: chore (Wallets): Cleaning branch

--- src/app/modules/wallets/wallets-routing.module.ts
+++ src/app/modules/wallets/wallets-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from '../usuarios/shared-usuarios/guards/auth/auth.guard';
-import { AcceptedToSGuard } from './shared-wallets/guards/accepted-tos/accepted-tos.guard';
 
 const routes: Routes = [
   {
@@ -9,14 +8,8 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     children: [
       {
-        canActivate: [AcceptedToSGuard],
         path: 'test',
         loadChildren: () => import('./test-wallet/test-wallet.module').then((m) => m.TestWalletPageModule),
-      },
-      {
-        path: 'create-first/disclaimer',
-        loadChildren: () =>
-          import('./disclaimer-wallet/disclaimer-wallet.module').then((m) => m.DisclaimerWalletPageModule),
       },
     ],
   },
