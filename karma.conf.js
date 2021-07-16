6294ec3e5aef27c8247fc3d0cfa207f44a97461e
juan <verajm@gmail.com>
2021-07-15 22:57:31 -0300

    round 7

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -27,12 +27,10 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['ChromeHeadlessNoSandbox'],
     singleRun: false,
-    customLaunchers:{
-      ChromeHeadlessNoSandbox:{
-        base: 'ChromeHeadlessNoSandbox',
-        flags: [
-          '--no-sandbox'
-        ]
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
       }
     }
   });
