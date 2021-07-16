08001138dbad5b1aa4347e667fd23f26ce80908a
juan <verajm@gmail.com>
2021-07-16 07:25:26 -0300

    single mode

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function(config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadlessNoSandbox'],
-    singleRun: false,
+    singleRun: true,
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
