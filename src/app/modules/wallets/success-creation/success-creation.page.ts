3ba1e2167d5a6f3c0d826df60c0fd3c84132e641
ponspyth <anna@xcapit.com>
2021-07-20 15:25:46 -0300

    :sparkles: feat (wallets): success creation wallet is added

--- src/app/modules/wallets/success-creation/success-creation.page.ts
+++ src/app/modules/wallets/success-creation/success-creation.page.ts
@@ -1,12 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-success-creation',
-  template: ``,
+  template: `<ion-content class="ion-padding">
+    <div class="main">
+      <div class="main__ux_success_image">
+        <img src="../../../../assets/img/wallets/success_celebration.svg" />
+      </div>
+      <div class="main__primary_text">
+        <ion-text>{{ 'wallets.success_creation.title' | translate }}</ion-text>
+      </div>
+      <div class="main__secondary_text">
+        <ion-text>{{ 'wallets.success_creation.subtitle' | translate }}</ion-text>
+      </div>
+      <div class="main__actions">
+        <div class="main__actions__primary">
+          <ion-button appTrackClick class="ux_button" name="Go To Home" (click)="this.goToHome()">
+            {{ 'wallets.success_creation.success_button' | translate }}
+          </ion-button>
+        </div>
+      </div>
+    </div>
+  </ion-content>`,
   styleUrls: ['./success-creation.page.scss'],
 })
 export class SuccessCreationPage implements OnInit {
-  constructor() {}
+  constructor(private navController: NavController) {}
 
   ngOnInit() {}
+  goToHome() {
+    this.navController.navigateBack(['/tabs/funds']);
+  }
 }
