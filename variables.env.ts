6b2a98f683d0797312152fb66ba8147c6d61cf3d
Franco <franco@xcapit.com>
2021-07-27 09:03:10 -0300

    :wrench: chore: Restore variables.env.ts

---
+++ variables.env.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  apiUrl: 'https://dummy.xcapit.com/v1/api',
+  production: true,
+  environment: 'PREPROD',
+  whitelistedDomains: [
+    'dummy.xcapit.com',
+    'xcapit.com',
+    'app.xcapit.com',
+    'app.xcapit.com:443',
+    'xcapit.com:443',
+    'dummy.xcapit.com:443',
+  ],
+  firebase: {
+    apiKey: 'AIzaSyCU-F8osRaeWGwTxdAJmDhWFfkjZqzUG7s',
+    projectId: 'test-pwa-2019',
+    storageBucket: 'test-pwa-2019.appspot.com',
+    messagingSenderId: '1059796815977',
+    appId: 'dummy',
+    vapidKey: 'dummy',
+  },
+  appUrl: 'https://dummy.xcapit.com/',
+};
