88bda4da146435afcdb73c87a0c6b79c9f2fd1a6
Franco <franco@xcapit.com>
2021-08-24 10:43:45 -0300

    feat: SX-1134 - New home page

--- src/app/shared/components/icon-button-card/icon-button-card.component.ts
+++ src/app/shared/components/icon-button-card/icon-button-card.component.ts
@@ -3,10 +3,15 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-icon-button-card',
   template: `
-    <div class="rc">
+    <div class="rc" style="position:relative;">
       <div class="rc__icon"><ion-icon color="uxprimary" [name]="this.icon"></ion-icon></div>
       <div class="rc__text">
         <ion-text class="ux-font-lato ux-fweight-regular ux-fsize-10">{{ this.text }}</ion-text>
+      </div>
+      <div *ngIf="this.comingSoon" class="coming_soon">
+        <ion-text class="ux-font-lato ux-fweight-bold ux-fsize-8">{{
+          'shared.app_icon_button_card.coming_soon' | translate
+        }}</ion-text>
       </div>
     </div>
   `,
@@ -15,6 +20,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class IconButtonCardComponent implements OnInit {
   @Input() icon: string;
   @Input() text: string;
+  @Input() comingSoon = false;
 
   constructor() {}
 
