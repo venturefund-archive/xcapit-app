b1e3ed29428ac50e298a2972fd517456fa0b5840
Martin Brunori <martinbrunori@Martins-MacBook-Pro.local>
2021-08-31 12:48:06 -0300

    test: Add tests

--- src/app/modules/funds/shared-funds/components/fund-balance-chart/fund-balance-chart.component.spec.ts
+++ src/app/modules/funds/shared-funds/components/fund-balance-chart/fund-balance-chart.component.spec.ts
@@ -1,19 +1,17 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FundBalanceChartComponent } from './fund-balance-chart.component';
 
 describe('FundBalanceChartComponent', () => {
   let component: FundBalanceChartComponent;
   let fixture: ComponentFixture<FundBalanceChartComponent>;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [FundBalanceChartComponent],
-        schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      }).compileComponents();
-    })
-  );
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [FundBalanceChartComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FundBalanceChartComponent);
@@ -23,5 +21,22 @@ describe('FundBalanceChartComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create chart on changes and fundBalance exists', () => {
+    component.fundBalance = [{ color: 'red', percentage: 50, ca: 'ETH' }];
+    component.ngOnChanges();
+    expect(component.colors).toEqual(['red']);
+    expect(component.data).toEqual([50]);
+    expect(component.labels).toEqual(['ETH']);
+    expect(component.chart).toBeTruthy();
+  });
+
+  it('should not create chart on changes and fund balance not exists', () => {
+    component.fundBalance = null;
+    component.ngOnChanges();
+    expect(component.colors).toEqual([]);
+    expect(component.data).toEqual([]);
+    expect(component.labels).toEqual([]);
   });
 });
