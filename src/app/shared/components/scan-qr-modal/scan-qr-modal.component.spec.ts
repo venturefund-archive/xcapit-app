81ca56b99ce4cea8649f3f2b08676f5517132ba7
Martin Brunori <martinbrunori@Martins-MacBook-Pro.local>
2021-09-07 11:18:12 -0300

    fix(wallets): SX-1203 QR scanned address not showing

--- src/app/shared/components/scan-qr-modal/scan-qr-modal.component.spec.ts
+++ src/app/shared/components/scan-qr-modal/scan-qr-modal.component.spec.ts
@@ -3,29 +3,30 @@ import { IonicModule, ModalController } from '@ionic/angular';
 import { ScanQrModalComponent } from './scan-qr-modal.component';
 import { Renderer2, Type } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import { FakeModalController } from '../../../../testing/fakes/modal-controller.fake.spec';
 
-describe('ScanQrModalComponent', () => {
+fdescribe('ScanQrModalComponent', () => {
   let component: ScanQrModalComponent;
   let fixture: ComponentFixture<ScanQrModalComponent>;
   let barcodeScannerMock: any;
   let renderer2: Renderer2;
-  let modalControllerMock: any;
   let addClassSpy: any;
   let removeClassSpy: any;
+  let fakeModalController: FakeModalController;
+  let modalControllerSpy;
   beforeEach(() => {
+    fakeModalController = new FakeModalController();
+    modalControllerSpy = fakeModalController.createSpy();
     barcodeScannerMock = {
       hideBackground: () => Promise.resolve(),
       showBackground: () => Promise.resolve(),
       startScan: () => Promise.resolve(),
       checkPermission: () => Promise.resolve({ granted: true }),
     };
-    modalControllerMock = {
-      dismiss: () => Promise.resolve(),
-    };
     TestBed.configureTestingModule({
       declarations: [ScanQrModalComponent],
       imports: [IonicModule],
-      providers: [Renderer2, { provide: ModalController, useValue: modalControllerMock }],
+      providers: [Renderer2, { provide: ModalController, useValue: modalControllerSpy }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ScanQrModalComponent);
@@ -45,6 +46,7 @@ describe('ScanQrModalComponent', () => {
     spy.and.returnValue(Promise.resolve({ hasContent: true, content: 'test' }));
     fixture.detectChanges();
     await fixture.whenStable();
+    expect(modalControllerSpy.dismiss).toHaveBeenCalledOnceWith('test', 'success');
     expect(spy).toHaveBeenCalledTimes(1);
     expect(addClassSpy).toHaveBeenCalledTimes(2);
     expect(removeClassSpy).toHaveBeenCalledTimes(2);
@@ -56,24 +58,23 @@ describe('ScanQrModalComponent', () => {
     fixture.detectChanges();
     await fixture.whenStable();
     expect(spy).toHaveBeenCalledTimes(1);
+    expect(modalControllerSpy.dismiss).toHaveBeenCalledOnceWith(undefined, 'error');
     expect(addClassSpy).toHaveBeenCalledTimes(2);
     expect(removeClassSpy).toHaveBeenCalledTimes(2);
   });
 
   it('should dismiss on unauthorized', async () => {
-    const modalControllerSpy = spyOn(modalControllerMock, 'dismiss');
     const spy = spyOn(component.barcodeScanner, 'checkPermission');
     spy.and.returnValue(Promise.resolve({ granted: false }));
     fixture.detectChanges();
     await fixture.whenStable();
     expect(spy).toHaveBeenCalledTimes(1);
-    expect(modalControllerSpy).toHaveBeenCalledWith(null, 'unauthorized');
+    expect(modalControllerSpy.dismiss).toHaveBeenCalledWith(null, 'unauthorized');
   });
 
   it('should dismiss on close', async () => {
-    const modalControllerSpy = spyOn(modalControllerMock, 'dismiss');
     fixture.debugElement.query(By.css('ion-button[name="Cancel"]')).nativeElement.click();
     await fixture.whenStable();
-    expect(modalControllerSpy).toHaveBeenCalledWith(null, 'cancelled');
+    expect(modalControllerSpy.dismiss).toHaveBeenCalledWith(null, 'cancelled');
   });
 });
