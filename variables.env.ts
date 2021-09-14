fef9e02486cc001e1bcf6abfa9e34475e14fd87c
juan <verajm@gmail.com>
2021-09-14 09:43:36 -0300

    missing vars

--- variables.env.ts
+++ variables.env.ts
@@ -19,4 +19,11 @@ export const environment = {
     vapidKey: 'dummy',
   },
   appUrl: 'https://dummy.xcapit.com/',
+  walletNetwork: 'testnet',
+  ethAlchemyApiUrl: 'https://eth-rinkeby.alchemyapi.io/v2/apikey',
+  rskApiUrl: "https://rsk.node",
+    derivedPaths: {
+    ERC20: "m/44'/60'/0'/0/0",
+    RSK: "m/44'/37310'/0'/0/0"
+  },
 };
