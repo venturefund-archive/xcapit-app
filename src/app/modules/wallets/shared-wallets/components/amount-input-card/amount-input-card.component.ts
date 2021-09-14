0ec1ea055a04ecaea8f041bfcc4540bb18744a9e
Martin Brunori <martinbrunori@Martins-MacBook-Pro.local>
2021-09-14 14:45:04 -0300

    feat(wallets): SX-1246 Get valid RBTC price

--- src/app/modules/wallets/shared-wallets/components/amount-input-card/amount-input-card.component.ts
+++ src/app/modules/wallets/shared-wallets/components/amount-input-card/amount-input-card.component.ts
@@ -59,9 +59,13 @@ export class AmountInputCardComponent implements OnInit {
 
   private amountChange(value: number) {
     this.loading = true;
-    this.apiWalletService.getPrices([this.currencyName], false).subscribe((res) => {
-      this.form.patchValue({ referenceAmount: value * res.prices[this.currencyName] });
+    this.apiWalletService.getPrices([this.base()], false).subscribe((res) => {
+      this.form.patchValue({ referenceAmount: value * res.prices[this.base()] });
       this.loading = false;
     });
+  }
+
+  private base() {
+    return this.currencyName === 'RBTC' ? 'BTC' : this.currencyName;
   }
 }
