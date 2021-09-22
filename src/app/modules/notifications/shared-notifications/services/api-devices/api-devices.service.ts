335f00913c478d191be285558d19be29f2e40f22
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2021-09-22 11:16:03 -0300

    fix(notifications): Remove loading on device registration

--- src/app/modules/notifications/shared-notifications/services/api-devices/api-devices.service.ts
+++ src/app/modules/notifications/shared-notifications/services/api-devices/api-devices.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { CRUD } from 'src/app/shared/services/crud/crud';
 import { CustomHttpService } from 'src/app/shared/services/custom-http/custom-http.service';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -8,13 +7,11 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class ApiDevicesService {
-  crud: CRUD;
-
   entity = 'notifications/device';
 
   constructor(private http: CustomHttpService) {}
 
   register(fcmToken: string): Observable<any> {
-    return this.http.post(`${environment.apiUrl}/${this.entity}/fcm`, { fcm_registration_id: fcmToken });
+    return this.http.post(`${environment.apiUrl}/${this.entity}/fcm`, { fcm_registration_id: fcmToken }, '', false);
   }
 }
