a71774fe14d4ced918dfbdd803764d5b9afe9871
juan <verajm@gmail.com>
2021-09-28 21:41:12 -0300

    test 5

--- karma.conf.js
+++ karma.conf.js
@@ -11,6 +11,7 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
+      require("karma-junit-reporter"),
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
@@ -21,16 +22,9 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true,
     },
     // reporters: ['progress', 'kjhtml'],
-    reporters: ['progress', 'junit'],
+    reporters: ['progress', 'junit','kjhtml'],
     junitReporter: {
-      outputDir: '', // results will be saved as $outputDir/$browserName.xml
-      outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
-      suite: '', // suite will become the package name attribute in xml testsuite element
-      useBrowserName: true, // add browser name to report and classes names
-      nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
-      classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
-      properties: {}, // key value pair of properties to add to the <properties> section of the report
-      xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
+      outputDir: "reports",
     },
     port: 9876,
     colors: true,
