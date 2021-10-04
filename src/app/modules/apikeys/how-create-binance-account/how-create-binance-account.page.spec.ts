d55ea140e6b4981ecf89503c9b2030c28bdaf0f2
diegomeineri95 <diego@xcapit.com>
2021-10-04 16:53:21 -0300

    fix(how-create-binance-account): change trackClickDirective for Fake

--- src/app/modules/apikeys/how-create-binance-account/how-create-binance-account.page.spec.ts
+++ src/app/modules/apikeys/how-create-binance-account/how-create-binance-account.page.spec.ts
@@ -4,8 +4,8 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { IonicModule, NavController } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
-import { TrackClickDirective } from 'src/app/shared/directives/track-click/track-click.directive';
 import { FakeNavController } from 'src/testing/fakes/nav-controller.fake.spec';
+import { FakeTrackClickDirective } from 'src/testing/fakes/track-click-directive.fake.spec';
 import { TrackClickDirectiveTestHelper } from 'src/testing/track-click-directive-test.helper';
 
 import { HowCreateBinanceAccountPage } from './how-create-binance-account.page';
@@ -21,9 +21,9 @@ describe('HowCreateBinanceAccountPage', () => {
       fakeNavController = new FakeNavController({});
       navControllerSpy = fakeNavController.createSpy();
       TestBed.configureTestingModule({
-        declarations: [HowCreateBinanceAccountPage, TrackClickDirective],
+        declarations: [HowCreateBinanceAccountPage, FakeTrackClickDirective],
         imports: [IonicModule.forRoot(), TranslateModule.forRoot(), HttpClientTestingModule],
-        providers: [TrackClickDirective, { provide: NavController, useValue: navControllerSpy }],
+        providers: [{ provide: NavController, useValue: navControllerSpy }],
         schemas: [CUSTOM_ELEMENTS_SCHEMA],
       }).compileComponents();
 
