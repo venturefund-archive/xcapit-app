e95e4e11f858e5f7dfa9ea574febc29fcc80935e
Franco Schillage <fschillage@gmail.com>
2021-10-22 15:50:12 -0300

    feat: Change categories on support page

---
+++ src/app/modules/tickets/shared-tickets/constants/ticket-categories.ts
@@ -0,0 +1,9 @@
+export const TICKET_CATEGORIES = [
+  { name: 'Mi cuenta/Registro', value: 'tickets.categories.my_account' },
+  { name: 'Vincular mi cuenta de Binance', value: 'tickets.categories.link_binance_account' },
+  { name: 'Planes de suscripción', value: 'tickets.categories.subscription_plans' },
+  { name: 'Comprar criptomonedas', value: 'tickets.categories.buy_crypto' },
+  { name: 'Estrategias de inversión', value: 'tickets.categories.investment_strategies' },
+  { name: 'Seguridad de mi capital', value: 'tickets.categories.capital_security' },
+  { name: 'Otros', value: 'tickets.categories.others' },
+];
