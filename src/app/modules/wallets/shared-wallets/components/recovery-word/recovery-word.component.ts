81e93eb89585994a2044928a2a4901e7b464e66a
Alan Terreno <alan@xcapit.com>
2021-11-19 12:00:59 -0300

    fix: fix fonts styles

--- src/app/modules/wallets/shared-wallets/components/recovery-word/recovery-word.component.ts
+++ src/app/modules/wallets/shared-wallets/components/recovery-word/recovery-word.component.ts
@@ -6,7 +6,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
     <ion-button
       name="Recovery Word"
       size="small"
-      class="word"
+      class="word ux-font-text-xxs"
       color="uxprimary"
       [disabled]="!this.isActivated"
       (click)="useValue(this.word)"
