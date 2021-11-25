632268c55d15a0e0bc4b0ad0927d63b2b507c382
Martin Brunori <martinbrunori@Martins-MacBook-Pro.local>
2021-11-25 11:53:21 -0300

    refactor: TIC-170 Remove NativeMarket deprecated Plugin

--- src/app/shared/services/update-app/update-app.service.ts
+++ src/app/shared/services/update-app/update-app.service.ts
@@ -3,14 +3,12 @@ import { UpdateService } from '../update/update.service';
 import { AlertController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
-import { Plugins } from '@capacitor/core';
 import { App } from '@capacitor/app';
 @Injectable({
   providedIn: 'root',
 })
 export class UpdateAppService extends UpdateService {
   app = App;
-  nativeMarket = Plugins.NativeMarket;
 
   constructor(alertController: AlertController, translate: TranslateService, http: HttpClient) {
     super(alertController, translate, http);
@@ -20,8 +18,5 @@ export class UpdateAppService extends UpdateService {
     return await this.app.getInfo().then((res) => res.version);
   }
 
-  protected async update(): Promise<any> {
-    const { id } = await this.app.getInfo();
-    this.nativeMarket.openStoreListing({ appId: id });
-  }
+  protected async update(): Promise<any> {}
 }
