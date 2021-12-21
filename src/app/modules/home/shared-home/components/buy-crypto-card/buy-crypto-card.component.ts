f31ebc9d0fbd8b2d8eca172430e0000cf7cfafd9
Martin Brunori <martinbrunori@Martins-MacBook-Pro.local>
2021-12-21 10:26:22 -0300

    feat(home): SX-1661 Add test track

--- src/app/modules/home/shared-home/components/buy-crypto-card/buy-crypto-card.component.ts
+++ src/app/modules/home/shared-home/components/buy-crypto-card/buy-crypto-card.component.ts
@@ -3,12 +3,12 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-buy-crypto-card',
   template: `
-    <div class="bcc" (click)="this.emitClicked()">
+    <div class="bcc" (click)="this.emitClicked()" name="Buy Crypto" appTrackClick>
       <div class="bcc__image">
         <img src="assets/img/home/btc-coins.svg" alt="BTC coins" />
       </div>
       <div class="bcc__text">
-        <ion-text class="ux-font-text-lg">{{ 'Compra crypto al mejor precio' }}</ion-text>
+        <ion-text class="ux-font-text-lg">{{ 'home.shared.buy_crypto_card.title' | translate }}</ion-text>
       </div>
       <div class="bcc__arrow">
         <ion-icon name="ux-forward" color="uxlight"></ion-icon>
