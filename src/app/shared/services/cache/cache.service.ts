5f5e3a82baa5cf698ab0cb76e5e905c48c374d87
Franco Schillage <fschillage@gmail.com>
2022-01-05 17:52:11 -0300

    feat: WIP: IoniStorage and cache service added

---
+++ src/app/shared/services/cache/cache.service.ts
@@ -0,0 +1,41 @@
+import { IonicStorageService } from './../ionic-storage/ionic-storage.service';
+import { Injectable } from '@angular/core';
+import { CONFIG } from '../../../config/app-constants.config';
+import add from 'date-fns/add';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CacheService {
+  constructor(private storage: IonicStorageService) {}
+
+  private addPrefix(key: string): string {
+    return `${CONFIG.cache.PREFIX}${key}`;
+  }
+
+  private expirationDate(): number {
+    return add(this.now(), { seconds: CONFIG.cache.TTL }).getTime();
+  }
+
+  private now(): number {
+    return new Date().getTime();
+  }
+
+  private isExpired(timestamp: string): boolean {
+    return this.now() > parseInt(timestamp);
+  }
+
+  update(key: string, value: any): Promise<any> {
+    return this.storage.set(this.addPrefix(key), Object.assign(value, { expiration_date: this.expirationDate() }));
+  }
+
+  async get(key: string): Promise<any> {
+    const storedValue = await this.storage.get(this.addPrefix(key));
+
+    if (storedValue && !this.isExpired(storedValue.expiration_date)) {
+      return storedValue;
+    } else {
+      this.storage.remove(this.addPrefix(key));
+    }
+  }
+}
