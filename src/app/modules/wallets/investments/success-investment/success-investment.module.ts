9275565860fcdaf8afa1ec3df8eaf143d9657906
Alan Terreno <alan@xcapit.com>
2022-01-10 14:40:06 -0300

    fix: redirection changed

--- src/app/modules/wallets/investments/success-investment/success-investment.module.ts
+++ src/app/modules/wallets/investments/success-investment/success-investment.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { SuccessInvestmentPage } from './success-investment.page';
 import { RouterModule, Routes } from '@angular/router';
-import { SharedWealthManagementsModule } from '../shared-wealth-managements/shared-wealth-managements.module';
+import { SharedWealthManagementsModule } from '../../../wealth-managements/shared-wealth-managements/shared-wealth-managements.module';
 
 const routes: Routes = [
   {
