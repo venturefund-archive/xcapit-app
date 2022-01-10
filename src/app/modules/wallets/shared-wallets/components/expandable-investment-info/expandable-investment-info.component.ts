0de4efe22ccfdf8eaca3c4c35991bde85bccbd51
Franco Schillage <fschillage@gmail.com>
2022-01-10 16:28:23 -0300

    feat: Add inline image to deposit and obtain information items

--- src/app/modules/wallets/shared-wallets/components/expandable-investment-info/expandable-investment-info.component.ts
+++ src/app/modules/wallets/shared-wallets/components/expandable-investment-info/expandable-investment-info.component.ts
@@ -15,7 +15,9 @@ import { Component, OnInit } from '@angular/core';
                 <ion-text class="ux-font-text-lg">USDC</ion-text>
                 <ion-text class="ux-font-text-base">USD Coin</ion-text>
               </ion-label>
-              <ion-badge class="ux-font-num-subtitulo"> 2.6{{ 'defi_investments.shared.expandable_investment_info.annual'| translate}} </ion-badge>
+              <ion-badge class="ux-font-num-subtitulo">
+                2.6{{ 'defi_investments.shared.expandable_investment_info.annual' | translate }}
+              </ion-badge>
             </div>
           </div>
           <ion-icon
@@ -29,28 +31,38 @@ import { Component, OnInit } from '@angular/core';
         <ion-list lines="none" slot="content" class="eif__accordion__content">
           <ion-item>
             <ion-label class="eif__accordion__content__information-item">
-              <ion-text class="eif__accordion__content__information-item__label ux-font-titulo-xs "
-                >{{ 'defi_investments.shared.expandable_investment_info.TVL'| translate}}</ion-text
-              >
+              <ion-text class="eif__accordion__content__information-item__label ux-font-titulo-xs ">{{
+                'defi_investments.shared.expandable_investment_info.TVL' | translate
+              }}</ion-text>
               <ion-text class="eif__accordion__content__information-item__text ux-font-text-base">$158,551,23</ion-text>
             </ion-label>
           </ion-item>
           <ion-item>
             <ion-label class="eif__accordion__content__information-item">
-              <ion-text class="eif__accordion__content__information-item__label ux-font-titulo-xs "
-                >{{ 'defi_investments.shared.expandable_investment_info.protocol_type'| translate}}</ion-text
-              >
+              <ion-text class="eif__accordion__content__information-item__label ux-font-titulo-xs ">{{
+                'defi_investments.shared.expandable_investment_info.protocol_type' | translate
+              }}</ion-text>
               <ion-text class="eif__accordion__content__information-item__text ux-font-text-base">Vault</ion-text>
             </ion-label>
           </ion-item>
           <ion-item class="split-information-item">
             <ion-label class="eif__accordion__content__information-item">
-              <ion-text class="eif__accordion__content__information-item__label ux-font-titulo-xs ">{{ 'defi_investments.shared.expandable_investment_info.deposit_asset'| translate}}</ion-text>
-              <ion-text class="eif__accordion__content__information-item__text ux-font-text-base ">USDC</ion-text>
+              <ion-text class="eif__accordion__content__information-item__label ux-font-titulo-xs ">{{
+                'defi_investments.shared.expandable_investment_info.deposit_asset' | translate
+              }}</ion-text>
+              <div class="inline-image">
+                <img src="assets/img/coins/USDC.png" />
+                <ion-text class="eif__accordion__content__information-item__text ux-font-text-base ">USDC</ion-text>
+              </div>
             </ion-label>
             <ion-label class="eif__accordion__content__information-item">
-              <ion-text class="eif__accordion__content__information-item__label ux-font-titulo-xs">{{ 'defi_investments.shared.expandable_investment_info.receive_asset'| translate}}</ion-text>
-              <ion-text class="eif__accordion__content__information-item__text ux-font-text-base">USDC</ion-text>
+              <ion-text class="eif__accordion__content__information-item__label ux-font-titulo-xs">{{
+                'defi_investments.shared.expandable_investment_info.receive_asset' | translate
+              }}</ion-text>
+              <div class="inline_image">
+                <img src="assets/img/coins/USDC.png" />
+                <ion-text class="eif__accordion__content__information-item__text ux-font-text-base">USDC</ion-text>
+              </div>
             </ion-label>
           </ion-item>
           <ion-item>
@@ -61,7 +73,9 @@ import { Component, OnInit } from '@angular/core';
           </ion-item>
           <ion-item>
             <ion-label class="eif__accordion__content__information-item">
-              <ion-text class="eif__accordion__content__information-item__label ux-font-titulo-xs ">{{ 'defi_investments.shared.expandable_investment_info.platform'| translate}}</ion-text>
+              <ion-text class="eif__accordion__content__information-item__label ux-font-titulo-xs ">{{
+                'defi_investments.shared.expandable_investment_info.platform' | translate
+              }}</ion-text>
               <ion-text class="eif__accordion__content__information-item__text ux-font-text-base">2PI</ion-text>
             </ion-label>
           </ion-item>
