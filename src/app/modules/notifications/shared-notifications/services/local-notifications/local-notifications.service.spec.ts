64ef2577a1cd976f34bdab3c3d459d1bae0b4e06
Martin Brunori <martinbrunori@Martins-MacBook-Pro-7.local>
2022-01-14 10:31:43 -0300

    feat(notifications): TIC-201 Add tests to local notifications

--- src/app/modules/notifications/shared-notifications/services/local-notifications/local-notifications.service.spec.ts
+++ src/app/modules/notifications/shared-notifications/services/local-notifications/local-notifications.service.spec.ts
@@ -1,18 +1,40 @@
 import { TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
+import { LocalNotificationSchema } from '@capacitor/local-notifications';
 import { LocalNotificationsService } from './local-notifications.service';
 
-describe('LocalNotificationsService', () => {
-  let http: any;
+fdescribe('LocalNotificationsService', () => {
+  let localNotificationsSpy: jasmine.SpyObj<any>;
   let service: LocalNotificationsService;
   beforeEach(() => {
-    http = jasmine.createSpyObj('HttpClient', ['post']);
-    http.post.and.returnValue(of({}));
+    localNotificationsSpy = jasmine.createSpyObj('LocalNotifications', {
+      requestPermissions: Promise.resolve({ display: 'granted' }),
+      addListener: null,
+      schedule: Promise.resolve(),
+    });
     TestBed.configureTestingModule({});
     service = TestBed.inject(LocalNotificationsService);
+    service.localNotifications = localNotificationsSpy;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should request permissions on init', () => {
+    service.init();
+    expect(localNotificationsSpy.requestPermissions).toHaveBeenCalledTimes(1);
+  });
+
+  it('should send notification if permission is granted', async () => {
+    await service.init();
+    await service.send(jasmine.createSpyObj('Notification', {}, { notification: 'test' }));
+    expect(localNotificationsSpy.schedule).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not send notification if permission is not granted', async () => {
+    localNotificationsSpy.requestPermissions.and.resolveTo({ display: 'not granted' });
+    await service.init();
+    await service.send(jasmine.createSpyObj('Notification', {}, { notification: 'test' }));
+    expect(localNotificationsSpy.schedule).toHaveBeenCalledTimes(0);
   });
 });
