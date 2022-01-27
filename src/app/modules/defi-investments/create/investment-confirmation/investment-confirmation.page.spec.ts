231751266fc63b329ea320a95abea47bc7c6d0e1
Franco Schillage <fschillage@gmail.com>
2022-01-27 17:36:03 -0300

    feat: Refactor of confirmation page and tests added

--- src/app/modules/defi-investments/create/investment-confirmation/investment-confirmation.page.spec.ts
+++ src/app/modules/defi-investments/create/investment-confirmation/investment-confirmation.page.spec.ts
@@ -1,16 +1,23 @@
+import {
+  Investment,
+  TwoPiInvestment,
+} from './../../shared-defi-investments/models/two-pi-investment/two-pi-investment.model';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule, ModalController, NavController } from '@ionic/angular';
-
 import { InvestmentConfirmationPage } from './investment-confirmation.page';
 import { InvestmentDataService } from '../../shared-defi-investments/services/investment-data/investment-data.service';
 import { WalletService } from '../../../wallets/shared-wallets/services/wallet/wallet.service';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { WalletEncryptionService } from '../../../wallets/shared-wallets/services/wallet-encryption/wallet-encryption.service';
 import { FakeNavController } from '../../../../../testing/fakes/nav-controller.fake.spec';
 import { FakeModalController } from '../../../../../testing/fakes/modal-controller.fake.spec';
 import { Wallet } from 'ethers';
+import { By } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { InvestmentProduct } from '../../shared-defi-investments/interfaces/investment-product.interface';
+import { ToastService } from 'src/app/shared/services/toast/toast.service';
 
-fdescribe('InvestmentConfirmationPage', () => {
+describe('InvestmentConfirmationPage', () => {
   let component: InvestmentConfirmationPage;
   let fixture: ComponentFixture<InvestmentConfirmationPage>;
   let investmentDataServiceSpy: jasmine.SpyObj<InvestmentDataService>;
@@ -21,17 +28,24 @@ fdescribe('InvestmentConfirmationPage', () => {
   let navControllerSpy: jasmine.SpyObj<NavController>;
   let fakeNavController: FakeNavController;
   let wallet: Wallet;
+  let productSpy: jasmine.SpyObj<InvestmentProduct>;
+  let investmentSpy: jasmine.SpyObj<Investment>;
+  let toastServiceSpy: jasmine.SpyObj<ToastService>;
   beforeEach(
     waitForAsync(() => {
-      fakeModalController = new FakeModalController();
+      fakeModalController = new FakeModalController({ data: 'fake_password' });
       modalControllerSpy = fakeModalController.createSpy();
       fakeNavController = new FakeNavController();
       navControllerSpy = fakeNavController.createSpy();
+      productSpy = jasmine.createSpyObj('InvestmentProduct', {
+        token: { value: 'USDC' },
+        contractAddress: '0xtest',
+      });
       investmentDataServiceSpy = jasmine.createSpyObj(
         'InvestmentDataService',
         {},
         {
-          product: { token: () => ({ value: 'USDC' }) },
+          product: productSpy,
           amount: 50,
           quoteAmount: 100,
         }
@@ -44,6 +58,12 @@ fdescribe('InvestmentConfirmationPage', () => {
       walletEncryptionServiceSpy = jasmine.createSpyObj('WalletEncryptionService', {
         getDecryptedWalletForCurrency: wallet,
       });
+      investmentSpy = jasmine.createSpyObj('Investment', {
+        deposit: Promise.resolve({ wait: () => Promise.resolve() }),
+      });
+      toastServiceSpy = jasmine.createSpyObj('ToastService', {
+        showErrorToast: Promise.resolve(),
+      });
       TestBed.configureTestingModule({
         declarations: [InvestmentConfirmationPage],
         imports: [IonicModule.forRoot(), TranslateModule.forRoot()],
@@ -53,7 +73,9 @@ fdescribe('InvestmentConfirmationPage', () => {
           { provide: ModalController, useValue: modalControllerSpy },
           { provide: WalletEncryptionService, useValue: walletEncryptionServiceSpy },
           { provide: NavController, useValue: navControllerSpy },
+          { provide: ToastService, useValue: toastServiceSpy },
         ],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
       }).compileComponents();
 
       fixture = TestBed.createComponent(InvestmentConfirmationPage);
@@ -64,5 +86,67 @@ fdescribe('InvestmentConfirmationPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render transaction data properly', async () => {
+    component.ionViewDidEnter();
+    await Promise.all([fixture.whenStable(), fixture.whenRenderingDone()]);
+    fixture.detectChanges();
+
+    const productEl = fixture.debugElement.query(By.css('app-expandable-investment-info'));
+    const amountEl = fixture.debugElement.query(By.css('.summary__amount__qty__amount'));
+    const quoteAmountEl = fixture.debugElement.query(By.css('.summary__amount__qty__quoteAmount'));
+    expect(productEl).toBeTruthy();
+    expect(amountEl.nativeElement.innerHTML).toContain(50);
+    expect(quoteAmountEl.nativeElement.innerHTML).toContain(100);
+  });
+
+  it('should make deposit when password is valid', async () => {
+    spyOn(component, 'investment').and.returnValue(investmentSpy);
+    await component.ionViewDidEnter();
+    fixture.detectChanges();
+    fixture.debugElement.query(By.css('ion-button[name="Confirm Investment"]')).nativeElement.click();
+    await Promise.all([fixture.whenStable(), fixture.whenRenderingDone()]);
+    expect(investmentSpy.deposit).toHaveBeenCalledTimes(1);
+    expect(navControllerSpy.navigateForward).toHaveBeenCalledOnceWith('/defi/success-investment');
+  });
+
+  it('should not make deposit when password is valid but deposit fails', async () => {
+    investmentSpy.deposit.and.returnValue(Promise.reject());
+    spyOn(component, 'investment').and.returnValue(investmentSpy);
+    await component.ionViewDidEnter();
+    fixture.detectChanges();
+    fixture.debugElement.query(By.css('ion-button[name="Confirm Investment"]')).nativeElement.click();
+    await Promise.all([fixture.whenStable(), fixture.whenRenderingDone()]);
+    expect(investmentSpy.deposit).toHaveBeenCalledTimes(1);
+    expect(navControllerSpy.navigateForward).toHaveBeenCalledOnceWith('/defi/error-investment');
+  });
+
+  it('should not make deposit when modal closes', async () => {
+    fakeModalController.modifyReturns({ data: undefined }, {});
+    await component.ionViewDidEnter();
+    fixture.detectChanges();
+    fixture.debugElement.query(By.css('ion-button[name="Confirm Investment"]')).nativeElement.click();
+    await Promise.all([fixture.whenStable(), fixture.whenRenderingDone()]);
+    expect(walletEncryptionServiceSpy.getDecryptedWalletForCurrency).not.toHaveBeenCalled();
+    expect(investmentSpy.deposit).not.toHaveBeenCalled();
+  });
+
+  it('should not make deposit when password is not valid', async () => {
+    walletEncryptionServiceSpy.getDecryptedWalletForCurrency.and.rejectWith();
+    await component.ionViewDidEnter();
+    fixture.detectChanges();
+    fixture.debugElement.query(By.css('ion-button[name="Confirm Investment"]')).nativeElement.click();
+    await Promise.all([fixture.whenStable(), fixture.whenRenderingDone()]);
+    expect(investmentSpy.deposit).not.toHaveBeenCalled();
+    expect(toastServiceSpy.showErrorToast).toHaveBeenCalledOnceWith({
+      message: 'defi_investments.confirmation.password_error',
+    });
+  });
+
+  it('should return an investment', async () => {
+    await component.ionViewDidEnter();
+    fixture.detectChanges();
+    expect(component.investment(wallet)).toBeInstanceOf(TwoPiInvestment);
   });
 });
--- src/app/modules/defi-investments/create/investment-confirmation/investment-confirmation.page.ts
+++ src/app/modules/defi-investments/create/investment-confirmation/investment-confirmation.page.ts
@@ -1,4 +1,7 @@
-import { TwoPiInvestment } from '../../shared-defi-investments/models/two-pi-investment/two-pi-investment.model';
+import {
+  Investment,
+  TwoPiInvestment,
+} from '../../shared-defi-investments/models/two-pi-investment/two-pi-investment.model';
 import { InvestmentProduct } from '../../shared-defi-investments/interfaces/investment-product.interface';
 import { TranslateService } from '@ngx-translate/core';
 import { WalletPasswordComponent } from '../../../wallets/shared-wallets/components/wallet-password/wallet-password.component';
@@ -8,6 +11,8 @@ import { Component } from '@angular/core';
 import { InvestmentDataService } from '../../shared-defi-investments/services/investment-data/investment-data.service';
 import { Amount } from '../../shared-defi-investments/types/amount.type';
 import { WalletEncryptionService } from 'src/app/modules/wallets/shared-wallets/services/wallet-encryption/wallet-encryption.service';
+import { Wallet } from 'ethers';
+import { ToastService } from 'src/app/shared/services/toast/toast.service';
 
 @Component({
   selector: 'app-investment-confirmation',
@@ -32,15 +37,15 @@ import { WalletEncryptionService } from 'src/app/modules/wallets/shared-wallets/
             </div>
 
             <div class="summary__amount__qty">
-              <ion-text class="ux-font-text-base"
-                >{{ this.amount.value | number: '1.2-2' }} {{ this.amount.token }}</ion-text
+              <ion-text class="ux-font-text-base summary__amount__qty__amount"
+                >{{ this.amount.value | number: '1.2-6' }} {{ this.amount.token }}</ion-text
               >
-              <ion-text class="ux-font-text-base"
-                >{{ this.quoteAmount.value | number: '1.2-2' }} {{ this.quoteAmount.token }}
+              <ion-text class="ux-font-text-base summary__amount__qty__quoteAmount"
+                >{{ this.quoteAmount.value | number: '1.2-6' }} {{ this.quoteAmount.token }}
               </ion-text>
             </div>
           </div>
-          <div class="summary__fee">
+          <div class="summary__fee" *ngIf="this.fee">
             <div class="summary__fee__label">
               <ion-text class="ux-font-titulo-xs">{{
                 'defi_investments.confirmation.transaction_fee' | translate
@@ -48,10 +53,10 @@ import { WalletEncryptionService } from 'src/app/modules/wallets/shared-wallets/
             </div>
 
             <div class="summary__fee__qty">
-              <ion-text class="ux-font-text-base"
+              <ion-text class="ux-font-text-base summary__fee__qty__amount"
                 >{{ this.amount.value | number: '1.2-2' }} {{ this.amount.token }}</ion-text
               >
-              <ion-text class="ux-font-text-base"
+              <ion-text class="ux-font-text-base summary__fee__qty__quoteFee"
                 >{{ this.quoteAmount.value | number: '1.2-2' }} {{ this.quoteAmount.token }}
               </ion-text>
             </div>
@@ -69,7 +74,7 @@ import { WalletEncryptionService } from 'src/app/modules/wallets/shared-wallets/
         type="submit"
         class="ion-padding-start ion-padding-end ux_button"
         color="uxsecondary"
-        (click)="this.requestPassword()"
+        (click)="this.invest()"
       >
         {{ 'defi_investments.confirmation.submit' | translate }}
       </ion-button>
@@ -91,7 +96,8 @@ export class InvestmentConfirmationPage {
     private modalController: ModalController,
     private translate: TranslateService,
     private walletEncryptionService: WalletEncryptionService,
-    private navController: NavController
+    private navController: NavController,
+    private toastService: ToastService
   ) {}
 
   async ionViewDidEnter() {
@@ -121,23 +127,43 @@ export class InvestmentConfirmationPage {
       cssClass: 'ux-routeroutlet-modal small-wallet-password-modal',
     });
     await modal.present();
-    const { data } = await modal.onDidDismiss();
-    this.loadingEnabled(true);
-    await this.deposit(data);
+    const { data } = await modal.onWillDismiss();
+    return data;
   }
 
-  async deposit(data: string) {
-    const investment = TwoPiInvestment.create(
-      this.product,
-      await this.walletEncryptionService.getDecryptedWalletForCurrency(data, this.product.token())
-    );
+  investment(wallet: Wallet): Investment {
+    return TwoPiInvestment.create(this.product, wallet);
+  }
+
+  async decryptedWallet(password: string): Promise<Wallet> {
     try {
-      await (await investment.deposit(this.amount.value)).wait();
-      await this.navController.navigateForward('/defi/success-investment');
+      return await this.walletEncryptionService.getDecryptedWalletForCurrency(password, this.product.token());
     } catch {
-      await this.navController.navigateForward('/defi/error-investment');
-    } finally {
       this.loadingEnabled(false);
+      await this.toastService.showErrorToast({
+        message: this.translate.instant('defi_investments.confirmation.password_error'),
+      });
+    }
+  }
+  async wallet(): Promise<Wallet | void> {
+    const password = await this.requestPassword();
+    if (password) {
+      this.loadingEnabled(true);
+      return await this.decryptedWallet(password);
+    }
+  }
+
+  async invest() {
+    const wallet = await this.wallet();
+    if (wallet) {
+      try {
+        await (await this.investment(wallet).deposit(this.amount.value)).wait();
+        await this.navController.navigateForward('/defi/success-investment');
+      } catch {
+        await this.navController.navigateForward('/defi/error-investment');
+      } finally {
+        this.loadingEnabled(false);
+      }
     }
   }
 
