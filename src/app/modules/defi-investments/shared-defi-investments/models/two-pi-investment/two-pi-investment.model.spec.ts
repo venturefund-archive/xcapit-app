4dcd1ba09337caab7d9f5ab68113d45e76f714d4
Franco Schillage <fschillage@gmail.com>
2022-01-28 10:26:26 -0300

    chore: Refactor balance in twoPiInvestment

--- src/app/modules/defi-investments/shared-defi-investments/models/two-pi-investment/two-pi-investment.model.spec.ts
+++ src/app/modules/defi-investments/shared-defi-investments/models/two-pi-investment/two-pi-investment.model.spec.ts
@@ -1,6 +1,6 @@
 import { TwoPiInvestment } from './two-pi-investment.model';
 import { InvestmentProduct } from '../../interfaces/investment-product.interface';
-import { BigNumber, Contract, Wallet } from 'ethers';
+import { BigNumber, Contract, Wallet, VoidSigner, Signer } from 'ethers';
 import { ERC20Token } from '../erc20-token/erc20-token.model';
 import { ERC20Provider } from '../erc20-provider/erc20-provider.model';
 import { TransactionResponse } from '@ethersproject/abstract-provider';
@@ -8,18 +8,28 @@ import { TwoPiContract } from '../two-pi-contract/two-pi-contract.model';
 
 const contractAddress = '0xCB50fF1863cBBAd718d3A1eEEf403a95C58d3B16';
 const referralAddress = 'referralAddress';
+const testAddress = '0x0001';
 
 describe('TwoPiInvestment', () => {
   let productSpy: jasmine.SpyObj<InvestmentProduct>;
-  let wallet: Wallet;
+  let wallet: Signer;
   let erc20TokenSpy: jasmine.SpyObj<ERC20Token>;
   let erc20ProviderSpy: jasmine.SpyObj<ERC20Provider>;
   let twoPiContractSpy: jasmine.SpyObj<TwoPiContract>;
   let contractSpy: jasmine.SpyObj<Contract>;
   let twoPiInvestment: TwoPiInvestment;
   beforeEach(() => {
-    contractSpy = jasmine.createSpyObj('Contract', { deposit: Promise.resolve({} as TransactionResponse) });
-    productSpy = jasmine.createSpyObj('InvestmentProduct', { id: 1, token: { decimals: 6 }, contractAddress });
+    contractSpy = jasmine.createSpyObj('Contract', {
+      deposit: Promise.resolve({} as TransactionResponse),
+      balanceOf: Promise.resolve(BigNumber.from('50000000')),
+      getPricePerFullShare: Promise.resolve(BigNumber.from('1000000')),
+    });
+    productSpy = jasmine.createSpyObj('InvestmentProduct', {
+      id: 1,
+      token: { decimals: 6 },
+      contractAddress,
+      decimals: 6,
+    });
     wallet = Wallet.fromMnemonic(
       'clever brain critic belt soldier daring own luxury begin plate orange banana',
       "m/44'/80001'/0'/0/0"
@@ -37,7 +47,7 @@ describe('TwoPiInvestment', () => {
     );
   });
 
-  it('should create', () => {
+  it('should create with signer', () => {
     expect(twoPiInvestment).toBeTruthy();
   });
 
@@ -45,10 +55,29 @@ describe('TwoPiInvestment', () => {
     expect(TwoPiInvestment.create(productSpy, wallet)).toBeTruthy();
   });
 
-  it('should deposit', async () => {
+  it('should deposit specified amount on investment product contract', async () => {
     const wei = BigNumber.from('50000000');
     await twoPiInvestment.deposit(50);
     expect(erc20TokenSpy.approve).toHaveBeenCalledOnceWith(contractAddress, wei);
     expect(contractSpy.deposit).toHaveBeenCalledOnceWith(1, wei, referralAddress);
+  });
+
+  it('should return the balance of a wallet in the investment product', async () => {
+    twoPiInvestment = new TwoPiInvestment(
+      productSpy,
+      new VoidSigner(testAddress),
+      erc20TokenSpy,
+      erc20ProviderSpy,
+      twoPiContractSpy,
+      referralAddress
+    );
+    const balance = await twoPiInvestment.balance();
+    expect(contractSpy.balanceOf).toHaveBeenCalledOnceWith(1, '0x0001');
+    expect(contractSpy.getPricePerFullShare).toHaveBeenCalledOnceWith(1);
+    expect(balance).toEqual(50);
+  });
+
+  it('should withdraw', () => {
+    expect(twoPiInvestment.withdraw()).toBeUndefined();
   });
 });
