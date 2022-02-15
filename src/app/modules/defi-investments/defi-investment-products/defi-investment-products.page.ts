019ac2c782769dc1183805e2ebf94d24250c7cc3
Federico Marquez <federico@xcapit.com>
2022-02-15 16:35:35 -0300

    feat(DefiInvestments): Add navigation to new component and connect page

--- src/app/modules/defi-investments/defi-investment-products/defi-investment-products.page.ts
+++ src/app/modules/defi-investments/defi-investment-products/defi-investment-products.page.ts
@@ -10,6 +10,7 @@ import { TwoPiApi } from '../shared-defi-investments/models/two-pi-api/two-pi-ap
 import { TwoPiProduct } from '../shared-defi-investments/models/two-pi-product/two-pi-product.model';
 import { VoidSigner } from 'ethers';
 import { WalletService } from '../../wallets/shared-wallets/services/wallet/wallet.service';
+import { ApiUsuariosService } from '../../usuarios/shared-usuarios/services/api-usuarios/api-usuarios.service';
 
 @Component({
   selector: 'app-defi-investment-products',
@@ -55,15 +56,17 @@ import { WalletService } from '../../wallets/shared-wallets/services/wallet/wall
           ></app-defi-investment-product>
         </div>
       </div>
-      <app-choose-investor-profile-card *ngIf="this.activeInvestments.length || this.availableInvestments.length"></app-choose-investor-profile-card>
+      <app-choose-investor-profile-card [hasDoneInvestorTest]="this.hasDoneInvestorTest" *ngIf="this.activeInvestments.length || this.availableInvestments.length"></app-choose-investor-profile-card>
     </ion-content>
   `,
   styleUrls: ['./defi-investment-products.page.scss'],
 })
 export class DefiInvestmentProductsPage {
   defiProducts: DefiProduct[];
+  investorCategory: string;
   constructor(
     private apiWalletService: ApiWalletService,
+    private apiUsuariosService: ApiUsuariosService,
     private twoPiApi: TwoPiApi,
     private walletEncryptionService: WalletEncryptionService,
     private walletService: WalletService
@@ -72,9 +75,23 @@ export class DefiInvestmentProductsPage {
   activeInvestments: DefiInvestment[] = [];
   availableInvestments: DefiInvestment[] = [];
 
+  get hasDoneInvestorTest(): boolean {
+    return this.investorCategory !== 'wealth_managements.profiles.no_category';
+  }
+
+  ionViewWillEnter() {
+    this.getUser();
+  }
+
   async ionViewDidEnter() {
     this.getAvailableDefiProducts();
     await this.getInvestments();
+  }
+
+  getUser() {
+    this.apiUsuariosService.getUser().subscribe(user => {
+      this.investorCategory = user.profile.investor_category;
+    });
   }
 
   private getAvailableDefiProducts(): void {
--- src/app/modules/defi-investments/shared-defi-investments/components/choose-investor-profile-card/choose-investor-profile-card.component.ts
+++ src/app/modules/defi-investments/shared-defi-investments/components/choose-investor-profile-card/choose-investor-profile-card.component.ts
@@ -1,26 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ApiUsuariosService } from 'src/app/modules/usuarios/shared-usuarios/services/api-usuarios/api-usuarios.service';
 
 @Component({
   selector: 'app-choose-investor-profile-card',
   template: `
     <ion-card class="cipc">
       <div class="cipc__title">
-        <ion-text class="ux-font-header-titulo">
+        <ion-text name="Title" class="ux-font-header-titulo">
           {{ this.titleText | translate }}
         </ion-text>
       </div>
       <div class="cipc__text">
-        <ion-text class="ux-font-text-xs">
+        <ion-text name="Description" class="ux-font-text-xs">
           {{ this.descriptionText | translate }}
         </ion-text>
       </div>
       <div class="cipc__button_primary button-container">
-        <ion-button class="ux_button ion-no-padding" size="small" color="uxsecondary">
+        <ion-button appTrackClick name="Primary Button" class="ux_button ion-no-padding" size="small" color="uxsecondary" (click)="this.primaryAction()">
           {{ this.buttonPrimaryText | translate }}
         </ion-button>
       </div>
       <div class="cipc__button_secondary button-container">
-        <ion-button class="ux_button ion-no-padding" size="small" fill="clear" color="info">
+        <ion-button appTrackClick name="Secondary Button" class="ux_button ion-no-padding" size="small" fill="clear" color="info" (click)="this.secondaryAction()">
           {{ this.buttonSecondaryText | translate }}
         </ion-button>
       </div>
@@ -29,11 +31,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./choose-investor-profile-card.component.scss'],
 })
 export class ChooseInvestorProfileCardComponent implements OnInit {
-  titleText: string = 'defi_investments.defi_investment_products.choose_investor_profile.complete_test.title';
-  descriptionText: string = 'defi_investments.defi_investment_products.choose_investor_profile.complete_test.description';
-  buttonPrimaryText: string = 'defi_investments.defi_investment_products.choose_investor_profile.complete_test.button_primary';
-  buttonSecondaryText: string = 'defi_investments.defi_investment_products.choose_investor_profile.complete_test.button_secondary';
-  constructor() {}
+  @Input() hasDoneInvestorTest: boolean;
+  private get translationBaseRoute(): string {
+    const base = 'defi_investments.defi_investment_products.choose_investor_profile';
+    
+    if (this.hasDoneInvestorTest) {
+      return `${base}.complete_test`;
+    }
+
+    return `${base}.incomplete_test`;
+  }
+
+  get titleText(): string {
+    return `${this.translationBaseRoute}.title`;
+  } 
+
+  get descriptionText(): string {
+    return `${this.translationBaseRoute}.description`;
+  }
+
+  get buttonPrimaryText(): string {
+    return `${this.translationBaseRoute}.button_primary`;
+  }
+
+  get buttonSecondaryText(): string {
+    return `${this.translationBaseRoute}.button_secondary`;
+  }
+
+  constructor(
+    private navController: NavController
+  ) {}
 
   ngOnInit() {}
+
+  primaryAction() {
+    if (this.hasDoneInvestorTest) {
+      this.goToSelectInvestorProfile();
+    } else {
+      this.goToInvestorTest();
+    }
+  }
+
+  secondaryAction() {
+    if (this.hasDoneInvestorTest) {
+      this.goToInvestorTest();
+    } else {
+      this.goToSelectInvestorProfile();
+    }
+  }
+
+  private goToInvestorTest() {
+    this.navController.navigateForward(['/wealth-management/investor-test']);
+  }
+
+  private goToSelectInvestorProfile() {
+    this.navController.navigateForward(['/wealth-management/about-investor-profiles']);
+  }
 }
