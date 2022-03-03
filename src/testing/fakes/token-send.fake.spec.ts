cd39ce9f305bf6dfb02001c152f9aab225cb8231
Martin317 <martin@xcapit.com>
2022-03-03 09:28:24 -0300

    feat(wallets): TIC-234 Fix static method order

--- src/testing/fakes/token-send.fake.spec.ts
+++ src/testing/fakes/token-send.fake.spec.ts
@@ -3,15 +3,15 @@ import { parseEther } from 'ethers/lib/utils';
 export class FakeTokenSend {
   constructor() {}
 
+  static create(): FakeTokenSend {
+    return new this();
+  }
+
   value() {
     return {
       send: () => Promise.resolve(),
       sendEstimateFee: () => Promise.resolve(parseEther('0.00008')),
       formatFee: () => '0.00012',
     };
-  }
-
-  static create(): FakeTokenSend {
-    return new this();
   }
 }
