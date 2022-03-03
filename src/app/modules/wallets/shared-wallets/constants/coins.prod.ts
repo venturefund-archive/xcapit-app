5a6d627d6d7d367e266bd6210c7f4b6bca7cd3d6
Franco Schillage <fschillage@gmail.com>
2022-03-03 17:17:16 -0300

    feat: SX-1955 - List WBTC - Polygon token

--- src/app/modules/wallets/shared-wallets/constants/coins.prod.ts
+++ src/app/modules/wallets/shared-wallets/constants/coins.prod.ts
@@ -371,7 +371,7 @@ export const PROD_COINS: Coin[] = [
     id: 27,
     name: 'WETH - Wrapped Ether',
     logoRoute: 'assets/img/coins/WETH.svg',
-    last: true,
+    last: false,
     value: 'WETH',
     network: 'MATIC',
     chainId: 137,
@@ -381,4 +381,19 @@ export const PROD_COINS: Coin[] = [
     decimals: 18,
     symbol: 'ETHUSDT',
   },
+  {
+    id: 28,
+    name: 'WBTC - Wrapped BTC',
+    logoRoute: 'assets/img/coins/WBTC.png',
+    last: true,
+    value: 'BTC',
+    network: 'MATIC',
+    chainId: 137,
+    rpc: environment.maticApiUrl,
+    abi: linkAbi,
+    contract: '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6',
+    decimals: 8,
+    symbol: 'BTCUSDT',
+  },
+  
 ];
