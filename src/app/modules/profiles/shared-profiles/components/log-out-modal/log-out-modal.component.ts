fb5d6657dfa81d40ef9c27ae9edcb07bc2d41ae9
Federico Marquez <federico@xcapit.com>
2022-03-09 08:58:30 -0300

    style(Profiles): Add language texts and translations

--- src/app/modules/profiles/shared-profiles/components/log-out-modal/log-out-modal.component.ts
+++ src/app/modules/profiles/shared-profiles/components/log-out-modal/log-out-modal.component.ts
@@ -6,26 +6,26 @@ import { Component, OnInit } from '@angular/core';
   <div class="ux-card lom">
     <div class="ion-padding lom__content">
       <div class="lom__content__title">
-        <ion-text class="ux-font-text-lg">Titulo</ion-text>
+        <ion-text class="ux-font-text-lg">{{ 'profiles.user_profile_menu.log_out_modal.title' | translate }}</ion-text>
       </div>
       <div class="lom__content__text">
-        <ion-text class="ux-font-text-base">Texto</ion-text>
+        <ion-text class="ux-font-text-base">{{ 'profiles.user_profile_menu.log_out_modal.text' | translate }}</ion-text>
       </div>
       <div class="lom__content__more-info">
-        <ion-text class="ux-font-text-base">Texto2</ion-text><br>
+        <ion-text class="ux-font-text-base">{{ 'profiles.user_profile_menu.log_out_modal.more_info' | translate }}</ion-text><br>
         <ion-button class="ux-link-xs ion-no-padding ion-no-margin" fill="clear">
-          Link
+          {{ 'profiles.user_profile_menu.log_out_modal.more_info_link' | translate }}
         </ion-button>
       </div>
       <div class="lom__content__checkbox">
         <ion-item class="ion-no-padding ion-no-margin" lines="none">
           <ion-checkbox mode="md" slot="start"></ion-checkbox>
-          <ion-label class="ux-font-text-base">No volver</ion-label>
+          <ion-label class="ux-font-text-base">{{ 'profiles.user_profile_menu.log_out_modal.checkbox_label' | translate }}</ion-label>
         </ion-item>
       </div>
     </div>
     <div class="lom__close-button">
-      <ion-button class="ux_button" fill="clear">Cerrar</ion-button>
+      <ion-button class="ux_button" fill="clear">{{ 'profiles.user_profile_menu.log_out_modal.log_out_button' | translate }}</ion-button>
     </div>
   </div>
   `,
