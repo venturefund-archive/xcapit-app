34c4926953b85636d26a90d0c4fc4d3f5cd37273
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-03-17 19:07:42 -0300

    style: Apply prettier

--- src/app/modules/wallets/nft-detail/nft-detail.page.spec.ts
+++ src/app/modules/wallets/nft-detail/nft-detail.page.spec.ts
@@ -48,59 +48,57 @@ describe('NftDetailPage', () => {
   let nftServiceSpy: jasmine.SpyObj<NftService>;
   let routeSpy: jasmine.SpyObj<Router>;
 
-  beforeEach(
-    waitForAsync(() => {
-      routeSpy = jasmine.createSpyObj('Router', ['getCurrentNavigation']);
-      nftServiceSpy = jasmine.createSpyObj('NftService', {
-        xcapitNFTs: Promise.resolve(nft),
-        getNFTMexico: NFT_DATA_NONPROD,
-      });
-      fakeNavController = new FakeNavController();
-      navControllerSpy = fakeNavController.createSpy();
-      TestBed.configureTestingModule({
-        declarations: [NftDetailPage],
-        imports: [
-          IonicModule.forRoot(),
-          TranslateModule.forRoot(),
-          HttpClientTestingModule,
-          ReactiveFormsModule,
-          RouterTestingModule,
-        ],
-        providers: [
-          { provide: NavController, useValue: navControllerSpy },
-          { provide: NftService, useValue: nftServiceSpy },
-          { provide: Router, useValue: routeSpy },
-        ],
-        schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      }).compileComponents();
+  beforeEach(waitForAsync(() => {
+    routeSpy = jasmine.createSpyObj('Router', ['getCurrentNavigation']);
+    nftServiceSpy = jasmine.createSpyObj('NftService', {
+      xcapitNFTs: Promise.resolve(nft),
+      getNFTMexico: NFT_DATA_NONPROD,
+    });
+    fakeNavController = new FakeNavController();
+    navControllerSpy = fakeNavController.createSpy();
+    TestBed.configureTestingModule({
+      declarations: [NftDetailPage],
+      imports: [
+        IonicModule.forRoot(),
+        TranslateModule.forRoot(),
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        { provide: NavController, useValue: navControllerSpy },
+        { provide: NftService, useValue: nftServiceSpy },
+        { provide: Router, useValue: routeSpy },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
 
-      fixture = TestBed.createComponent(NftDetailPage);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    })
-  );
+    fixture = TestBed.createComponent(NftDetailPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-      it('should render properly of nft card when nft metadata is passed by state', async () => {
-          routeSpy.getCurrentNavigation.and.returnValue(getTestNavigationState(true));
-      fixture = TestBed.createComponent(NftDetailPage);
-      component = fixture.componentInstance;
-      component.ionViewWillEnter();
-      await fixture.whenStable();
-      await fixture.whenRenderingDone();
-      fixture.detectChanges();
-      const imageEl = fixture.debugElement.query(By.css('img.nd__image'));
-      const titleEl = fixture.debugElement.query(By.css('ion-text.nd__title'));
-      const descriptionEl = fixture.debugElement.query(By.css('ion-text.nd__subtitle'));
+  it('should render properly of nft card when nft metadata is passed by state', async () => {
+    routeSpy.getCurrentNavigation.and.returnValue(getTestNavigationState(true));
+    fixture = TestBed.createComponent(NftDetailPage);
+    component = fixture.componentInstance;
+    component.ionViewWillEnter();
+    await fixture.whenStable();
+    await fixture.whenRenderingDone();
+    fixture.detectChanges();
+    const imageEl = fixture.debugElement.query(By.css('img.nd__image'));
+    const titleEl = fixture.debugElement.query(By.css('ion-text.nd__title'));
+    const descriptionEl = fixture.debugElement.query(By.css('ion-text.nd__subtitle'));
 
-      expect(imageEl.attributes.src).toEqual(NFTMetadata.image);
-      expect(titleEl.nativeElement.innerHTML).toContain(NFTMetadata.name);
-      expect(descriptionEl.nativeElement.innerHTML).toContain(NFTMetadata.description);
-      expect(component.form.value.tokenID).toEqual(NFTMetadata.tokenID);
-      expect(component.form.value.contractAddress).toEqual(NFT_DATA_NONPROD[0].contractAddress);
-      expect(nftServiceSpy.xcapitNFTs).toHaveBeenCalledTimes(0);
-    });
+    expect(imageEl.attributes.src).toEqual(NFTMetadata.image);
+    expect(titleEl.nativeElement.innerHTML).toContain(NFTMetadata.name);
+    expect(descriptionEl.nativeElement.innerHTML).toContain(NFTMetadata.description);
+    expect(component.form.value.tokenID).toEqual(NFTMetadata.tokenID);
+    expect(component.form.value.contractAddress).toEqual(NFT_DATA_NONPROD[0].contractAddress);
+    expect(nftServiceSpy.xcapitNFTs).toHaveBeenCalledTimes(0);
+  });
 });
