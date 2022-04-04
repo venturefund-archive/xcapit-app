6cd051ebf542d9ab4741122f5b8c716d45e2cf54
Anna Arevalo <anna@xcapit.com>
2022-04-04 11:56:26 -0300

    feat(transactions): hash and date are now added

--- src/app/modules/wallets/shared-wallets/components/wallet-transaction-card-item/wallet-transaction-card-item.component.ts
+++ src/app/modules/wallets/shared-wallets/components/wallet-transaction-card-item/wallet-transaction-card-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { format, parseISO } from 'date-fns';
 
 @Component({
   selector: 'app-wallet-transaction-card-item',
@@ -10,9 +11,21 @@ import { Component, Input, OnInit } from '@angular/core';
         </div>
         <div class="wtci__content">
           <div class="wtci__content__top">
-            <ion-label class="ux-font-lato ux-fsize-14 ux-fweight-bold">{{
-              'wallets.transactions.' + this.transaction.type | translate
-            }}</ion-label>
+            <div class="wtci__content__top__type_date_hash">
+              <div class="wtci__content__top__type_date_hash__type_date">
+                <ion-label class="ux-font-lato ux-fsize-14 ux-fweight-bold">{{
+                  'wallets.transactions.' + this.transaction.type | translate
+                }}</ion-label>
+                <ion-label class="ux-font-text-xxs date">
+                  {{ this.formattedDate}}
+                </ion-label>
+              </div>
+              <div class="wtci__content__top__type_date_hash__hash">
+                <ion-text class="ux-font-text-xs">
+                  {{ this.transaction.hash }}
+                </ion-text>
+              </div>
+            </div>
             <ion-label class="ux-font-lato ux-fsize-14 ux-fweight-semibold">
               {{ this.transaction.amount | number: '1.2-6' }} {{ this.transaction.symbol }}
             </ion-label>
@@ -27,8 +40,14 @@ import { Component, Input, OnInit } from '@angular/core';
 export class WalletTransactionCardItemComponent implements OnInit {
   @Input() transaction;
   @Input() last: boolean;
+  formattedDate
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.formattedDate = this.formatDate(this.transaction.date)
+  }
+  formatDate(value) {
+  return format(parseISO(value), 'dd-MM-yyyy');
+  }
 }
