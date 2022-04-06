01ad9409a5fdf0a0eed20c05d24e7f162a5b18bf
Federico Marquez <federico@xcapit.com>
2022-04-06 08:36:08 -0300

    feat(Shared): Refactor update service and add update plugin support

--- src/app/shared/components/update-app-modal/update-app-modal.component.spec.ts
+++ src/app/shared/components/update-app-modal/update-app-modal.component.spec.ts
@@ -1,24 +1,84 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
+import { By } from '@angular/platform-browser';
+import { IonicModule, ModalController } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
+import { FakeModalController } from 'src/testing/fakes/modal-controller.fake.spec';
+import { FakeTrackClickDirective } from 'src/testing/fakes/track-click-directive.fake.spec';
+import { TrackClickDirectiveTestHelper } from 'src/testing/track-click-directive-test.spec';
 import { UpdateAppModalComponent } from './update-app-modal.component';
 
 describe('UpdateAppModalComponent', () => {
   let component: UpdateAppModalComponent;
   let fixture: ComponentFixture<UpdateAppModalComponent>;
+  let fakeModalController: FakeModalController;
+  let modalControllerSpy: jasmine.SpyObj<ModalController>;
+  let trackClickDirectiveHelper: TrackClickDirectiveTestHelper<UpdateAppModalComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ UpdateAppModalComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      fakeModalController = new FakeModalController();
+      modalControllerSpy = fakeModalController.createSpy();
+      TestBed.configureTestingModule({
+        declarations: [UpdateAppModalComponent, FakeTrackClickDirective],
+        imports: [IonicModule.forRoot(), TranslateModule.forRoot()],
+        providers: [
+          { provide: ModalController, useValue: modalControllerSpy }
+        ],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(UpdateAppModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(UpdateAppModalComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+      trackClickDirectiveHelper = new TrackClickDirectiveTestHelper(fixture);
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should close modal when Close button clicked', () => {
+    fixture.debugElement.query(By.css('ion-button[name="Close"]')).nativeElement.click();
+    expect(modalControllerSpy.dismiss).toHaveBeenCalledOnceWith(false);
+  });
+
+  it('should open store when Update App button clicked', async () => {
+    fixture.debugElement.query(By.css('ion-button[name="Update App"]')).nativeElement.click();
+    await fixture.whenStable();
+    expect(modalControllerSpy.dismiss).toHaveBeenCalledOnceWith(true);
+  });
+
+  it('should close modal when Remind Me Later button clicked', () => {
+    fixture.debugElement.query(By.css('ion-button[name="Remind Me Later"]')).nativeElement.click();
+    expect(modalControllerSpy.dismiss).toHaveBeenCalledOnceWith(false);
+  });
+
+  it('should call trackEvent on trackService when Close is clicked', () => {
+    const el = trackClickDirectiveHelper.getByElementByName('ion-button', 'Close');
+    const directive = trackClickDirectiveHelper.getDirective(el);
+    const spyClickEvent = spyOn(directive, 'clickEvent');
+    el.nativeElement.click();
+    fixture.detectChanges();
+    expect(spyClickEvent).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call trackEvent on trackService when Update App is clicked', () => {
+    const el = trackClickDirectiveHelper.getByElementByName('ion-button', 'Update App');
+    const directive = trackClickDirectiveHelper.getDirective(el);
+    const spyClickEvent = spyOn(directive, 'clickEvent');
+    el.nativeElement.click();
+    fixture.detectChanges();
+    expect(spyClickEvent).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call trackEvent on trackService when Remind Me Later is clicked', () => {
+    const el = trackClickDirectiveHelper.getByElementByName('ion-button', 'Remind Me Later');
+    const directive = trackClickDirectiveHelper.getDirective(el);
+    const spyClickEvent = spyOn(directive, 'clickEvent');
+    el.nativeElement.click();
+    fixture.detectChanges();
+    expect(spyClickEvent).toHaveBeenCalledTimes(1);
   });
 });
--- src/app/shared/components/update-app-modal/update-app-modal.component.ts
+++ src/app/shared/components/update-app-modal/update-app-modal.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ModalController } from '@ionic/angular';
+import { UpdateService } from '../../services/update/update.service';
 @Component({
   selector: 'app-update-app-modal',
   template: `
     <div class="ion-padding uam__content">
       <div class="uam__content__button-close">
-        <ion-button fill="clear" color="neutral80" size="medium" class="ion-no-padding ion-no-margin">
+        <ion-button appTrackClick (click)="this.close()" name="Close" fill="clear" color="neutral80" size="medium" class="ion-no-padding ion-no-margin">
           <ion-icon name="ux-close"></ion-icon>
         </ion-button>
       </div>
@@ -20,12 +21,12 @@ import { Component, OnInit } from '@angular/core';
         </ion-text>
       </div>
       <div class="uam__content__button-primary">
-        <ion-button class="ux_button ion-no-padding ion-no-margin" color="secondary" expand="block">
+        <ion-button appTrackClick (click)="this.openStore()" name="Update App" class="ux_button ion-no-padding ion-no-margin" color="secondary" expand="block">
           {{ 'shared.update_modal.button_primary' | translate }}
         </ion-button>
       </div>
       <div class="uam__content__button-secondary">
-        <ion-button class="ux_button ion-no-padding ion-no-margin" color="info" fill="clear" expand="block">
+        <ion-button appTrackClick (click)="this.close()" name="Remind Me Later" class="ux_button ion-no-padding ion-no-margin" color="info" fill="clear" expand="block">
           {{ 'shared.update_modal.button_secondary' | translate }}
         </ion-button>
       </div>
@@ -34,7 +35,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-app-modal.component.scss'],
 })
 export class UpdateAppModalComponent implements OnInit {
-  constructor() {}
+  constructor(private modalController: ModalController) {}
 
   ngOnInit() {}
+
+  openStore() {
+    this.modalController.dismiss(true);
+  }
+
+  close() {
+    this.modalController.dismiss(false);
+  }
 }
