90b571b9fea76e6afabcd32ff5781e9bbcaf6631
Federico Marquez <federico@xcapit.com>
2022-04-18 16:38:26 -0300

    feat(FiatRamps): Add text for no operations

--- src/app/modules/fiat-ramps/shared-ramps/components/operations-list/operations-list.component.ts
+++ src/app/modules/fiat-ramps/shared-ramps/components/operations-list/operations-list.component.ts
@@ -5,67 +5,87 @@ import { Operation } from '../../interfaces/operation.interface';
   selector: 'app-operations-list',
   template: `
     <ion-card class="ux-card">
-      <ion-card-header>
+      <ion-card-header [ngClass]="this.cssWithLine">
         <ion-card-title class="ux-font-text-lg">{{ 'fiat_ramps.operations_list.title' | translate }}</ion-card-title>
       </ion-card-header>
       <ion-card-content>
-        <ion-list>
-          <ion-item lines="none" class="header-row">
-            <ion-label>
-              <ion-text class="ux-font-text-xxs" color="neutral80">
-                {{ 'fiat_ramps.operations_list.operation' | translate }}
-              </ion-text>
-            </ion-label>
-            <ion-label>
-              <ion-text class="ux-font-text-xxs" color="neutral80">
-                {{ 'fiat_ramps.operations_list.amount' | translate }}
-              </ion-text>
-            </ion-label>
-            <ion-label>
-              <ion-text class="ux-font-text-xxs" color="neutral80">
-                {{ 'fiat_ramps.operations_list.date' | translate }}
-              </ion-text>
-            </ion-label>
-            <ion-label>
-              <ion-text class="ux-font-text-xxs" color="neutral80">
-                {{ 'fiat_ramps.operations_list.status' | translate }}
-              </ion-text>
-            </ion-label>
-          </ion-item>
-          <div *ngFor="let op of this.operationsList; let last = last">
-            <ion-item class="row" (click)="viewOperationDetail(op)">
+        <div *ngIf="this.hasOperations; then operationsTable; else noOperationsMessage"></div>
+        <ng-template #operationsTable>
+          <ion-list>
+            <ion-item lines="none" class="header-row">
               <ion-label>
-                <ion-text class="">
-                  <img [src]="op.provider.logoRoute" alt="{{ op.provider.name }}" />
-                  {{ op.currency_in }}
-                </ion-text>
-              </ion-label>
-              <ion-label *ngIf="op.operation_type === 'cash-in'; then cashInAmount; else cashOutAmount">
-              </ion-label>
-                <ng-template #cashInAmount>
-                  <ion-text class="ux-font-titulo-xs">{{ op.amount_in | currency }}</ion-text>
-                </ng-template>
-                <ng-template #cashOutAmount>
-                  <ion-text class="ux-font-titulo-xs">{{ op.amount_out | currency }}</ion-text>
-                </ng-template>
-              <ion-label>
-                <ion-text class="">
-                  {{ op.created_at | date: 'dd/MM/yy' }}
+                <ion-text class="ux-font-text-xxs" color="neutral80">
+                  {{ 'fiat_ramps.operations_list.operation' | translate }}
                 </ion-text>
               </ion-label>
               <ion-label>
-                <app-operation-status-chip [status]="op.status"></app-operation-status-chip>
+                <ion-text class="ux-font-text-xxs" color="neutral80">
+                  {{ 'fiat_ramps.operations_list.amount' | translate }}
+                </ion-text>
+              </ion-label>
+              <ion-label>
+                <ion-text class="ux-font-text-xxs" color="neutral80">
+                  {{ 'fiat_ramps.operations_list.date' | translate }}
+                </ion-text>
+              </ion-label>
+              <ion-label>
+                <ion-text class="ux-font-text-xxs" color="neutral80">
+                  {{ 'fiat_ramps.operations_list.status' | translate }}
+                </ion-text>
               </ion-label>
             </ion-item>
-          </div>
-        </ion-list>
+            <div *ngFor="let op of this.operationsList; let last = last">
+              <ion-item class="row" (click)="viewOperationDetail(op)">
+                <ion-label>
+                  <ion-text class="ux-font-text-xs">
+                    <img [src]="op.provider.logoRoute" alt="{{ op.provider.name }}" />
+                    {{ op.currency_in }}
+                  </ion-text>
+                </ion-label>
+                <ion-label *ngIf="op.operation_type === 'cash-in'; then cashInAmount; else cashOutAmount">
+                </ion-label>
+                  <ng-template #cashInAmount>
+                    <ion-text class="ux-font-titulo-xs">{{ op.amount_in | currency }}</ion-text>
+                  </ng-template>
+                  <ng-template #cashOutAmount>
+                    <ion-text class="ux-font-titulo-xs">{{ op.amount_out | currency }}</ion-text>
+                  </ng-template>
+                <ion-label>
+                  <ion-text class="ux-font-text-xs">
+                    {{ op.created_at | date: 'dd/MM/yy' }}
+                  </ion-text>
+                </ion-label>
+                <ion-label>
+                  <app-operation-status-chip [status]="op.status"></app-operation-status-chip>
+                </ion-label>
+              </ion-item>
+            </div>
+          </ion-list>
+        </ng-template>
+        <ng-template #noOperationsMessage>
+          <ion-text class="no-operations-text ux-font-text-base">
+            {{ 'fiat_ramps.operations_list.no_operations_message' | translate }}
+          </ion-text>
+        </ng-template>
       </ion-card-content>
     </ion-card>
-  `,
+  `, 
   styleUrls: ['./operations-list.component.scss'],
 })
 export class OperationsListComponent implements OnInit {
   @Input() operationsList: Operation[];
+
+  get cssWithLine(): string {
+    if (this.hasOperations) {
+      return 'with-line';
+    }
+
+    return '';
+  }
+
+  get hasOperations(): boolean {
+    return this.operationsList?.length > 0;
+  }
 
   constructor() {}
 
