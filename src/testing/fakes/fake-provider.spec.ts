e04ea84a14aacfb57b3654ad96e63726fed3dc01
Franco Schillage <franco@xcapit.com>
2022-04-27 15:07:24 -0300

    feat: Add getGasPrice to FakeProvider

--- src/testing/fakes/fake-provider.spec.ts
+++ src/testing/fakes/fake-provider.spec.ts
@@ -1,14 +1,24 @@
+import { BigNumber } from 'ethers';
 import { Fake } from './fake.spec';
 
 export class FakeProvider implements Fake {
   private spy: any;
 
-  constructor() {}
+  constructor(private _getGasPriceReturn: any = {}) {}
 
   createSpy() {
-    this.spy = jasmine.createSpyObj('Provider', {}, { _isProvider: true });
+    this.spy = jasmine.createSpyObj(
+      'Provider',
+      {
+        getGasPrice: () => Promise.resolve(),
+      },
+      { _isProvider: true }
+    );
+    this.modifyReturns(this._getGasPriceReturn);
     return this.spy;
   }
 
-  modifyReturns() {}
+  modifyReturns(_getGasPriceReturn) {
+    this.spy.getGasPrice.and.resolveTo(BigNumber.from(_getGasPriceReturn));
+  }
 }
