47abc5f18df6398a586714bd38d64a68b94664ed
Franco Schillage <franco@xcapit.com>
2022-04-28 15:25:34 -0300

    fix: Minimum refactor to number input directive

--- src/app/shared/directives/number-input/number-input.directive.ts
+++ src/app/shared/directives/number-input/number-input.directive.ts
@@ -4,22 +4,23 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
   selector: '[appNumberInput]',
 })
 export class NumberInputDirective {
-  @Input() appNumberInput = 'decimal';
+  @Input() appNumberInput: string;
 
   constructor(private el: ElementRef) {}
   @HostListener('keypress', ['$event'])
   public onInput(event: any) {
     let inputEl = this.el.nativeElement.getElementsByTagName('input')[0];
     let inputChar = String.fromCharCode(event.which ? event.which : event.keyCode);
-    const pattern = this.patterns()[this.appNumberInput];
-    if (!pattern.test(inputEl.value + inputChar)) {
+
+    if (!this.pattern().test(inputEl.value + inputChar)) {
       event.preventDefault();
       return false;
     }
     return true;
   }
 
-  patterns() {
-    return { decimal: /^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$/, positiveInteger: /^\d{1,3}$/ };
+  pattern() {
+    const patterns = { decimal: /^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$/, positiveInteger: /^\d{1,3}$/ };
+    return this.appNumberInput ? patterns[this.appNumberInput] : patterns.decimal;
   }
 }
