aacd8ef5d986c5bc8a30515ddec6faa497c92717
Franco Schillage <franco@xcapit.com>
2022-04-28 15:35:13 -0300

    fix: Replace let for const - Codacy fix

--- src/app/shared/directives/number-input/number-input.directive.ts
+++ src/app/shared/directives/number-input/number-input.directive.ts
@@ -9,8 +9,8 @@ export class NumberInputDirective {
   constructor(private el: ElementRef) {}
   @HostListener('keypress', ['$event'])
   public onInput(event: any) {
-    let inputEl = this.el.nativeElement.getElementsByTagName('input')[0];
-    let inputChar = String.fromCharCode(event.which ? event.which : event.keyCode);
+    const inputEl = this.el.nativeElement.getElementsByTagName('input')[0];
+    const inputChar = String.fromCharCode(event.which ? event.which : event.keyCode);
 
     if (!this.pattern().test(inputEl.value + inputChar)) {
       event.preventDefault();
@@ -20,7 +20,7 @@ export class NumberInputDirective {
   }
 
   pattern() {
-    const patterns = { decimal: /^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$/, positiveInteger: /^\d{1,3}$/ };
+    const patterns = { decimal: /^[+]?([0-9]+(?:[.][0-9]*)?|\.[0-9]+)$/, positiveInteger: /^\d{1,3}$/ };
     return this.appNumberInput ? patterns[this.appNumberInput] : patterns.decimal;
   }
 }
