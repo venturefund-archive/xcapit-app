e3c2787454427b7f0ef144a6de384ac9839b2ad3
Matias Genre <matiasgenre@xcapit.com>
2022-05-20 18:40:06 -0300

    feat(recovery-phrase-read): WIP - iterate access page recovery phrase

--- src/app/modules/wallets/recovery-phrase-read/recovery-phrase-read.page.ts
+++ src/app/modules/wallets/recovery-phrase-read/recovery-phrase-read.page.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { WalletMnemonicService } from '../shared-wallets/services/wallet-mnemonic/wallet-mnemonic.service';
 import { Mnemonic } from '@ethersproject/hdnode';
 import { ClipboardService } from '../../../shared/services/clipboard/clipboard.service';
 import { ToastService } from '../../../shared/services/toast/toast.service';
 import { TranslateService } from '@ngx-translate/core';
+import { IonAccordionGroup } from '@ionic/angular';
 
 @Component({
   selector: 'app-recovery-phrase-read',
@@ -17,18 +18,33 @@ import { TranslateService } from '@ngx-translate/core';
       </ion-toolbar>
     </ion-header>
     <ion-content class="rpr ion-padding-start ion-padding-end">
+      
       <div class="ux_main">
         <div class="ux_content">
           <div class="rpr__title">
             <ion-text class="ux-font-text-lg" color="primary">
               {{ 'wallets.recovery_phrase_read.title' | translate }}
             </ion-text>
+            <ion-icon icon="information-circle"></ion-icon>
           </div>
           <div class="rpr__text">
             <ion-text class="ux-font-text-base" color="neutral90">
               {{ 'wallets.recovery_phrase_read.text' | translate }}
             </ion-text>
           </div>
+          <div >
+          <ion-button            
+            name="Toggle Accordion"
+            class="ux_button ion-no-margin"
+            color="info"
+            appTrackClick
+            fill="clear"
+            size="small"
+            ><!--           
+            (click)="this.toggleAccordion()" -->
+            {{ this.buttonText2 | translate }}
+          </ion-button>
+        </div>
           <div class="rpr__phrase-card" *ngIf="this.mnemonic">
             <app-recovery-phrase-card
               [phrase]="this.mnemonic.phrase.split(' ')"
@@ -57,10 +73,13 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./recovery-phrase-read.page.scss'],
 })
 export class RecoveryPhraseReadPage implements OnInit {
+ // @ViewChild(IonAccordionGroup, { static: true }) accordionGroup: IonAccordionGroup;
   mnemonic: Mnemonic;
   buttonColor: string;
   buttonFill: string;
   buttonText: string;
+  buttonText2: string;
+  isAccordionOpen: boolean;
   constructor(
     private walletMnemonicService: WalletMnemonicService,
     private clipboardService: ClipboardService,
@@ -68,11 +87,14 @@ export class RecoveryPhraseReadPage implements OnInit {
     private translate: TranslateService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    //this.closeAccordion();
+  }
 
   ionViewWillEnter() {
     this.buttonColor = 'primary';
     this.buttonFill = 'outline';
+    this.buttonText2 = 'wallets.recovery_phrase_read.button_show_sentence';
     this.buttonText = 'wallets.recovery_phrase_read.button_text';
     this.mnemonic = this.walletMnemonicService.mnemonic;
   }
@@ -102,4 +124,25 @@ export class RecoveryPhraseReadPage implements OnInit {
       message: this.translate.instant(text),
     });
   }
+
+ // toggleAccordion() {
+  //  if (this.isAccordionOpen) {
+  //    this.closeAccordion();
+///} else {
+//this.openAccordion();
+  //  }
+  //}
+
+  //Abre el Ver Mas
+//  private openAccordion() {
+ //   this.buttonText = 'wallets.recovery_phrase_read.button_show_sentence';
+    //this.accordionGroup.value = 'operations';
+ //   this.isAccordionOpen = true;
+//}
+//Cierra el ver mas
+//  private closeAccordion() {
+  //  this.buttonText = 'wallets.recovery_phrase_read.button_show_sentence';
+    //this.accordionGroup.value = undefined;
+   // this.isAccordionOpen = false;
+//  }
 }
