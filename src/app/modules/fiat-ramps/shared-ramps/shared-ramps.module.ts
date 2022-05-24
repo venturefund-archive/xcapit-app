6652c9c1e233f8ced459751d4cdceb79447c9457
Federico Marquez <federico@xcapit.com>
2022-05-24 15:43:40 -0300

    style(FiatRamps): Move component to correct page and fix styles

--- src/app/modules/fiat-ramps/shared-ramps/shared-ramps.module.ts
+++ src/app/modules/fiat-ramps/shared-ramps/shared-ramps.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FixedFooterComponent } from './components/fixed-footer/fixed-footer/fixed-footer.component';
-import { ProviderNewOperationCardComponent } from './components/provider-new-operation-card/provider-new-operation-card/provider-new-operation-card.component';
+import { OperationsListAccordionComponent } from './components/operations-list-accordion/operations-list-accordion.component';
+import { OperationsListComponent } from './components/operations-list/operations-list.component';
+import { OperationsListItemComponent } from './components/operations-list-item/operations-list-item.component';
+import { OperationStatusChipComponent } from './components/operation-status-chip/operation-status-chip.component';
 import { TransferConfirmCardComponent } from './components/transfer-confirm-card/transfer-confirm-card.component';
+import { ProviderNewOperationCardComponent } from './components/provider-new-operation-card/provider-new-operation-card/provider-new-operation-card.component';
 import { KycDisclaimerModalComponent } from './components/kyc-disclaimer-modal/kyc-disclaimer-modal.component';
 
 @NgModule({
@@ -11,6 +15,10 @@ import { KycDisclaimerModalComponent } from './components/kyc-disclaimer-modal/k
     TransferConfirmCardComponent,
     ProviderNewOperationCardComponent,
     KycDisclaimerModalComponent,
+    OperationsListComponent,
+    OperationStatusChipComponent,
+    OperationsListAccordionComponent,
+    OperationsListItemComponent,
   ],
   imports: [SharedModule],
   exports: [
@@ -19,6 +27,10 @@ import { KycDisclaimerModalComponent } from './components/kyc-disclaimer-modal/k
     TransferConfirmCardComponent,
     ProviderNewOperationCardComponent,
     KycDisclaimerModalComponent,
+    OperationsListComponent,
+    OperationStatusChipComponent,
+    OperationsListAccordionComponent,
+    OperationsListItemComponent,
   ],
 })
 export class SharedRampsModule {}
