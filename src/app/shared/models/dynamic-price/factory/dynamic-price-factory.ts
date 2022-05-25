9c92376ae0a8707333986f171f479024511e88ce
Franco Schillage <franco@xcapit.com>
2022-05-25 09:48:05 -0300

    feat: Add DynamicPriceFactory

---
+++ src/app/shared/models/dynamic-price/factory/dynamic-price-factory.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Coin } from 'src/app/modules/wallets/shared-wallets/interfaces/coin.interface';
+import { ApiWalletService } from 'src/app/modules/wallets/shared-wallets/services/api-wallet/api-wallet.service';
+import { DynamicPrice } from '../dynamic-price.model';
+
+@Injectable({ providedIn: 'root' })
+export class DynamicPriceFactory {
+  public new(
+    milliseconds: number,
+    _aCoin: Coin,
+    _anApiWalletService: ApiWalletService,
+  ): DynamicPrice {
+    return DynamicPrice.create(milliseconds, _aCoin, _anApiWalletService);
+  }
+}
