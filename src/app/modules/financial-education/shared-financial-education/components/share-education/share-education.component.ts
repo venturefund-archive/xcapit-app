3a2a6901a090b78ad876bade6e7145b46e8bc55a
Franco Schillage <franco@xcapit.com>
2022-05-31 17:34:42 -0300

    fix: Add file_path for cache files on android, update capacitor share

--- src/app/modules/financial-education/shared-financial-education/components/share-education/share-education.component.ts
+++ src/app/modules/financial-education/shared-financial-education/components/share-education/share-education.component.ts
@@ -7,60 +7,57 @@ import { PlatformService } from 'src/app/shared/services/platform/platform.servi
 @Component({
   selector: 'app-share-education',
   template: `
-  <div *ngIf="this.isNative" class="se" (click)="this.share()">
-    <img src='/assets/img/financial-education/shared-financial-education/share.svg'>
-  </div>
+    <div *ngIf="this.canShare" class="se" (click)="this.share()">
+      <img src="/assets/img/financial-education/shared-financial-education/share.svg" />
+    </div>
   `,
   styleUrls: ['./share-education.component.scss'],
 })
 export class ShareEducationComponent implements OnInit {
-  preview : string;
-  asset : string;
-  blob : Promise<any>
-  public isNative: boolean;
-  constructor(private http: HttpClient, private platformService: PlatformService,) { }
-  ngOnInit() {
-    this.setBlob();
-    this.isNativePlatform();
-   }
-  
-   isNativePlatform(){
-    this.isNative = this.platformService.isNative();
-    console.log(this.isNative)
-   }
+  asset: string;
+  public canShare: boolean;
+  constructor(private http: HttpClient) {}
 
-   async setBlob(){
+  async ngOnInit() {
+    await Promise.all([this.loadAsset(), this.isNativePlatform()]);
+  }
+
+  async isNativePlatform(): Promise<void> {
+    this.canShare = (await Share.canShare()).value;
+  }
+
+  async setBlob() {
     const blob = await this.http
-    .get('/assets/img/financial-education/shared-financial-education/share-image.jpg', { responseType: 'blob' })
-    .toPromise();
-    return blob
-   }
+      .get('/assets/img/financial-education/shared-financial-education/share-image.jpg', { responseType: 'blob' })
+      .toPromise();
+    return blob;
+  }
 
-  async share(){ 
+  async loadAsset() {
+    const blob = await this.setBlob();
 
     const reader = new FileReader();
-    reader.readAsDataURL(await this.setBlob());
+    reader.readAsDataURL(blob);
+
     reader.onloadend = () => {
       const result = reader.result as string;
-      this.preview = result;
-      this.asset = result.split(',')[1]; // Imagen en base64 a enviar
+      this.asset = result.split(',')[1];
     };
-    
-    const fileName = new Date().getTime() + 'share.jpg'; // Nombre del file que se descarga
-    const savedFile = await Filesystem.writeFile({ // Guardamos file en cache
+  }
+
+  async share() {
+    const fileName = new Date().getTime() + '.jpg';
+    const savedFile = await Filesystem.writeFile({
       directory: Directory.Cache,
       path: fileName,
       data: this.asset,
     });
 
     await Share.share({
-       title: 'Señora Alegria',
-       text: '¿Alguien puede pensar en los niños!?',
-      url: savedFile.uri, // Mandamos la uri del file que guardamos
+      title: 'Share Xcapit',
+      text: '',
+      url: savedFile.uri,
       dialogTitle: 'Share',
     });
-
-    console.log("entré al share")
   }
-
 }
