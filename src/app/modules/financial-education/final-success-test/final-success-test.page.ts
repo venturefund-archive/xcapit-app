4be19d58b3285039e2e8ac4aa9dcd4ef28089080
Luscherr14 <lucila@xcapit.com>
2022-06-13 10:12:05 -0300

    fix(sucess-types): solve conflict in gitlab

---
+++ src/app/modules/financial-education/final-success-test/final-success-test.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { SUCCESS_TYPES } from 'src/app/shared/components/success-content/success-types.constant';
+import { TrackService } from 'src/app/shared/services/track/track.service';
+
+@Component({
+  selector: 'app-final-success-test',
+  template: ` <ion-header>
+  <ion-toolbar color="white" class="no-border ion-padding">
+  <app-share-education [lightBackground]="this.lightBackground"></app-share-education>
+  </ion-toolbar>
+</ion-header>
+  <ion-content class="ion-padding">
+  <app-success-content *ngIf="this.data" [data]="this.data"></app-success-content>
+</ion-content>`,
+  styleUrls: ['./final-success-test.page.scss'],
+})
+export class FinalSuccessTestPage implements OnInit {
+  data = SUCCESS_TYPES.final_success_test;
+  lightBackground= true;
+  constructor(private trackService : TrackService) { }
+
+  ngOnInit() {
+  }
+
+  ionViewWillEnter() {
+    this.trackService.trackEvent({
+      eventAction: 'screenview',
+      description: window.location.href,
+      eventLabel: 'ux_education_all_modules_complete'
+    });
+  }
+}
