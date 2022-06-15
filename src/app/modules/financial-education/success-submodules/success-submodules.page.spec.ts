47c6e8365184551c863e3f240e06ac3fddd05d0e
Matias Genre <matiasgenre@xcapit.com>
2022-06-15 00:17:21 -0300

    feat(success-submodule): change in success test

--- src/app/modules/financial-education/success-submodules/success-submodules.page.spec.ts
+++ src/app/modules/financial-education/success-submodules/success-submodules.page.spec.ts
@@ -1,24 +1,54 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-
+import { TranslateModule } from '@ngx-translate/core';
+import { TrackService } from '../../../shared/services/track/track.service';
+import { SUCCESS_TYPES } from '../../../shared/components/success-content/success-types.constant';
 import { SuccessSubmodulesPage } from './success-submodules.page';
 
-describe('SuccessSubmodulesPage', () => {
+
+fdescribe('SuccessSubmodulesPage', () => {
   let component: SuccessSubmodulesPage;
   let fixture: ComponentFixture<SuccessSubmodulesPage>;
+  let activatedRouteSpy: jasmine.SpyObj<ActivatedRoute>;
+  let trackServiceSpy: jasmine.SpyObj<TrackService>;
+  
 
   beforeEach(waitForAsync(() => {
+
+    trackServiceSpy = jasmine.createSpyObj('TrackServiceSpy',{
+      trackEvent: Promise.resolve(true),
+    })
+
     TestBed.configureTestingModule({
-      declarations: [ SuccessSubmodulesPage ],
-      imports: [IonicModule.forRoot()]
+      declarations: [ SuccessSubmodulesPage ],  
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteSpy },
+        { provide: TrackService, useValue: trackServiceSpy}
+      ],
+      imports: [IonicModule.forRoot(), TranslateModule.forRoot()]
     }).compileComponents();
 
     fixture = TestBed.createComponent(SuccessSubmodulesPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should track screenview event on init', () => {
+    component.event();
+    expect(trackServiceSpy.trackEvent).toHaveBeenCalledTimes(1);
+  });
+
+  it('should set data on init', () => {
+    component.ionViewWillEnter();
+    fixture.detectChanges();
+    expect(component.data).toEqual(SUCCESS_TYPES.success_submodules);
+  });  
+
 });
