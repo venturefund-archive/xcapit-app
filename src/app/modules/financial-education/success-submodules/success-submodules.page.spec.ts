ab033707d6b9c7ed7c4a51e662f7892b162456ca
Matias Genre <matiasgenre@xcapit.com>
2022-06-15 12:31:24 -0300

    feat: complete tests

--- src/app/modules/financial-education/success-submodules/success-submodules.page.spec.ts
+++ src/app/modules/financial-education/success-submodules/success-submodules.page.spec.ts
@@ -6,49 +6,74 @@ import { TranslateModule } from '@ngx-translate/core';
 import { TrackService } from '../../../shared/services/track/track.service';
 import { SUCCESS_TYPES } from '../../../shared/components/success-content/success-types.constant';
 import { SuccessSubmodulesPage } from './success-submodules.page';
+import { ModulesService } from 'src/app/shared/services/financial-education/modules.service';
+import { FakeActivatedRoute } from 'src/testing/fakes/activated-route.fake.spec';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-
-fdescribe('SuccessSubmodulesPage', () => {
+describe('SuccessSubmodulesPage', () => {
   let component: SuccessSubmodulesPage;
   let fixture: ComponentFixture<SuccessSubmodulesPage>;
+  let fakeActivatedRoute: FakeActivatedRoute;
   let activatedRouteSpy: jasmine.SpyObj<ActivatedRoute>;
+  //Crear 1- Variable
   let trackServiceSpy: jasmine.SpyObj<TrackService>;
-  
+  let moduleServiceSpy: jasmine.SpyObj<ModulesService>;
 
-  beforeEach(waitForAsync(() => {
+  //aca definomos todo globalmente antes que arranquen cada test
+  beforeEach(
+    waitForAsync(() => {
+      //2do- definir el spy con sus metodos o variables
+      trackServiceSpy = jasmine.createSpyObj('TrackService', {
+        trackEvent: Promise.resolve(true),
+      });
 
-    trackServiceSpy = jasmine.createSpyObj('TrackServiceSpy',{
-      trackEvent: Promise.resolve(true),
+      moduleServiceSpy = jasmine.createSpyObj('ModulesService', {
+        getModules: [
+          {
+            id: 1,
+            sub_modules: [
+              {
+                id: 1,
+                title:'Test',
+                screenViewLabel:'Event test'
+              },
+            ],
+          },
+        ],
+      });
+
+      fakeActivatedRoute = new FakeActivatedRoute({ moduleId: 1, submoduleId: 1 });
+      activatedRouteSpy = fakeActivatedRoute.createSpy();
+
+      TestBed.configureTestingModule({
+        declarations: [SuccessSubmodulesPage],
+        providers: [
+          { provide: ActivatedRoute, useValue: activatedRouteSpy },
+          //3ro no uses el real, usa el trackServiceSpy
+          { provide: TrackService, useValue: trackServiceSpy },
+          { provide: ModulesService, useValue: moduleServiceSpy },
+        ],
+        imports: [IonicModule.forRoot(), TranslateModule.forRoot()],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      }).compileComponents();
+
+      fixture = TestBed.createComponent(SuccessSubmodulesPage);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
     })
-
-    TestBed.configureTestingModule({
-      declarations: [ SuccessSubmodulesPage ],  
-      providers: [
-        { provide: ActivatedRoute, useValue: activatedRouteSpy },
-        { provide: TrackService, useValue: trackServiceSpy}
-      ],
-      imports: [IonicModule.forRoot(), TranslateModule.forRoot()]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(SuccessSubmodulesPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-    
-  }));
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should track screenview event on init', () => {
-    component.event();
-    expect(trackServiceSpy.trackEvent).toHaveBeenCalledTimes(1);
   });
 
   it('should set data on init', () => {
     component.ionViewWillEnter();
     fixture.detectChanges();
     expect(component.data).toEqual(SUCCESS_TYPES.success_submodules);
-  });  
-
+    expect(component.data.textPrimary).toEqual('financial_education.success_submodule.textPrimary');
+    expect(trackServiceSpy.trackEvent).toHaveBeenCalledOnceWith({eventAction: 'screenview',
+    description: window.location.href,
+    eventLabel:'Event test'});
+  });
 });
