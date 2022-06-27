f324ace77498312dd54f883175ef567e30634868
Martin317 <martin@xcapit.com>
2022-06-27 16:21:48 -0300

    fix(wallets): Firebase event not sent

--- src/app/modules/wallets/shared-wallets/components/info-phrase-modal/info-phrase-modal.component.spec.ts
+++ src/app/modules/wallets/shared-wallets/components/info-phrase-modal/info-phrase-modal.component.spec.ts
@@ -4,19 +4,22 @@ import { IonicModule, ModalController } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 import { FakeModalController } from 'src/testing/fakes/modal-controller.fake.spec';
 import { InfoPhraseModalComponent } from './info-phrase-modal.component';
+import { TrackClickDirectiveTestHelper } from '../../../../../../testing/track-click-directive-test.spec';
+import { FakeTrackClickDirective } from '../../../../../../testing/fakes/track-click-directive.fake.spec';
 
 describe('InfoPhraseModalComponent', () => {
   let component: InfoPhraseModalComponent;
   let fixture: ComponentFixture<InfoPhraseModalComponent>;
   let modalControllerSpy: jasmine.SpyObj<ModalController>;
   let fakeModalController: FakeModalController;
+  let trackClickDirectiveHelper: TrackClickDirectiveTestHelper<InfoPhraseModalComponent>;
 
   beforeEach(
     waitForAsync(() => {
       fakeModalController = new FakeModalController();
       modalControllerSpy = fakeModalController.createSpy();
       TestBed.configureTestingModule({
-        declarations: [InfoPhraseModalComponent],
+        declarations: [InfoPhraseModalComponent, FakeTrackClickDirective],
         imports: [IonicModule.forRoot(), TranslateModule.forRoot()],
         providers: [{ provide: ModalController, useValue: modalControllerSpy }],
       }).compileComponents();
@@ -24,6 +27,7 @@ describe('InfoPhraseModalComponent', () => {
       fixture = TestBed.createComponent(InfoPhraseModalComponent);
       component = fixture.componentInstance;
       fixture.detectChanges();
+      trackClickDirectiveHelper = new TrackClickDirectiveTestHelper(fixture);
     })
   );
 
@@ -43,5 +47,15 @@ describe('InfoPhraseModalComponent', () => {
     fixture.detectChanges();
     await fixture.whenStable();
     expect(modalControllerSpy.dismiss).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call trackEvent on trackService when ux_phrase_information_confirmation button clicked', () => {
+    const el = trackClickDirectiveHelper.getByElementByName('ion-button', 'ux_phrase_information_confirmation');
+    const directive = trackClickDirectiveHelper.getDirective(el);
+
+    const spy = spyOn(directive, 'clickEvent');
+    el.nativeElement.click();
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 });
