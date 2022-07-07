3972df9d8735c16f82d468db5a3e5eb8c47a8615
Luscherr14 <lucila@xcapit.com>
2022-07-07 17:05:28 -0300

    feat(swaps): sx-2397 Add password error service test

--- src/app/modules/swaps/shared-swaps/services/password-error-handler/password-error-handler.service.spec.ts
+++ src/app/modules/swaps/shared-swaps/services/password-error-handler/password-error-handler.service.spec.ts
@@ -5,6 +5,7 @@ import { PasswordErrorHandlerService } from './password-error-handler.service';
 describe('PasswordErrorHandlerService', () => {
   let service: PasswordErrorHandlerService;
 
+
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(PasswordErrorHandlerService);
@@ -13,4 +14,17 @@ describe('PasswordErrorHandlerService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should be called callback when error message is invalid password', () => {
+    const callbackSpy = jasmine.createSpy() 
+    service.handlePasswordError({message: 'invalid password'}, callbackSpy)
+    expect(callbackSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not be called callback when error message is invalid password', () => {
+    const callbackSpy = jasmine.createSpy() 
+    service.handlePasswordError({message: 'valid password'}, callbackSpy)
+    expect(callbackSpy).toHaveBeenCalledTimes(0);
+  });
+
 });
