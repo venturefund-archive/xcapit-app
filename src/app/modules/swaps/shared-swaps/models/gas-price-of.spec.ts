dbf9e28e5e0a5fdc7012ec78aa972c0cf375e520
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-11 13:03:38 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/gas-price-of.spec.ts
+++ src/app/modules/swaps/shared-swaps/models/gas-price-of.spec.ts
@@ -21,7 +21,6 @@ export class GasStationOf {
   price(): GasPrice {
     let gasPrice: GasPrice = new DefaultGasPriceOf(this._aBlockchain, this._providers);
     if (this._aBlockchain.gasPriceClass()) {
-
       gasPrice = new PolygonGasPrice(this._httpClient);
     }
     return gasPrice;
