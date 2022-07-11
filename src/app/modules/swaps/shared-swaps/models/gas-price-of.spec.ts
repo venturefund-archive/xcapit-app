322065c19cb673379e62c47c23643ad2cabbc104
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-11 12:30:59 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/gas-price-of.spec.ts
+++ src/app/modules/swaps/shared-swaps/models/gas-price-of.spec.ts
@@ -2,14 +2,15 @@ import { Blockchain } from './blockchain/blockchain';
 import { rawPolygonData } from './fixtures/raw-blockchains-data';
 import { FakeHttpClient } from 'src/testing/fakes/fake-http.spec';
 import { HttpClient } from '@angular/common/http';
-import { providers } from './fakes/fake-ethers-providers';
+import { providers as fakeProviders } from './fakes/fake-ethers-providers';
 
 
 export class GasStationOf {
 
   constructor(
     private _aBlockchain: Blockchain,
-    private _httpClient: HttpClient | FakeHttpClient
+    private _httpClient: HttpClient | FakeHttpClient,
+    private _providers: any =
   ) {
     console.log("hi")
   }
@@ -24,7 +25,7 @@ fdescribe('Gas Station Of', () => {
     gasStation = new GasStationOf(
       new Blockchain(rawPolygonData),
       new FakeHttpClient(),
-      providers
+      fakeProviders
     );
   });
 
