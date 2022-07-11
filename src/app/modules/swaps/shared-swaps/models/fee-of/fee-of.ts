2bd807bd70df83929ba62c2a41232f74932ad860
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-11 17:38:52 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/fee-of/fee-of.ts
+++ src/app/modules/swaps/shared-swaps/models/fee-of/fee-of.ts
@@ -1,3 +1,7 @@
+import { BigNumber } from 'ethers';
+import { Fee } from 'src/app/modules/defi-investments/shared-defi-investments/interfaces/fee.interface';
+
+
 export class FeeOf implements Fee {
 
   constructor(private _aGasUnits: BigNumber, private _aGasPriceValue: BigNumber) {}
