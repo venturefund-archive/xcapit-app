2e1131268bb8fea98257a12c9087cd98799000c4
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-11 17:30:45 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/fee-of/fee-of.spec.ts
+++ src/app/modules/swaps/shared-swaps/models/fee-of/fee-of.spec.ts
@@ -1,11 +1,12 @@
 import { BigNumber } from "ethers";
+import { Fee } from "src/app/modules/defi-investments/shared-defi-investments/interfaces/fee.interface";
 import { FakeHttpClient } from "src/testing/fakes/fake-http.spec";
 import { Blockchain } from "../blockchain/blockchain";
 import { fakeProviders } from "../fakes/fake-ethers-providers";
 import { rawEthereumData } from "../fixtures/raw-blockchains-data";
 import { GasStationOf } from "../gas-station-of/gas-station-of";
 
-export class FeeOf {
+export class FeeOf implements Fee {
 
   constructor(private _aGasUnits: BigNumber, private _aGasStation: GasStationOf) { }
 }
