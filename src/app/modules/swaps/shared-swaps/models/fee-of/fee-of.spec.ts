0b18465029e3ef0c7da75b5e4c95a313acd7f86e
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-11 17:35:23 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/fee-of/fee-of.spec.ts
+++ src/app/modules/swaps/shared-swaps/models/fee-of/fee-of.spec.ts
@@ -11,7 +11,7 @@ export class FeeOf implements Fee {
 
   constructor(private _aGasUnits: BigNumber, private _aGasPriceValue: BigNumber) {}
 
-  value(): Promise<BigNumber> {
+  value(): BigNumber {
     return this._aGasPriceValue.mul(this._aGasUnits);
   }
 }
