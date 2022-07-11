616bc8dd9ff79906dc4e34067957664b670323c1
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-11 17:28:02 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/fee-of/fee-of.spec.ts
+++ src/app/modules/swaps/shared-swaps/models/fee-of/fee-of.spec.ts
@@ -1,8 +1,9 @@
 import { BigNumber } from "ethers";
+import { GasStationOf } from "../gas-station-of/gas-station-of";
 
 export class FeeOf {
 
-  constructor(private _aGasUnits: BigNumber) { }
+  constructor(private _aGasUnits: BigNumber, private _aGasStation: GasStationOf) { }
 }
 
 
