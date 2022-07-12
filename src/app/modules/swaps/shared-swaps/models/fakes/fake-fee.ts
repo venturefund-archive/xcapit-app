d1297e355d2e70072738dcd21570376daf5b7ea3
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-12 15:57:08 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/fakes/fake-fee.ts
+++ src/app/modules/swaps/shared-swaps/models/fakes/fake-fee.ts
@@ -1,4 +1,6 @@
 import { BigNumber } from "ethers";
+import { Fee } from "src/app/modules/defi-investments/shared-defi-investments/interfaces/fee.interface";
+
 
 export class FakeFee implements Fee {
 
