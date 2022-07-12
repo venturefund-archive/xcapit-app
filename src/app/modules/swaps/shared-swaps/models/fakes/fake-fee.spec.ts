e86749ac200f940c44c417ce28876487512d6d69
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-12 15:50:45 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/fakes/fake-fee.spec.ts
+++ src/app/modules/swaps/shared-swaps/models/fakes/fake-fee.spec.ts
@@ -1,6 +1,11 @@
+import { BigNumber } from "@ethersproject/bignumber";
 import { Fee } from "src/app/modules/defi-investments/shared-defi-investments/interfaces/fee.interface";
 
-export class FakeFee implements Fee { }
+export class FakeFee implements Fee {
+    value(): Promise<BigNumber> {
+        throw new Error("Method not implemented.");
+    }
+}
 
 
 fdescribe('FakeFee', () => {
