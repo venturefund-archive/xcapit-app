77a2c0c770c2f9a869fd71128bc865ed0e56b7c1
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-12 09:19:23 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/fee-of/fee-of.ts
+++ src/app/modules/swaps/shared-swaps/models/fee-of/fee-of.ts
@@ -1,10 +1,11 @@
 import { BigNumber } from 'ethers';
 import { Fee } from 'src/app/modules/defi-investments/shared-defi-investments/interfaces/fee.interface';
+import { GasUnits } from '../gas-units/gas-units';
 
 
 export class FeeOf implements Fee {
 
-  constructor(private _aGasUnits: BigNumber, private _aGasPriceValue: BigNumber) {}
+  constructor(private _aGasUnits: GasUnits, private _aGasPriceValue: BigNumber) {}
 
   async value(): Promise<BigNumber> {
     return this._aGasPriceValue.mul(this._aGasUnits);
