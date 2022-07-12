638a9a5104534b3d825eb776437c3906aaa17cf4
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-12 16:11:55 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/blockchain-fee/blockchain-fee.spec.ts
+++ src/app/modules/swaps/shared-swaps/models/blockchain-fee/blockchain-fee.spec.ts
@@ -2,7 +2,10 @@ import { Fee } from "src/app/modules/defi-investments/shared-defi-investments/in
 import { BlockchainTokens } from "../blockchain-tokens/blockchain-tokens";
 import { Blockchain } from "../blockchain/blockchain";
 import { FakeFee } from "../fakes/fake-fee";
+import { rawEthereumData } from "../fixtures/raw-blockchains-data";
 import { NativeTokenOf } from "../native-token-of/native-token-of";
+import { TokenRepo } from "../token-repo/token-repo";
+import { DefaultTokens } from "../tokens/tokens";
 
 
 export class BlockchainFee {
