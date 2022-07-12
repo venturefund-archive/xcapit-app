aa1c7820d2d7e53cec10826069366a1714a86638
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-12 10:09:49 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/native-token-of/native-token-of.spec.ts
+++ src/app/modules/swaps/shared-swaps/models/native-token-of/native-token-of.spec.ts
@@ -1,42 +1,43 @@
-import { BlockchainTokens } from "../blockchain-tokens/blockchain-tokens";
-import { Blockchain } from "../blockchain/blockchain";
-import { rawEthereumData } from "../fixtures/raw-blockchains-data";
-import { rawTokensData } from "../fixtures/raw-tokens-data";
-import { TokenRepo } from "../token-repo/token-repo";
-import { Token } from "../token/token";
-import { DefaultTokens } from "../tokens/tokens";
+import { BlockchainTokens } from '../blockchain-tokens/blockchain-tokens';
+import { Blockchain } from '../blockchain/blockchain';
+import { rawEthereumData } from '../fixtures/raw-blockchains-data';
+import { rawTokensData } from '../fixtures/raw-tokens-data';
+import { TokenRepo } from '../token-repo/token-repo';
+import { Token } from '../token/token';
+import { DefaultTokens } from '../tokens/tokens';
 
 
 export class NativeTokenOf implements Token {
 
-  constructor(private _aBlockchainTokens: BlockchainTokens) { }
-    blockchainId(): string {
-        throw new Error("Method not implemented.");
-    }
-    symbol(): string {
-        throw new Error("Method not implemented.");
-    }
-    decimals(): number {
-        throw new Error("Method not implemented.");
-    }
-    address(): string {
-        throw new Error("Method not implemented.");
-    }
-    json() {
-        throw new Error("Method not implemented.");
-    }
+  constructor(private _aBlockchainTokens: BlockchainTokens) {}
+
+  blockchainId(): string {
+    throw new Error('Method not implemented.');
+  }
+
+  symbol(): string {
+    throw new Error('Method not implemented.');
+  }
+
+  decimals(): number {
+    throw new Error('Method not implemented.');
+  }
+
+  address(): string {
+    throw new Error('Method not implemented.');
+  }
+
+  json() {
+    throw new Error('Method not implemented.');
+  }
 }
 
-
 fdescribe('NativeTokenOf', () => {
-
   let token: NativeTokenOf;
 
   beforeEach(() => {
     token = new NativeTokenOf(
-      new BlockchainTokens(
-        new Blockchain(rawEthereumData),
-        new DefaultTokens(new TokenRepo(rawTokensData)))
+      new BlockchainTokens(new Blockchain(rawEthereumData), new DefaultTokens(new TokenRepo(rawTokensData)))
     );
   });
 
