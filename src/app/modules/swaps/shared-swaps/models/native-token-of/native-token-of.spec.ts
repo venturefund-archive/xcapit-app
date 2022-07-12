44c07d1eb862b87f6d8850038257f4071caaabf0
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-12 10:09:42 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/native-token-of/native-token-of.spec.ts
+++ src/app/modules/swaps/shared-swaps/models/native-token-of/native-token-of.spec.ts
@@ -10,6 +10,21 @@ import { DefaultTokens } from "../tokens/tokens";
 export class NativeTokenOf implements Token {
 
   constructor(private _aBlockchainTokens: BlockchainTokens) { }
+    blockchainId(): string {
+        throw new Error("Method not implemented.");
+    }
+    symbol(): string {
+        throw new Error("Method not implemented.");
+    }
+    decimals(): number {
+        throw new Error("Method not implemented.");
+    }
+    address(): string {
+        throw new Error("Method not implemented.");
+    }
+    json() {
+        throw new Error("Method not implemented.");
+    }
 }
 
 
