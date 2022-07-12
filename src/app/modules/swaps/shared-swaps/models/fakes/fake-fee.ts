66e8ad7d5bc71e5ac4bbd65f16c5d56dde7563cc
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-12 15:56:57 -0300

    Working...

---
+++ src/app/modules/swaps/shared-swaps/models/fakes/fake-fee.ts
@@ -0,0 +1,8 @@
+export class FakeFee implements Fee {
+
+  constructor(private _aTestData: string|number) { }
+
+  async value(): Promise<BigNumber> {
+    return BigNumber.from(this._aTestData);
+  }
+}
