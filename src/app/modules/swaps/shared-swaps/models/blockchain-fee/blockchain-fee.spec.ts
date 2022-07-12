56d1137045828ccc9df9514887422e01931a9e3c
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-12 16:02:32 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/blockchain-fee/blockchain-fee.spec.ts
+++ src/app/modules/swaps/shared-swaps/models/blockchain-fee/blockchain-fee.spec.ts
@@ -1,6 +1,11 @@
+import { Fee } from "src/app/modules/defi-investments/shared-defi-investments/interfaces/fee.interface";
 import { FakeFee } from "../fakes/fake-fee";
 
-export class BlockchainFee { }
+
+export class BlockchainFee {
+
+  constructor(private _aFee: Fee) { }
+}
 
 
 fdescribe('BlockchainFee', () => {
