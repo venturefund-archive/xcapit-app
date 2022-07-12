f8ff7000ec60757d48e1252ed7680afc3e99d6b0
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-12 09:00:42 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/gas-units/gas-units.spec.ts
+++ src/app/modules/swaps/shared-swaps/models/gas-units/gas-units.spec.ts
@@ -17,7 +17,7 @@ fdescribe('GasUnits', () => {
   });
 
   it('value', () => {
-    expect(new GasUnits(100).value().eq(100)).toBeTruthy();
+    expect(new GasUnits(100).value().eq(100)).toBeTrue();
   });
 
 });
