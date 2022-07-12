60a7be005533a274456a0bd169bdd6710abf3f63
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-12 16:26:02 -0300

    Working...

--- src/app/modules/defi-investments/shared-defi-investments/models/formatted-fee/formatted-fee.model.ts
+++ src/app/modules/defi-investments/shared-defi-investments/models/formatted-fee/formatted-fee.model.ts
@@ -1,8 +1,10 @@
 import { BigNumber, FixedNumber } from 'ethers';
 import { Fee } from '../../interfaces/fee.interface';
 
-export class FormattedFee {
+export class FormattedFee implements Fee {
+
   constructor(private readonly _aFee: Fee, private readonly _decimals: number = 18) {}
+
   async value(): Promise<number> {
     return FixedNumber.from(await this._aFee.value())
       .divUnsafe(FixedNumber.from(BigNumber.from('10').pow(this._decimals)))
