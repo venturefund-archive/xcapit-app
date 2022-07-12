d0a8227f49f22ac7bf45b01b30bb27cc2a845ecc
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-12 15:57:13 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/fakes/fake-fee.spec.ts
+++ src/app/modules/swaps/shared-swaps/models/fakes/fake-fee.spec.ts
@@ -1,14 +1,7 @@
 import { BigNumber } from '@ethersproject/bignumber';
 import { Fee } from 'src/app/modules/defi-investments/shared-defi-investments/interfaces/fee.interface';
+import { FakeFee } from './fake-fee';
 
-export class FakeFee implements Fee {
-
-  constructor(private _aTestData: string|number) { }
-
-  async value(): Promise<BigNumber> {
-    return BigNumber.from(this._aTestData);
-  }
-}
 
 fdescribe('FakeFee', () => {
 
