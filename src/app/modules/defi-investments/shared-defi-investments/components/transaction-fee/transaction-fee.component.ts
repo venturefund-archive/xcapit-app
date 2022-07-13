eb6b9872a13a31ac09a0857fbc65cd122eae506f
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-13 16:51:34 -0300

    Working...

--- src/app/modules/defi-investments/shared-defi-investments/components/transaction-fee/transaction-fee.component.ts
+++ src/app/modules/defi-investments/shared-defi-investments/components/transaction-fee/transaction-fee.component.ts
@@ -79,7 +79,17 @@ export class TransactionFeeComponent implements OnChanges {
   ) { }
 
   ngOnChanges(changes: SimpleChanges) {
+
     if(this.autoPrice && changes.fee && this.fee?.token) {
+      this.setNullQuoteFee();
+      this.dynamicPriceSubscription && this.dynamicPriceSubscription.unsubscribe();
+      this.dynamicPriceSubscription = this.getDynamicPrice()
+        .subscribe((price: number) => this.setQuoteFee(price));
+    }
+  }
+
+  private setAutoPrice(feeChanges) {
+    if(this.autoPrice && feeChanges && this.fee?.token) {
       this.setNullQuoteFee();
       this.dynamicPriceSubscription && this.dynamicPriceSubscription.unsubscribe();
       this.dynamicPriceSubscription = this.getDynamicPrice()
