9ea7700cf46323145c5b070a4094d5086053b83e
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-07-14 12:37:57 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/gas-station-of/factory/gas-station-of.factory.spec.ts
+++ src/app/modules/swaps/shared-swaps/models/gas-station-of/factory/gas-station-of.factory.spec.ts
@@ -1,27 +1,3 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { providers } from "ethers";
-import { FakeHttpClient } from "src/testing/fakes/fake-http.spec";
-import { Blockchain } from "../blockchain/blockchain";
-import { fakeProviders } from "../fakes/fake-ethers-providers";
-import { rawPolygonData } from "../fixtures/raw-blockchains-data";
-import { rawPolygonGasStation } from "../fixtures/raw-polygon-gs-data";
-import { GasStationOf } from "./gas-station-of";
-
-
-@Injectable({ providedIn: 'root' })
-export class GasStationOfFactory {
-
-  create(
-    _aBlockchain: Blockchain,
-    _httpClient: HttpClient | FakeHttpClient,
-    _providers: any = providers
-  ): GasStationOf {
-    return new GasStationOf(_aBlockchain, _httpClient, _providers);
-  }
-}
-
-
 fdescribe('GasStationOfFactory', () => {
 
   it('new', () => {
