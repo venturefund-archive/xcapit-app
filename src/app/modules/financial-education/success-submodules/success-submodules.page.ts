1795306fe3b8568ef01736ed17af320366ff250d
Diego  Meineri <diego@xcapit.com>
2022-07-21 17:05:00 -0300

    Working...

--- src/app/modules/financial-education/success-submodules/success-submodules.page.ts
+++ src/app/modules/financial-education/success-submodules/success-submodules.page.ts
@@ -45,8 +45,8 @@ export class SuccessSubmodulesPage implements OnInit {
   }
 
   setTitle() {
-    // const moduleId = parseInt(this.route.snapshot.paramMap.get('module'));
-    // const submoduleId = parseInt(this.route.snapshot.paramMap.get('submodule'));
+     const moduleId = parseInt(this.route.snapshot.paramMap.get('module'));
+     const submoduleId = parseInt(this.route.snapshot.paramMap.get('submodule'));
     // const module = this.modules.find((item) => item.id === moduleId);
     // this.subModule = module.sub_modules && module.sub_modules.find((item) => item.id === submoduleId);
     // this.data.textPrimary = this.translate.instant('financial_education.success_submodule.textPrimary', {
