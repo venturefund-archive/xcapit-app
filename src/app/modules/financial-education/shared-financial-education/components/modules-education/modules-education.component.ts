2dfbc7c303fd1236b30dd4f5ff82b84840813ec3
Alan <alan@xcapit.com>
2022-07-21 12:17:36 -0300

    Working...

--- src/app/modules/financial-education/shared-financial-education/components/modules-education/modules-education.component.ts
+++ src/app/modules/financial-education/shared-financial-education/components/modules-education/modules-education.component.ts
@@ -37,7 +37,7 @@ import { FinancialEducationService } from '../../services/financial-education/fi
           >
             <div class="item-content">
               <div class="item-content__body">
-                <ion-icon class="item-content__body__status" name="ux-completed"></ion-icon>
+                <ion-icon class="item-content__body__status" name="ux-to_do"></ion-icon>
                 <div>
                   <ion-label name="sub_module_title ux-font-text-xxs" color="primary">{{
                     submodule.title | translate
