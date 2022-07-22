471e01f4a5311be2ccfd541a08ac114c4e49d84d
Diego  Meineri <diego@xcapit.com>
2022-07-22 14:48:23 -0300

    Working...

--- src/app/modules/financial-education/test-typeform/test-typeform.page.spec.ts
+++ src/app/modules/financial-education/test-typeform/test-typeform.page.spec.ts
@@ -111,6 +111,7 @@ fdescribe('TestTypeformPage', () => {
   it('should navigate to error test page when submit test on typeform and status is not completed', async () => {
     rawEducationData.finance[0].status = 'to_do';
     rawEducationData.finance[0].submodules[0].status = 'to_do';
+    rawSubmoduleResult.status = 'to_do';
     await component.ionViewWillEnter();
     await fixture.whenStable();
     await fixture.whenRenderingDone();
