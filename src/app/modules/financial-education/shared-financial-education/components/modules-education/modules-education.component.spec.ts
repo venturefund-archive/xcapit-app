9c8c606f7a014d3122c0c2a22bad05ff2347543c
Diego  Meineri <diego@xcapit.com>
2022-07-22 17:09:50 -0300

    Working...

--- src/app/modules/financial-education/shared-financial-education/components/modules-education/modules-education.component.spec.ts
+++ src/app/modules/financial-education/shared-financial-education/components/modules-education/modules-education.component.spec.ts
@@ -86,7 +86,7 @@ fdescribe('ModulesEducationComponent', () => {
 
     const imgModuleEl = fixture.debugElement.query(By.css('img[name="module_img"]'));
     const titleModuleEl = fixture.debugElement.query(By.css('ion-label[name="module_title"]'));
-    const submoduleTitleEl = fixture.debugElement.query(By.css('ion-label[name="sub_module_title"]'));
+    const submoduleTitleEl = fixture.debugElement.query(By.css('ion-label[name="module_state"]'));
     const moduleStateEl = fixture.debugElement.query(By.css('ion-label[name="sub_module_title"]'));
     expect(imgModuleEl.attributes.src).toContain('icon_finance_1');
     expect(titleModuleEl.nativeElement.innerHTML).toContain('title_finance_1');
