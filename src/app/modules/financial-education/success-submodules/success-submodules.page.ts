8978649590d23d5d8e61e1bcc7a18763047584a2
Diego  Meineri <diego@xcapit.com>
2022-07-22 15:38:40 -0300

    Working...

--- src/app/modules/financial-education/success-submodules/success-submodules.page.ts
+++ src/app/modules/financial-education/success-submodules/success-submodules.page.ts
@@ -63,7 +63,6 @@ export class SuccessSubmodulesPage implements OnInit {
 
     this.data = category === 'finance' ? this.data.finance : this.data.crypto;
     
-    console.log(this.data)
     const module = this.data.find((item) => item.id === moduleId);
     this.subModule = module.submodules && module.submodules.find((submodule) => submodule.id === submoduleId);
     this.success_data.textPrimary = this.translate.instant('financial_education.success_submodule.textPrimary', {
