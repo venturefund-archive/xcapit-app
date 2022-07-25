53b2029f671df1dbf6e1dc123d1723cd896ef745
Alan <alan@xcapit.com>
2022-07-25 10:51:00 -0300

    Working...

--- src/app/modules/financial-education/shared-financial-education/components/global-progress-card/global-progress-card.component.spec.ts
+++ src/app/modules/financial-education/shared-financial-education/components/global-progress-card/global-progress-card.component.spec.ts
@@ -5,6 +5,7 @@ import { GlobalProgressCardComponent } from './global-progress-card.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { rawEducationData } from '../../fixtures/rawEducationData';
+import { rawEducationProgressData } from '../../fixtures/rawEducationProgressData';
 
 fdescribe('GlobalProgressCardComponent', () => {
   let component: GlobalProgressCardComponent;
@@ -20,7 +21,7 @@ fdescribe('GlobalProgressCardComponent', () => {
 
       fixture = TestBed.createComponent(GlobalProgressCardComponent);
       component = fixture.componentInstance;
-      component.modules = [...rawEducationData.finance, ...rawEducationData.crypto].filter((mod) => !mod.coming_soon);
+      component.modules = rawEducationProgressData;
       fixture.detectChanges();
     })
   );
@@ -30,8 +31,6 @@ fdescribe('GlobalProgressCardComponent', () => {
   });
 
   it('should render card_state_0 if progress is 0', async () => {
-    rawEducationData.finance[0].status = 'to_do';
-    rawEducationData.crypto[0].status = 'to_do';
     component.calculateProgressPercentage();
     await Promise.all([fixture.whenRenderingDone(), fixture.whenStable()]);
     fixture.detectChanges();
@@ -41,19 +40,19 @@ fdescribe('GlobalProgressCardComponent', () => {
     expect(titleEl.nativeElement.innerHTML).toContain('financial_education.home.global_progress.card_state_0');
   });
 
-  // it('should render card_state_25 if progress is 25', () => {
-  //   component.modules = oneModulesDone;
-  //   component.calculateProgressPercentage();
-  //   fixture.detectChanges();
-  //   const titleEl = fixture.debugElement.query(
-  //     By.css('.gpc__card__accordion__item__content ion-text.ux-font-text-xxs')
-  //   );
-  //   expect(titleEl.nativeElement.innerHTML).toContain('financial_education.home.global_progress.card_state_25');
-  // });
+  it('should render card_state_25 if progress is 25', () => {
+    rawEducationProgressData[0].status = 'completed';
+    component.calculateProgressPercentage();
+    fixture.detectChanges();
+    const titleEl = fixture.debugElement.query(
+      By.css('.gpc__card__accordion__item__content ion-text.ux-font-text-xxs')
+    );
+    expect(titleEl.nativeElement.innerHTML).toContain('financial_education.home.global_progress.card_state_25');
+  });
 
   it('should render card_state_50 if progress is 50', () => {
-    rawEducationData.finance[0].status = 'to_do';
-    rawEducationData.crypto[0].status = 'completed';
+    rawEducationProgressData[0].status = 'completed';
+    rawEducationProgressData[1].status = 'completed';
     component.calculateProgressPercentage();
     fixture.detectChanges();
     const titleEl = fixture.debugElement.query(
@@ -62,19 +61,23 @@ fdescribe('GlobalProgressCardComponent', () => {
     expect(titleEl.nativeElement.innerHTML).toContain('financial_education.home.global_progress.card_state_50');
   });
 
-  // it('should render card_state_75 if progress is 75', () => {
-  //   component.modules = threeModulesDone;
-  //   component.calculateProgressPercentage();
-  //   fixture.detectChanges();
-  //   const titleEl = fixture.debugElement.query(
-  //     By.css('.gpc__card__accordion__item__content ion-text.ux-font-text-xxs')
-  //   );
-  //   expect(titleEl.nativeElement.innerHTML).toContain('financial_education.home.global_progress.card_state_75');
-  // });
+  it('should render card_state_75 if progress is 75', () => {
+    rawEducationProgressData[0].status = 'completed';
+    rawEducationProgressData[1].status = 'completed';
+    rawEducationProgressData[2].status = 'completed';
+    component.calculateProgressPercentage();
+    fixture.detectChanges();
+    const titleEl = fixture.debugElement.query(
+      By.css('.gpc__card__accordion__item__content ion-text.ux-font-text-xxs')
+    );
+    expect(titleEl.nativeElement.innerHTML).toContain('financial_education.home.global_progress.card_state_75');
+  });
 
   it('should render card_state_100 if progress is 100', () => {
-    rawEducationData.finance[0].status = 'completed';
-    rawEducationData.crypto[0].status = 'completed';
+    rawEducationProgressData[0].status = 'completed';
+    rawEducationProgressData[1].status = 'completed';
+    rawEducationProgressData[2].status = 'completed';
+    rawEducationProgressData[3].status = 'completed';
     component.calculateProgressPercentage();
     fixture.detectChanges();
     const titleEl = fixture.debugElement.query(
