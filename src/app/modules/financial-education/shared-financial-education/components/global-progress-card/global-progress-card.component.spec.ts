91fa36c17f3ab718fcc67efb12e7959c3cc76d33
Alan <alan@xcapit.com>
2022-07-25 10:15:11 -0300

    Working...

--- src/app/modules/financial-education/shared-financial-education/components/global-progress-card/global-progress-card.component.spec.ts
+++ src/app/modules/financial-education/shared-financial-education/components/global-progress-card/global-progress-card.component.spec.ts
@@ -32,7 +32,7 @@ fdescribe('GlobalProgressCardComponent', () => {
 
   it('should render card_state_0 if progress is 0', async () => {
     rawEducationData.finance[0].status = 'to_do';
-    rawEducationData.crypto[0].status = 'to_do';
+    rawEducationData.crypto[0].status = 'completed';
     fixture.detectChanges();
     await Promise.all([fixture.whenRenderingDone(), fixture.whenStable()]);
     fixture.detectChanges();
