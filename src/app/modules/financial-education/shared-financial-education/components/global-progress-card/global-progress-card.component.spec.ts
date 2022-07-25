5942d4fbdf7eadf2fe5f2477fa25245abff4dbe5
Alan <alan@xcapit.com>
2022-07-25 09:44:38 -0300

    Working...

--- src/app/modules/financial-education/shared-financial-education/components/global-progress-card/global-progress-card.component.spec.ts
+++ src/app/modules/financial-education/shared-financial-education/components/global-progress-card/global-progress-card.component.spec.ts
@@ -6,7 +6,7 @@ import { TranslateModule } from '@ngx-translate/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { rawEducationData } from '../../fixtures/rawEducationData';
 
-fdescribe('GlobalProgressCardComponent', () => {
+describe('GlobalProgressCardComponent', () => {
   let component: GlobalProgressCardComponent;
   let fixture: ComponentFixture<GlobalProgressCardComponent>;
 
