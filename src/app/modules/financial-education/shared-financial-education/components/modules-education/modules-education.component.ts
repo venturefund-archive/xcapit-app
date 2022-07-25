e3784389132a94eb3f1578f6a588b128c9677e94
Diego  Meineri <diego@xcapit.com>
2022-07-25 11:13:08 -0300

    Working...

--- src/app/modules/financial-education/shared-financial-education/components/modules-education/modules-education.component.ts
+++ src/app/modules/financial-education/shared-financial-education/components/modules-education/modules-education.component.ts
@@ -4,8 +4,8 @@ import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-modules-education',
   template: `
-    <ion-accordion-group [value]="this.module.opened" (ionChange)="this.change()">
-      <ion-accordion [value]="true" [disabled]="this.module.coming_soon" class="accordion-arrow-info">
+    <ion-accordion-group [value]="this.module.id" (ionChange)="this.change()">
+      <ion-accordion [value]="1" [disabled]="this.module.coming_soon" class="accordion-arrow-info">
         <ion-item class="ux-font-titulo-xs" slot="header" name="item_module">
           <img class="icon" name="module_img" [src]="this.module.icon" />
           <div>
