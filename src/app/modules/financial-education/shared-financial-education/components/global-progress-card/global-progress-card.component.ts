9f8d7769a429d27e5c30d4a7ee550de740b2dd47
Diego  Meineri <diego@xcapit.com>
2022-07-26 16:48:08 -0300

    feat(global-progress-card): add nft buttons

--- src/app/modules/financial-education/shared-financial-education/components/global-progress-card/global-progress-card.component.ts
+++ src/app/modules/financial-education/shared-financial-education/components/global-progress-card/global-progress-card.component.ts
@@ -3,16 +3,16 @@ import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-global-progress-card',
   template: `
-    <ion-accordion-group class="gpc__card">
-      <ion-accordion class="gpc__card__accordion">
-        <ion-item slot="header" class="gpc__card__accordion__item ion-no-padding" lines="none" color="info">
-          <app-circle-progress
-            *ngIf="this.allModules"
-            [percentage]="this.percentage"
-            [doneModules]="this.doneModules"
-            [allModules]="this.allModules"
-          ></app-circle-progress>
-          <div>
+    <div class="gpc">
+      <ion-accordion-group class="gpc__card">
+        <ion-accordion class="gpc__card__accordion" [ngClass]="{'non-bottom-radius': this.percentage === 100 }" >
+          <ion-item slot="header" class="gpc__card__accordion__item ion-no-padding" lines="none" color="info">
+            <app-circle-progress
+              *ngIf="this.allModules"
+              [percentage]="this.percentage"
+              [doneModules]="this.doneModules"
+              [allModules]="this.allModules"
+            ></app-circle-progress>
             <div class="gpc__card__accordion__item__content">
               <ion-text class="ux-font-header-titulo">{{
                 'financial_education.home.global_progress.card_title' | translate
@@ -42,40 +42,35 @@ import { NavController } from '@ionic/angular';
                 {{ 'financial_education.home.global_progress.card_state_100' | translate }}
               </ion-text>
             </div>
-            <div class="gpc__card__accordion__item__content">
-              <ion-text
-                class="gpc__card__accordion__item__content__nft-text ux-font-text-xs"
-                *ngIf="this.percentage === 0"
-              >
-                Generando NFT
-              </ion-text>
-              <ion-button
-                color="white"
-                fill="clear"
-                class="gpc__card__accordion__item__content__nft-text ux-link-xs"
-                *ngIf="this.percentage === 100"
-                (click)="this.goToWallet()"
-              >
-                Ver mi NFT
-              </ion-button>
-            </div>
-          </div>
-        </ion-item>
-        <div></div>
-        <ion-list slot="content" lines="none" class="gpc__card__accordion__list">
-          <ion-item class="ux-font-text-xxs gpc__card__accordion__list__item">
-            <ul class="gpc__card__accordion__list__item__ulist">
-              <li *ngFor="let module of this.modules">
-                <ion-text class="ux-font-text-xs">{{ this.module.title | translate }}</ion-text>
-                <ion-text class="ux-font-text-xxs">
-                  ({{ 'financial_education.home.statuses.' + this.module.status | translate }})</ion-text
-                >
-              </li>
-            </ul>
           </ion-item>
-        </ion-list>
-      </ion-accordion>
-    </ion-accordion-group>
+          <div></div>
+          <ion-list slot="content" lines="none" class="gpc__card__accordion__list">
+            <ion-item class="ux-font-text-xxs gpc__card__accordion__list__item">
+              <ul class="gpc__card__accordion__list__item__ulist">
+                <li *ngFor="let module of this.modules">
+                  <ion-text class="ux-font-text-xs">{{ this.module.title | translate }}</ion-text>
+                  <ion-text class="ux-font-text-xxs">
+                    ({{ 'financial_education.home.statuses.' + this.module.status | translate }})</ion-text
+                  >
+                </li>
+              </ul>
+            </ion-item>
+          </ion-list>
+        </ion-accordion>
+      </ion-accordion-group>
+    </div>
+    <div class="gpc__button" *ngIf="this.percentage === 100">
+        <ion-text class="gpc__button__nft-text ux-font-text-xs" *ngIf="this.percentage === 100 && !this.mintedNFT"> Generando NFT </ion-text>
+        <ion-button
+          color="white"
+          fill="clear"
+          class="gpc__button__nft-text ux-link-xs"
+          *ngIf="this.percentage === 100 && this.mintedNFT"
+          (click)="this.blablabla()"
+        >
+          Ver mi NFT
+        </ion-button>
+    </div>
   `,
   styleUrls: ['./global-progress-card.component.scss'],
 })
@@ -84,7 +79,7 @@ export class GlobalProgressCardComponent implements OnInit {
   doneModules: number;
   allModules: number;
   percentage: number;
-
+  mintedNFT = true;
   constructor(private navController: NavController) {}
 
   ngOnInit() {
@@ -94,10 +89,15 @@ export class GlobalProgressCardComponent implements OnInit {
   calculateProgressPercentage() {
     this.allModules = this.modules.length;
     this.doneModules = this.modules.filter((mod) => mod.status === 'completed').length;
-    this.percentage = (this.doneModules / this.allModules) * 100;
+    // this.percentage = (this.doneModules / this.allModules) * 100;
+    this.percentage = 100;
   }
 
-  goToWallet(){
-    this.navController.navigateForward('tabs/wallets')
+  goToWallet() {
+    this.navController.navigateForward('tabs/wallets');
+  }
+
+  blablabla() {
+    console.log('me clickeo');
   }
 }
