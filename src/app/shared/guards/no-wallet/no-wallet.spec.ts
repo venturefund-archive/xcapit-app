1faefd533a800cdad734ee2dd41e1ad1cd25974b
Martin317 <martin@xcapit.com>
2022-08-18 12:05:38 -0300

    feat(users): Add no wallet guard to new on boarding

---
+++ src/app/shared/guards/no-wallet/no-wallet.spec.ts
@@ -0,0 +1,65 @@
+import { TestBed } from '@angular/core/testing';
+import { StorageService } from 'src/app/modules/wallets/shared-wallets/services/storage-wallets/storage-wallets.service';
+import { FakeNavController } from 'src/testing/fakes/nav-controller.fake.spec';
+import { NavController } from '@ionic/angular';
+import { ActivatedRouteSnapshot } from '@angular/router';
+import { NoWallet } from './no-wallet.service';
+
+describe('NoWallet', () => {
+  let noWallet: NoWallet;
+  let fakeNavController: FakeNavController;
+  let navControllerSpy: jasmine.SpyObj<NavController>;
+  let storageServiceSpy: jasmine.SpyObj<StorageService>;
+  let routeSpy: jasmine.SpyObj<ActivatedRouteSnapshot>;
+  let routeSpyWithFallback: jasmine.SpyObj<ActivatedRouteSnapshot>;
+  const defaultFallbackUrl = '/users/login-new';
+  const customFallbackUrl = '/test/custom-fallback/url';
+
+  beforeEach(() => {
+    fakeNavController = new FakeNavController({});
+    navControllerSpy = fakeNavController.createSpy();
+    storageServiceSpy = jasmine.createSpyObj('StorageService', {
+      getWalletFromStorage: Promise.resolve({}),
+    });
+    routeSpy = jasmine.createSpyObj('ActivatedRouteSnapshot', {}, { data: {} });
+    routeSpyWithFallback = jasmine.createSpyObj(
+      'ActivatedRouteSnapshot',
+      {},
+      { data: { noWalletFallbackUrl: customFallbackUrl } }
+    );
+    TestBed.configureTestingModule({
+      providers: [
+        NoWallet,
+        { provide: NavController, useValue: navControllerSpy },
+        { provide: StorageService, useValue: storageServiceSpy },
+      ],
+    });
+  });
+
+  beforeEach(() => {
+    noWallet = TestBed.inject(NoWallet);
+  });
+
+  it('should create', () => {
+    noWallet = TestBed.inject(NoWallet);
+    expect(noWallet).toBeTruthy();
+  });
+
+  it('should hit route when wallet not exist', async () => {
+    const canActivateResult = await noWallet.canActivate(routeSpy);
+    expect(storageServiceSpy.getWalletFromStorage).toHaveBeenCalledTimes(1);
+    expect(canActivateResult).toBeTrue();
+  });
+
+  it('should navigate to default page when wallet exist', async () => {
+    storageServiceSpy.getWalletFromStorage.and.resolveTo({ addresses: {} });
+    await noWallet.canActivate(routeSpy);
+    expect(navControllerSpy.navigateForward).toHaveBeenCalledOnceWith([defaultFallbackUrl]);
+  });
+
+  it('should navigate to fallback url when wallet exist', async () => {
+    storageServiceSpy.getWalletFromStorage.and.resolveTo({ addresses: {} });
+    await noWallet.canActivate(routeSpyWithFallback);
+    expect(navControllerSpy.navigateForward).toHaveBeenCalledOnceWith([customFallbackUrl]);
+  });
+});
