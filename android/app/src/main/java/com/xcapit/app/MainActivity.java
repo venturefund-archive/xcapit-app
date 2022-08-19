5654d591e318d44acfefd8bbacdbbe58ce3a829e
Franco Schillage <franco@xcapit.com>
2022-08-19 10:12:06 -0300

    feat(project): Update Capacitor 4

--- android/app/src/main/java/com/xcapit/app/MainActivity.java
+++ android/app/src/main/java/com/xcapit/app/MainActivity.java
@@ -8,7 +8,7 @@ import com.getcapacitor.BridgeActivity;
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         registerPlugin(FirebaseAnalytics.class);
+        super.onCreate(savedInstanceState);
     }
 }
