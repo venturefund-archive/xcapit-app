278af0482239b2ad1ef9234fbd37a35e113959bb
Franco Schillage <franco@xcapit.com>
2022-08-25 17:57:17 -0300

    Working...

--- src/app/modules/fiat-ramps/shared-ramps/models/directa-price/directa-price.ts
+++ src/app/modules/fiat-ramps/shared-ramps/models/directa-price/directa-price.ts
@@ -29,10 +29,9 @@ export class DirectaPrice {
   }
 
   public value(): Observable<number> {
-    this._timer.pipe(
+    return this._timer.pipe(
       mergeMap(() => this.price()),
       map((res) => res.fx_rate)
     );
-    return of(1);
   }
 }
