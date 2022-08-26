f03b0e926bfd8c7bb7c7eea0d938ff31da064326
Matias Bruno <matias@it-up.com.ar>
2022-08-26 12:13:18 -0300

    Working...

--- src/app/modules/fiat-ramps/shared-ramps/models/directa-price/factory/directa-price-factory.spec.ts
+++ src/app/modules/fiat-ramps/shared-ramps/models/directa-price/factory/directa-price-factory.spec.ts
@@ -1,14 +1,11 @@
-import { Coin } from "src/app/modules/wallets/shared-wallets/interfaces/coin.interface";
-import { FiatRampsService } from "../../../services/fiat-ramps.service";
+import { Coin } from 'src/app/modules/wallets/shared-wallets/interfaces/coin.interface';
+import { FiatRampsService } from '../../../services/fiat-ramps.service';
 
 describe('DirectaPriceFactory', () => {
-    let coinSpy: jasmine.SpyObj<Coin>;
-    let fiatRampsServiceSpy: jasmine.SpyObj<FiatRampsService>;
-    beforeEach(() => {
-        coinSpy = jasmine.createSpyObj('Coin', {}, { value: 'MATIC' });
-        
-      });
-    it('create',()=>{
-
-    })
+  let coinSpy: jasmine.SpyObj<Coin>;
+  let fiatRampsServiceSpy: jasmine.SpyObj<FiatRampsService>;
+  beforeEach(() => {
+    coinSpy = jasmine.createSpyObj('Coin', {}, { value: 'MATIC' });
+  });
+  it('create', () => {});
 });
