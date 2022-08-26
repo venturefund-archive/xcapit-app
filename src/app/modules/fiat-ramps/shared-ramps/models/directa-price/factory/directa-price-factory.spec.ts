a47b75dabdcd9fceb11e953c72ccd3fe44b3adf3
Franco Schillage <franco@xcapit.com>
2022-08-26 12:15:46 -0300

    Working...

--- src/app/modules/fiat-ramps/shared-ramps/models/directa-price/factory/directa-price-factory.spec.ts
+++ src/app/modules/fiat-ramps/shared-ramps/models/directa-price/factory/directa-price-factory.spec.ts
@@ -1,11 +1,15 @@
 import { Coin } from 'src/app/modules/wallets/shared-wallets/interfaces/coin.interface';
 import { FiatRampsService } from '../../../services/fiat-ramps.service';
+import { DirectaPriceFactory } from './directa-price-factory';
+import { DirectaPrice } from '../directa-price';
 
-describe('DirectaPriceFactory', () => {
+fdescribe('DirectaPriceFactory', () => {
   let coinSpy: jasmine.SpyObj<Coin>;
   let fiatRampsServiceSpy: jasmine.SpyObj<FiatRampsService>;
   beforeEach(() => {
     coinSpy = jasmine.createSpyObj('Coin', {}, { value: 'MATIC' });
   });
-  it('create', () => {});
+  it('create', () => {
+    expect(new DirectaPriceFactory().new(2000, 'ARS', coinSpy, fiatRampsServiceSpy)).toBeInstanceOf(DirectaPrice);
+  });
 });
