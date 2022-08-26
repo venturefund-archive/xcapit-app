55d02beceac8d208fd4dde78a565949ac4818754
Matias Genre <matiasgenre@xcapit.com>
2022-08-26 17:22:43 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/transfer/transfer.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/transfer/transfer.spec.ts
@@ -1,6 +1,19 @@
+export class Transfer{
+  constructor(){    
+  }
+
+public fee(){
+  return true;
+}
+
+}
 
 fdescribe('Transfer', () => {
   it('new', () => {
     expect(new Transfer()).toBeTruthy();
   });
+
+  it('fee', ()=>{
+    expect(new Transfer().fee()).toBeTruthy();
+  })
 });
