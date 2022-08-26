e39e550ab0cff72b8f6960dafbca6d7f8f767955
Martin317 <martin@xcapit.com>
2022-08-26 17:18:07 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/transfers/transfers.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/transfers/transfers.spec.ts
@@ -4,6 +4,7 @@ import { FakeCovalentRepo } from '../covalent-repo/fake/fake-covalent-repo';
 
 export class Transfers {
   constructor(private readonly _aToken: RawToken, private readonly repo: CovalentRepo) {}
+  
 }
 
 fdescribe('Transfers', () => {
