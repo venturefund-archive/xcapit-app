91a27a7204f3745e47d3b331685c5debbeededb4
Matias Genre <matiasgenre@xcapit.com>
2022-08-26 16:59:03 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/covalent-repo/fake/fake-covalent-repo.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/covalent-repo/fake/fake-covalent-repo.spec.ts
@@ -1,10 +1,10 @@
 import { CovalentRepo } from '../covalent-repo.interface';
 import { RawToken } from '../../../../../swaps/shared-swaps/models/token-repo/token-repo';
 import { CovalentTransfersResponse } from '../../covalent-transfers-response/covalent-transfers-response';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 export class FakeCovalentRepo implements CovalentRepo {
-  constructor() {}
+  constructor(private readonly _transfersOfResponse: Observable<CovalentTransfersResponse>) {}
 
   transfersOf(aToken: RawToken, inAddress: string): Observable<CovalentTransfersResponse> {
     return undefined;
@@ -18,7 +18,7 @@ fdescribe('FakeCovalentRepo', () => {
 
   beforeEach(() => {
     aToken = jasmine.createSpyObj('RawToken', {}, { native: true, value: 'MATIC' });
-    fakeCovalentRepo = new FakeCovalentRepo(of);
+    fakeCovalentRepo = new FakeCovalentRepo();
   });
 
   it('new', () => {
