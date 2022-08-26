c6c96db5fc8f4f435ea8267689590f495b4a3093
Matias Genre <matiasgenre@xcapit.com>
2022-08-26 16:29:00 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/covalent-repo/covalent-repo.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/covalent-repo/covalent-repo.spec.ts
@@ -1,36 +1,8 @@
-import { HttpClient } from '@angular/common/http';
-import { async } from '@firebase/util';
 import { EnvService } from 'src/app/shared/services/env/env.service';
 import { FakeHttpClient } from 'src/testing/fakes/fake-http.spec';
 import { RawToken } from '../../../../swaps/shared-swaps/models/token-repo/token-repo';
-import { CovalentQuoteCurrency } from '../../types/covalent-quote-currencies.type';
-import { map } from 'rxjs/operators';
-import { CovalentTransfersResponse } from '../covalent-transfers-response/covalent-transfers-response';
-import { Coin } from '../../interfaces/coin.interface';
+import { CovalentRepo } from './covalent-repo';
 
-export class CovalentRepo {
-  constructor(private readonly _http: HttpClient | FakeHttpClient, private readonly _env: EnvService) {}
-
-  public transfersOf(aToken: RawToken, inAddress: string) {
-    return this._http
-      .get(this._url(aToken, inAddress), { headers: this._authHeaders() })
-      .pipe(map((res) => new CovalentTransfersResponse(res, aToken as Coin)));
-  }
-
-  private _authHeaders() {
-    return { Authorization: `Basic ${btoa(this._env.byKey('covalentApiKey') + ':')}` };
-  }
-
-  private _url(aToken: RawToken, address: string, quoteCurrency: CovalentQuoteCurrency = 'USD'): string {
-    return aToken.native
-      ? `${this._env.byKey('covalentApiUrl')}${
-          aToken.chainId
-        }/address/${address}/transactions_v2/?no-logs=true&match={"value":{"$ne": "0"}}&limit=10&quote-currency=${quoteCurrency}`
-      : `${this._env.byKey('covalentApiUrl')}${aToken.chainId}/address/${address}/transfers_v2/?contract-address=${
-          aToken.contract
-        }&limit=10&quote-currency=${quoteCurrency}`;
-  }
-}
 
 fdescribe('CovalentRepo', () => {
   let covalentRepo: CovalentRepo;
