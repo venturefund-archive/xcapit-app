8a51e467feea29c52a2ff5d016d0f9f2c4107579
luscherr14 <lucila@xcapit.com>
2022-08-26 17:30:26 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/transfers/transfers.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/transfers/transfers.spec.ts
@@ -1,15 +1,16 @@
 import { RawToken } from 'src/app/modules/swaps/shared-swaps/models/token-repo/token-repo';
 import { CovalentRepo } from '../covalent-repo/covalent-repo.interface';
 import { FakeCovalentRepo } from '../covalent-repo/fake/fake-covalent-repo';
+import { Transfer } from '../transfer/transfer.spec';
 
 export class Transfers {
   constructor(private readonly _aToken: RawToken, private readonly repo: CovalentRepo) {}
 
-  public transfersOf(aToken: RawToken, inAddress: string): Observable<CovalentTransfersResponse> {
-    return this.repo.transfersOf(aToken, inAddress)
-      
-        }
-  
+  /* public transfersOf(aToken: RawToken, inAddress: string): Promise<Transfer> {
+    return this.repo.transfersOf(aToken, inAddress).toPromise().then(res => {
+      new Transfer 
+    })
+  } */
 }
 
 fdescribe('Transfers', () => {
