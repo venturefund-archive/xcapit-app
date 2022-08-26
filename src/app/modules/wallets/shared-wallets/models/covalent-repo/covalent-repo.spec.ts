2dd3fcadecc6ca3e1ed18b0b2cc9e0680d2955fb
luscherr14 <lucila@xcapit.com>
2022-08-26 16:17:02 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/covalent-repo/covalent-repo.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/covalent-repo/covalent-repo.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { async } from '@firebase/util';
+import { EnvService } from 'src/app/shared/services/env/env.service';
 import { FakeHttpClient } from 'src/testing/fakes/fake-http.spec';
 import { RawToken } from '../../../../swaps/shared-swaps/models/token-repo/token-repo';
 import { CovalentQuoteCurrency } from '../../types/covalent-quote-currencies.type';
@@ -16,20 +17,22 @@ export class CovalentRepo {
     //   .pipe(map((res) => new CovalentTransfersResponse(res, asset)));
   }
 
-  private _url(aToken: RawToken, address: string, quoteCurrency: CovalentQuoteCurrency = 'USD'): string {
+ /*  private _url(aToken: RawToken, address: string, quoteCurrency: CovalentQuoteCurrency = 'USD'): string {
     return aToken.native
-      ? `${environment.covalentApiUrl}${asset.chainId}/address/${address}/transactions_v2/?no-logs=true&match={"value":{"$ne": "0"}}&limit=10&quote-currency=${quoteCurrency}`
-      : `${environment.covalentApiUrl}${asset.chainId}/address/${address}/transfers_v2/?contract-address=${asset.contract}&limit=10&quote-currency=${quoteCurrency}`;
-  }
+      ? `${environment.covalentApiUrl}${aToken.chainId}/address/${address}/transactions_v2/?no-logs=true&match={"value":{"$ne": "0"}}&limit=10&quote-currency=${quoteCurrency}`
+      : `${environment.covalentApiUrl}${aToken.chainId}/address/${address}/transfers_v2/?contract-address=${aToken.contract}&limit=10&quote-currency=${quoteCurrency}`;
+  } */
 }
 
 fdescribe('CovalentRepo', () => {
   let covalentRepo: CovalentRepo;
   let aToken: jasmine.SpyObj<RawToken>;
+  let envSpy: jasmine.SpyObj<EnvService>
   const inAddress = '';
 
   beforeEach(() => {
-    covalentRepo = new CovalentRepo(new FakeHttpClient(true));
+    envSpy = jasmine.createSpyObj('EnvService', {byKey: 'http://covalentUrl/'})
+    covalentRepo = new CovalentRepo(new FakeHttpClient(true), envSpy);
     aToken = jasmine.createSpyObj('RawToken', {}, { native: true, value: 'MATIC' });
   });
 
