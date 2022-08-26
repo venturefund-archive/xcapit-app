a9e3f1fceea2569d5edcb72e4cb0ceaa878e05a7
Martin317 <martin@xcapit.com>
2022-08-26 15:31:57 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/covalent-repo/covalent-repo.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/covalent-repo/covalent-repo.spec.ts
@@ -1,17 +1,20 @@
-import { HttpClient } from "@angular/common/http"
-import { FakeHttpClient } from "src/testing/fakes/fake-http.spec"
+import { HttpClient } from '@angular/common/http';
+import { FakeHttpClient } from 'src/testing/fakes/fake-http.spec';
 
-export class CovalentRepo{
-constructor (private readonly _http: HttpClient | FakeHttpClient) {}
+export class CovalentRepo {
+  constructor(private readonly _http: HttpClient | FakeHttpClient) {}
 
+  public transfersOf() {
+    return true;
+  }
 }
 
-fdescribe('CovalentRepo',()=>{
-    it('new' ,()=>{
-        expect(new CovalentRepo(new FakeHttpClient())).toBeTruthy()
-    });
+fdescribe('CovalentRepo', () => {
+  it('new', () => {
+    expect(new CovalentRepo(new FakeHttpClient())).toBeTruthy();
+  });
 
-    it('transfersOf',()=>{
-        expect(new CovalentRepo(new FakeHttpClient()).transfersOf()).toBeTruthy()
-    });
-})
+  it('transfersOf', () => {
+    expect(new CovalentRepo(new FakeHttpClient()).transfersOf()).toBeTruthy();
+  });
+});
