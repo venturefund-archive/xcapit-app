f6ec6f22a5ddaba9bc176750fae91b7b9b194c80
Franco Schillage <franco@xcapit.com>
2022-08-26 11:39:01 -0300

    Working...

--- src/app/modules/fiat-ramps/shared-ramps/models/directa-price/directa-price.spec.ts
+++ src/app/modules/fiat-ramps/shared-ramps/models/directa-price/directa-price.spec.ts
@@ -41,25 +41,4 @@ fdescribe('DirectaPrice', () => {
       });
     expect(subscription).toBeInstanceOf(Subscription);
   });
-  // it('should subscribe to interval', () => {
-  //   const kriptonDynamicPrice = KriptonDynamicPrice.create(
-  //     15,
-  //     'ars',
-  //     coinSpy,
-  //     new FakeHttpClient({}, amountOutResponse)
-  //   );
-  //   const subscription = kriptonDynamicPrice
-  //     .value()
-  //     .pipe(take(2))
-  //     .subscribe({
-  //       next: (res) => {
-  //         count++;
-  //         expect(res).toEqual(0.5);
-  //       },
-  //       complete: () => {
-  //         expect(count).toEqual(2);
-  //       },
-  //     });
-  //   expect(subscription).toBeInstanceOf(Subscription);
-  // });
 });
