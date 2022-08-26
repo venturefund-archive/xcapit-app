e639ae36d851e00b399fabb1467906c2f1825a97
Martin317 <martin@xcapit.com>
2022-08-26 16:55:15 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/covalent-repo/fake/fake-covalent-repo.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/covalent-repo/fake/fake-covalent-repo.spec.ts
@@ -1,9 +1,29 @@
-export class FakeCovalentRepo {
+import { CovalentRepo } from '../covalent-repo.interface';
+import { RawToken } from '../../../../../swaps/shared-swaps/models/token-repo/token-repo';
+import { CovalentTransfersResponse } from '../../covalent-transfers-response/covalent-transfers-response';
+import { Observable } from 'rxjs';
+
+export class FakeCovalentRepo implements CovalentRepo {
   constructor() {}
+
+  transfersOf(aToken: RawToken, inAddress: string): Observable<CovalentTransfersResponse> {
+    return undefined;
+  }
 }
 
 fdescribe('FakeCovalentRepo', () => {
+  let aToken: jasmine.SpyObj<RawToken>;
+  const inAddress = '';
+
+  beforeEach(() => {
+    aToken = jasmine.createSpyObj('RawToken', {}, { native: true, value: 'MATIC' });
+  });
+
   it('new', () => {
     expect(new FakeCovalentRepo()).toBeTruthy();
+  });
+
+  it('transfersOf', () => {
+    expect(new FakeCovalentRepo().transfersOf(aToken, inAddress)).toBeTruthy();
   });
 });
