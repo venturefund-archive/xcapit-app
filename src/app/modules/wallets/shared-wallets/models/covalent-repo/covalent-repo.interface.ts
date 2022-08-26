b279aaa07e15e826b0640ee929e8e711966f649a
Martin317 <martin@xcapit.com>
2022-08-26 16:50:29 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/covalent-repo/covalent-repo.interface.ts
+++ src/app/modules/wallets/shared-wallets/models/covalent-repo/covalent-repo.interface.ts
@@ -1,6 +1,7 @@
 import { CovalentTransfersResponse } from '../covalent-transfers-response/covalent-transfers-response';
 import { Observable } from 'rxjs';
+import { RawToken } from '../../../../swaps/shared-swaps/models/token-repo/token-repo';
 
 export interface CovalentRepo {
-  transfersOf: () => Observable<CovalentTransfersResponse>;
+  transfersOf: (aToken: RawToken, inAddress: string) => Observable<CovalentTransfersResponse>;
 }
