229bb76e91c58dcdbfa4ae424c9a3484b1de6f1b
Matias Bruno <matias@it-up.com.ar>
2022-08-26 11:59:51 -0300

    Working...

--- src/app/modules/fiat-ramps/shared-ramps/models/directa-price/directa-price.spec.ts
+++ src/app/modules/fiat-ramps/shared-ramps/models/directa-price/directa-price.spec.ts
@@ -24,7 +24,7 @@ fdescribe('DirectaPrice', () => {
     expect(directaPrice).toBeInstanceOf(DirectaPrice);
   });
 
-  xit('value', () => {
+  it('value', () => {
     const subscription = directaPrice.value().toPromise();
     expect(subscription).toEqual(3);
   });
