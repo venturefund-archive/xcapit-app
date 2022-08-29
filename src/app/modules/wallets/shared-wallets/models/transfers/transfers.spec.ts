4d29da6eb050c15e8a54b5dfbc3990f7abcb0c6f
Matias Genre <matiasgenre@xcapit.com>
2022-08-29 15:25:14 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/transfers/transfers.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/transfers/transfers.spec.ts
@@ -1,9 +1,8 @@
 import { RawToken } from 'src/app/modules/swaps/shared-swaps/models/token-repo/token-repo';
-import { CovalentRepo } from '../covalent-repo/covalent-repo.interface';
 import { FakeCovalentRepo } from '../covalent-repo/fake/fake-covalent-repo';
-import { Transfer } from '../transfer/transfer';
 import { rawTransfers } from '../covalent-repo/default/covalent-transfers.fixture';
 import { of } from 'rxjs';
+import { Transfers } from './transfers';
 
 export type RawTransfer = {
   block_signed_at: string;
@@ -24,21 +23,6 @@ export type RawTransfer = {
   gas_quote: number;
   gas_quote_rate: number;
 };
-
-export class Transfers {
-  constructor(
-    private readonly _aToken: RawToken,
-    private readonly _inAddress: string,
-    private readonly repo: CovalentRepo
-  ) {}
-
-  public all() {
-    return this.repo
-      .transfersOf(this._aToken, this._inAddress)
-      .toPromise()
-      .then((res) => res.data.items.map((rawTransfer: RawTransfer) => new Transfer(rawTransfer, this._aToken)));
-  }
-}
 
 fdescribe('Transfers', () => {
   let aToken: jasmine.SpyObj<RawToken>;
