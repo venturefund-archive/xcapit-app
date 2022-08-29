91064b2dcbcea17dd6c3a6c1fb208f2cf71624ec
luscherr14 <lucila@xcapit.com>
2022-08-29 17:32:37 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/transfer/native-transfer/native-transfer.ts
+++ src/app/modules/wallets/shared-wallets/models/transfer/native-transfer/native-transfer.ts
@@ -1,13 +1,14 @@
 import { AmountOf } from "src/app/modules/swaps/shared-swaps/models/amount-of/amount-of";
-import { Blockchain } from "src/app/modules/swaps/shared-swaps/models/blockchain/blockchain";
 import { RawToken } from "src/app/modules/swaps/shared-swaps/models/token-repo/token-repo";
+import { DefaultToken } from "src/app/modules/swaps/shared-swaps/models/token/token";
 import { RawTransfer } from "../../../types/raw-transfer.type";
+import { Transfer } from "../transfer.interface";
 
-export class NativeTransfer {
+export class NativeTransfer implements Transfer {
   constructor(private readonly _aRawTransfer: RawTransfer, private readonly _aToken: RawToken) {}
 
   public fee(): number {
-    return new AmountOf(this._aRawTransfer.gas_price.toString(), new Blockchain())
+    return new AmountOf(this._aRawTransfer.gas_price.toString(), new DefaultToken(this._aToken))
       .times(this._aRawTransfer.gas_spent)
       .value();
   }
