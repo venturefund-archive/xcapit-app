84956efe8e1388bd3e58a04966b484fbbbc53569
Matias Genre <matiasgenre@xcapit.com>
2022-08-29 17:25:46 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/transfer/transfer.interface.ts
+++ src/app/modules/wallets/shared-wallets/models/transfer/transfer.interface.ts
@@ -1,23 +1,3 @@
-import { RawToken } from '../../../../swaps/shared-swaps/models/token-repo/token-repo';
-import { AmountOf } from '../../../../swaps/shared-swaps/models/amount-of/amount-of';
-import { DefaultToken } from '../../../../swaps/shared-swaps/models/token/token';
-import { RawTransfer } from '../../types/raw-transfer.type';
-import { Blockchain } from '../../../../swaps/shared-swaps/models/blockchain/blockchain';
-
-export class Transfer {
-  constructor(private readonly _aRawTransfer: RawTransfer, private readonly _aToken: RawToken) {}
-
-  public fee(): number {
-    return new AmountOf(this._aRawTransfer.gas_price.toString(), new Blockchain())
-      .times(this._aRawTransfer.gas_spent)
-      .value();
-  }
-
-  public token(): RawToken {
-    return this._aToken;
-  }
-
-  public raw(): RawTransfer{
-    return this._aRawTransfer;
-  }
+export interface Transfer{
+  fee: () => number;
 }
