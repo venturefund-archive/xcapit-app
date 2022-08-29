bcbcc485e5d877f0d772acea2b24d7b405c57405
Martin317 <martin@xcapit.com>
2022-08-29 15:37:40 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/json-transfer/json-transfer.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/json-transfer/json-transfer.spec.ts
@@ -1,11 +1,11 @@
-import { Transfer } from "../transfer/transfer";
+import { Transfer } from '../transfer/transfer';
+import { rawTransfer } from '../covalent-repo/default/covalent-transfers.fixture';
+import { rawMATICTokenData } from '../../../../swaps/shared-swaps/models/fixtures/raw-one-inch-response-data';
 
-export class JSONTransfer {
-    
-}
+export class JSONTransfer {}
 
-fdescribe('JSONTransfer', ()=> {
-    it ('new', ()=>{
-        expect(new JSONTransfer(new Transfer())).toBeTruthy();
-    });
+fdescribe('JSONTransfer', () => {
+  it('new', () => {
+    expect(new JSONTransfer(new Transfer(rawTransfer, rawMATICTokenData))).toBeTruthy();
+  });
 });
