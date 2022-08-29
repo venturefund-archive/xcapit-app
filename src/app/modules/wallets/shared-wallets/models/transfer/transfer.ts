49f54ca8ddfaed63aa0f7267bfacf1d89593f89d
Martin317 <martin@xcapit.com>
2022-08-29 17:18:39 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/transfer/transfer.ts
+++ src/app/modules/wallets/shared-wallets/models/transfer/transfer.ts
@@ -2,12 +2,13 @@ import { RawToken } from '../../../../swaps/shared-swaps/models/token-repo/token
 import { AmountOf } from '../../../../swaps/shared-swaps/models/amount-of/amount-of';
 import { DefaultToken } from '../../../../swaps/shared-swaps/models/token/token';
 import { RawTransfer } from '../../types/raw-transfer.type';
+import { Blockchain } from '../../../../swaps/shared-swaps/models/blockchain/blockchain';
 
 export class Transfer {
   constructor(private readonly _aRawTransfer: RawTransfer, private readonly _aToken: RawToken) {}
 
   public fee(): number {
-    return new AmountOf(this._aRawTransfer.gas_price.toString(), new DefaultToken(this._aToken))
+    return new AmountOf(this._aRawTransfer.gas_price.toString(), new Blockchain())
       .times(this._aRawTransfer.gas_spent)
       .value();
   }
