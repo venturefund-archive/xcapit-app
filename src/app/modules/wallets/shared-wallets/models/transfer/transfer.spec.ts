2977c830e09d72f871f885cb906eca65d5fc0e97
Martin317 <martin@xcapit.com>
2022-08-29 15:14:39 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/transfer/transfer.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/transfer/transfer.spec.ts
@@ -3,26 +3,29 @@ import { rawMATICData } from 'src/app/modules/swaps/shared-swaps/models/fixtures
 import { RawToken } from '../../../../swaps/shared-swaps/models/token-repo/token-repo';
 import { rawTransfer } from '../covalent-repo/default/covalent-transfers.fixture';
 import { RawTransfer } from '../transfers/transfers.spec';
+import { DefaultToken } from '../../../../swaps/shared-swaps/models/token/token';
 
 export class Transfer {
   constructor(private readonly _aRawTransfer: RawTransfer, private readonly _aToken: RawToken) {}
 
   public fee() {
-    return new AmountOf(this._aRawTransfer.gas_spent.toString(), new token this._aToken).times(this._aRawTransfer.gas_price).value();
+    return new AmountOf(this._aRawTransfer.gas_spent.toString(), new DefaultToken(this._aToken))
+      .times(this._aRawTransfer.gas_price)
+      .value();
   }
 }
 
 fdescribe('Transfer', () => {
   let transfer: Transfer;
-  beforeEach (()=> {
+  beforeEach(() => {
     transfer = new Transfer(rawTransfer, rawMATICData);
   });
-  
+
   it('new', () => {
-    expect( transfer).toBeTruthy();
+    expect(transfer).toBeTruthy();
   });
-  
+
   it('fee', () => {
-    expect( transfer.fee()).toBeTruthy();
+    expect(transfer.fee()).toEqual(2);
   });
 });
