055d86530f99c351e29ec3f2f15295ac4c50f382
luscherr14 <lucila@xcapit.com>
2022-08-29 17:56:27 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/transfer/no-native-transfer/no-native-transfer.ts
+++ src/app/modules/wallets/shared-wallets/models/transfer/no-native-transfer/no-native-transfer.ts
@@ -1,6 +1,10 @@
 import { AmountOf } from 'src/app/modules/swaps/shared-swaps/models/amount-of/amount-of';
+import { BlockchainRepo } from 'src/app/modules/swaps/shared-swaps/models/blockchain-repo/blockchain-repo';
+import { Blockchain } from 'src/app/modules/swaps/shared-swaps/models/blockchain/blockchain';
+import { Blockchains } from 'src/app/modules/swaps/shared-swaps/models/blockchains/blockchains';
 import { RawToken } from 'src/app/modules/swaps/shared-swaps/models/token-repo/token-repo';
 import { DefaultToken } from 'src/app/modules/swaps/shared-swaps/models/token/token';
+import { environment } from 'src/environments/environment';
 import { RawTransfer } from '../../../types/raw-transfer.type';
 import { Transfer } from '../transfer.interface';
 
@@ -8,7 +12,7 @@ export class NoNativeTransfer implements Transfer {
   constructor(private readonly _aRawTransfer: RawTransfer, private readonly _aToken: RawToken) {}
 
   public fee(): number {
-    return new AmountOf(this._aRawTransfer.gas_price.toString(), new DefaultToken(this._aToken))
+    return new AmountOf(this._aRawTransfer.gas_price.toString(), this._nativeToken())
       .times(this._aRawTransfer.gas_spent)
       .value();
   }
@@ -19,5 +23,9 @@ export class NoNativeTransfer implements Transfer {
 
   public raw(): RawTransfer {
     return this._aRawTransfer;
+  }
+
+  private _nativeToken() {
+   return new Blockchains(new BlockchainRepo(environment.BLOCKCHAIN_DATA)).oneByName(this._aToken.network).nativeToken();
   }
 }
