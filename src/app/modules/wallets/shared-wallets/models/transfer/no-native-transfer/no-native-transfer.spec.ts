dfeb8c3b29d1b419f9710705146b5dd1db1989f9
Martin317 <martin@xcapit.com>
2022-08-29 17:41:05 -0300

    Working...

---
+++ src/app/modules/wallets/shared-wallets/models/transfer/no-native-transfer/no-native-transfer.spec.ts
@@ -0,0 +1,28 @@
+import { rawMATICData } from 'src/app/modules/swaps/shared-swaps/models/fixtures/raw-tokens-data';
+import { Transfer } from '../transfer.interface';
+import { NativeTransfer } from './native-transfer';
+import { rawTransfer } from '../../covalent-repo/default/covalent-transfers.fixture';
+import { NoNativeTransfer } from './no-native-transfer';
+
+fdescribe('NoNativeTransfer', () => {
+  let noNativeTransfer: Transfer;
+
+  beforeEach(() => {
+    noNativeTransfer = new NoNativeTransfer(rawTransfer, rawMATICData);
+  });
+
+  it('new', () => {
+    expect(noNativeTransfer).toBeTruthy();
+  });
+
+  it('fee', () => {
+    expect(noNativeTransfer.fee()).toEqual(0.024715417086192657);
+  });
+  it('token', () => {
+    expect(noNativeTransfer.token()).toEqual(rawMATICData);
+  });
+
+  it('raw', () => {
+    expect(noNativeTransfer.raw()).toEqual(rawTransfer);
+  });
+});
