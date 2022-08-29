6650706ad68a34071376cf68beb0bee924f26623
luscherr14 <lucila@xcapit.com>
2022-08-29 15:35:47 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/json-transfer/json-transfer.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/json-transfer/json-transfer.spec.ts
@@ -1,9 +1,11 @@
+import { Transfer } from "../transfer/transfer";
+
 export class JSONTransfer {
     
 }
 
 fdescribe('JSONTransfer', ()=> {
     it ('new', ()=>{
-        expect(new JSONTransfer()).toBeTruthy();
+        expect(new JSONTransfer(new Transfer())).toBeTruthy();
     });
 });
