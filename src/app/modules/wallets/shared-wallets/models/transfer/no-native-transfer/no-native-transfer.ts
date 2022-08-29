28ef3d6df58f8996a625d73b28460684408317f8
Martin317 <martin@xcapit.com>
2022-08-29 17:40:48 -0300

    Working...

---
+++ src/app/modules/wallets/shared-wallets/models/transfer/no-native-transfer/no-native-transfer.ts
@@ -0,0 +1,23 @@
+import { AmountOf } from "src/app/modules/swaps/shared-swaps/models/amount-of/amount-of";
+import { RawToken } from "src/app/modules/swaps/shared-swaps/models/token-repo/token-repo";
+import { DefaultToken } from "src/app/modules/swaps/shared-swaps/models/token/token";
+import { RawTransfer } from "../../../types/raw-transfer.type";
+import { Transfer } from "../transfer.interface";
+
+export class NativeTransfer implements Transfer {
+  constructor(private readonly _aRawTransfer: RawTransfer, private readonly _aToken: RawToken) {}
+
+  public fee(): number {
+    return new AmountOf(this._aRawTransfer.gas_price.toString(), new DefaultToken(this._aToken))
+      .times(this._aRawTransfer.gas_spent)
+      .value();
+  }
+
+  public token(): RawToken {
+    return this._aToken;
+  }
+
+  public raw(): RawTransfer{
+    return this._aRawTransfer;
+  }
+}
