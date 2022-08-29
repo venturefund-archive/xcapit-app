99754567c03d430cc13269cb5203972634a4d91e
Martin317 <martin@xcapit.com>
2022-08-29 18:00:01 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/transfer/no-native-transfer/no-native-transfer.ts
+++ src/app/modules/wallets/shared-wallets/models/transfer/no-native-transfer/no-native-transfer.ts
@@ -1,9 +1,7 @@
 import { AmountOf } from 'src/app/modules/swaps/shared-swaps/models/amount-of/amount-of';
 import { BlockchainRepo } from 'src/app/modules/swaps/shared-swaps/models/blockchain-repo/blockchain-repo';
-import { Blockchain } from 'src/app/modules/swaps/shared-swaps/models/blockchain/blockchain';
 import { Blockchains } from 'src/app/modules/swaps/shared-swaps/models/blockchains/blockchains';
 import { RawToken } from 'src/app/modules/swaps/shared-swaps/models/token-repo/token-repo';
-import { DefaultToken } from 'src/app/modules/swaps/shared-swaps/models/token/token';
 import { environment } from 'src/environments/environment';
 import { RawTransfer } from '../../../types/raw-transfer.type';
 import { Transfer } from '../transfer.interface';
@@ -22,10 +20,12 @@ export class NoNativeTransfer implements Transfer {
   }
 
   public raw(): RawTransfer {
-    return this._aRawTransfer;
+    return { ...this._aRawTransfer, ...this._aRawTransfer.transfers[0] };
   }
 
   private _nativeToken() {
-   return new Blockchains(new BlockchainRepo(environment.BLOCKCHAIN_DATA)).oneByName(this._aToken.network).nativeToken();
+    return new Blockchains(new BlockchainRepo(environment.BLOCKCHAIN_DATA))
+      .oneByName(this._aToken.network)
+      .nativeToken();
   }
 }
