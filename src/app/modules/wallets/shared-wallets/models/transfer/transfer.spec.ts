4549870a909c8a20f4e840f21940e9222b71cb17
luscherr14 <lucila@xcapit.com>
2022-08-29 15:12:16 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/transfer/transfer.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/transfer/transfer.spec.ts
@@ -1,13 +1,14 @@
+import { AmountOf } from 'src/app/modules/swaps/shared-swaps/models/amount-of/amount-of';
 import { rawMATICData } from 'src/app/modules/swaps/shared-swaps/models/fixtures/raw-tokens-data';
 import { RawToken } from '../../../../swaps/shared-swaps/models/token-repo/token-repo';
 import { rawTransfer } from '../covalent-repo/default/covalent-transfers.fixture';
 import { RawTransfer } from '../transfers/transfers.spec';
 
 export class Transfer {
-  constructor(private readonly _aRawTransfer: RawTransfer, private readonly aToken: RawToken) {}
+  constructor(private readonly _aRawTransfer: RawTransfer, private readonly _aToken: RawToken) {}
 
   public fee() {
-    return true;
+    return new AmountOf(this._aRawTransfer.gas_spent.toString(), new token this._aToken).times(this._aRawTransfer.gas_price).value();
   }
 }
 
