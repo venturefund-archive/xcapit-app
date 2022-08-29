97cfc44367ae949376a3bb51a548bd33c3110a82
Matias Genre <matiasgenre@xcapit.com>
2022-08-29 15:26:14 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/transfer/transfer.ts
+++ src/app/modules/wallets/shared-wallets/models/transfer/transfer.ts
@@ -1,7 +1,7 @@
-import { RawTransfer } from '../transfers/transfers.spec';
 import { RawToken } from '../../../../swaps/shared-swaps/models/token-repo/token-repo';
 import { AmountOf } from '../../../../swaps/shared-swaps/models/amount-of/amount-of';
 import { DefaultToken } from '../../../../swaps/shared-swaps/models/token/token';
+import { RawTransfer } from '../../types/raw-transfer.type';
 
 export class Transfer {
   constructor(private readonly _aRawTransfer: RawTransfer, private readonly _aToken: RawToken) {}
