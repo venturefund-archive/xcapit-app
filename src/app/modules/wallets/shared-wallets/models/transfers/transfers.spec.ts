840b38e4825e600bd8b27c6c49cd53db4b5e52dc
Martin317 <martin@xcapit.com>
2022-08-29 14:31:08 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/transfers/transfers.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/transfers/transfers.spec.ts
@@ -4,10 +4,23 @@ import { FakeCovalentRepo } from '../covalent-repo/fake/fake-covalent-repo';
 import { Transfer } from '../transfer/transfer.spec';
 
 export class Transfers {
-  constructor(private readonly _aToken: RawToken, private readonly repo: CovalentRepo) {}
-  public all() {
-    return true;
-  }
+  constructor(
+    private readonly _aToken: RawToken,
+    private readonly _inAddress: string,
+    private readonly repo: CovalentRepo
+  ) {}
+
+  // public all() {
+  //   return this.repo.transfersOf(this._aToken, this._inAddress).toPromise().then(res=>{
+  //     return res.map(item=>{
+  //       new Transfer(
+  //         as RawTransfer,
+  //         this._aToken
+  //       )
+  //     })
+  //   });
+  // }
+
   /* public transfersOf(aToken: RawToken, inAddress: string): Promise<Transfer> {
     return this.repo.transfersOf(aToken, inAddress).toPromise().then(res => {
       new Transfer(
@@ -19,12 +32,16 @@ export class Transfers {
 
 fdescribe('Transfers', () => {
   let aToken: jasmine.SpyObj<RawToken>;
+  const inAddress = '';
 
   beforeEach(() => {
     aToken = jasmine.createSpyObj('RawToken', {}, { native: true, value: 'MATIC' });
   });
 
   it('new', () => {
-    expect(new Transfers(aToken, new FakeCovalentRepo()).all()).toBeTruthy();
+    expect(new Transfers(aToken, inAddress, new FakeCovalentRepo())).toBeTruthy();
   });
+  // it('all', () => {
+  //   expect(new Transfers(aToken, inAddress, new FakeCovalentRepo()).all()).toBeTruthy();
+  // });
 });
