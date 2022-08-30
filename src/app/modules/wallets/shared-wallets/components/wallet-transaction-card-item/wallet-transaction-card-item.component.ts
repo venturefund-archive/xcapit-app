cdcc4a91531c37a3195f7bf691b183210cc5bb4e
Martin317 <martin@xcapit.com>
2022-08-30 16:31:47 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/components/wallet-transaction-card-item/wallet-transaction-card-item.component.ts
+++ src/app/modules/wallets/shared-wallets/components/wallet-transaction-card-item/wallet-transaction-card-item.component.ts
@@ -6,6 +6,8 @@ import { BrowserService } from 'src/app/shared/services/browser/browser.service'
 import { CovalentTransfer } from '../../models/covalent-transfer/covalent-transfer';
 import { ScanUrlOf } from '../../models/scan-url-of/scan-url-of';
 import { TransactionDetailsService } from '../../services/transaction-details/transaction-details.service';
+import { JSONTransfer } from '../../models/json-transfer/json-transfer';
+import { Transfer } from '../../models/transfer/transfer.interface';
 
 @Component({
   selector: 'app-wallet-transaction-card-item',
@@ -13,14 +15,14 @@ import { TransactionDetailsService } from '../../services/transaction-details/tr
     <div>
       <div class="wtci">
         <div>
-          <ion-img class="wtci__img" [src]="this.transaction.icon"></ion-img>
+          <ion-img class="wtci__img" [src]="this.tplTransfer.icon"></ion-img>
         </div>
         <div class="wtci__content">
           <div class="wtci__content__top">
             <div class="wtci__content__top__type_date_hash">
               <div class="wtci__content__top__type_date_hash__type_date">
                 <ion-label class="ux-font-lato ux-fsize-14 ux-fweight-bold">{{
-                  'wallets.transactions.' + this.transaction.type | translate
+                  'wallets.transactions.' + this.tplTransfer.type | translate
                 }}</ion-label>
                 <ion-label class="ux-font-text-xxs date">
                   {{ this.formattedDate }}
@@ -28,17 +30,17 @@ import { TransactionDetailsService } from '../../services/transaction-details/tr
               </div>
               <div class="wtci__content__top__type_date_hash__hash">
                 <ion-text (click)="this.openTransactionUrl()" class="ux-font-text-xs">
-                  {{ this.transaction.hash }}
+                  {{ this.tplTransfer.tx_hash }}
                 </ion-text>
               </div>
             </div>
             <div class="wtci__content__top__column">
               <ion-label class="ux-font-lato ux-fsize-14 ux-fweight-semibold">
-                {{ this.transaction.amount | formattedAmount }} {{ this.transaction.symbol }}
+                {{ this.tplTransfer.amount | formattedAmount }} {{ this.tplTransfer.contract_ticker_symbol }}
               </ion-label>
               <div class="ux-font-num-subtitulo wtci__content__top__column__badge">
-                <ion-badge  [ngClass]="{'confirmed': this.transaction.successful, 'declined': !this.transaction.successful}">
-                  {{ (this.transaction.successful ? 'wallets.transactions.confirmed' : 'wallets.transactions.declined' ) | translate }}</ion-badge>
+                <ion-badge  [ngClass]="{'confirmed': this.tplTransfer.successful, 'declined': !this.tplTransfer.successful}">
+                  {{ (this.tplTransfer.successful ? 'wallets.transactions.confirmed' : 'wallets.transactions.declined' ) | translate }}</ion-badge>
               </div>
             </div>
           </div>
@@ -50,17 +52,19 @@ import { TransactionDetailsService } from '../../services/transaction-details/tr
   styleUrls: ['./wallet-transaction-card-item.component.scss'],
 })
 export class WalletTransactionCardItemComponent implements OnInit {
-  @Input() transaction: CovalentTransfer;
+  @Input() transaction: Transfer;
   @Input() last: boolean;
   @Input() network: string;
   formattedDate: string;
+  tplTransfer: any;
 
   constructor(
     private navController: NavController,
     private transactionDetailsService: TransactionDetailsService,) {}
 
   ngOnInit() {
-    this.formattedDate = this.formatDate(this.transaction.date);
+    this.formattedDate = this.formatDate(this.tplTransfer.block_signed_at);
+    this.tplTransfer = new JSONTransfer(this.transaction).value()
   }
 
   openTransactionUrl() {
@@ -72,6 +76,8 @@ export class WalletTransactionCardItemComponent implements OnInit {
     return format(parseISO(value), 'dd-MM-yyyy');
   }
 
-  private async saveTransactionDetails(){ this.transactionDetailsService.transactionData = this.transaction}
+  private saveTransactionDetails() {
+    this.transactionDetailsService.transactionData = this.transaction
+  }
 
 }
--- src/app/modules/wallets/shared-wallets/services/transaction-details/transaction-details.service.ts
+++ src/app/modules/wallets/shared-wallets/services/transaction-details/transaction-details.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { TypedData } from 'eth-sig-util';
 import { CovalentTransfer } from '../../models/covalent-transfer/covalent-transfer';
+import { Transfer } from '../../models/transfer/transfer.interface';
 
 @Injectable({
     providedIn: 'root',
   })
   export class TransactionDetailsService {
-    transactionData: CovalentTransfer;
-  
+    transactionData: Transfer;
+
     constructor() {}
   }
