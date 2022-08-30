f440985e1581bcf3e3c1f8d5670907c632c0b2a7
luscherr14 <lucila@xcapit.com>
2022-08-30 15:25:36 -0300

    Working...

--- src/app/modules/wallets/asset-detail/asset-detail.page.ts
+++ src/app/modules/wallets/asset-detail/asset-detail.page.ts
@@ -105,7 +105,7 @@ import { Transfer } from '../shared-wallets/models/transfer/transfer.interface';
           </div>
           <div class="wad__transaction__wallet-transaction-card">
             <app-wallet-transaction-card
-              [transfers]="this.transfers"
+              [transactions]="this.transfers"
               [network]="this.currency.network"
             ></app-wallet-transaction-card>
           </div>
