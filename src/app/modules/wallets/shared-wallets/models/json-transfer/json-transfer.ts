981ac4941a8167fa16f01c07eec0243ca4b8657c
luscherr14 <lucila@xcapit.com>
2022-08-30 15:58:46 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/json-transfer/json-transfer.ts
+++ src/app/modules/wallets/shared-wallets/models/json-transfer/json-transfer.ts
@@ -6,6 +6,8 @@ export class JSONTransfer {
   public value(): any {
     return {
       fee: this._aTransfer.fee(),
+      type: this._aTransfer.type(),
+      icon: this._aTransfer.icon(),
       token: this._aTransfer.token(),
       ...this._aTransfer.raw(),
     };
