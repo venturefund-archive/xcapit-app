df6d38c1d9124a3321ed59565e8465d5f56716e4
Martin317 <martin@xcapit.com>
2022-08-30 16:20:38 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/json-transfer/json-transfer.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/json-transfer/json-transfer.spec.ts
@@ -67,11 +67,10 @@ const expectedValueNoNative = {
 
 fdescribe('JSONTransfer', () => {
   let jsonTransfer: JSONTransfer;
-  const aTestAddressNative = '0x1111111254fb6c44bac0bed2854e76f90643097d';
-  const aTestAddressNoNative = '0x1111111254fb6c44bac0bed2854e76f90643097d';
+  const aTestAddress = '0x1111111254fb6c44bac0bed2854e76f90643097d';
 
   beforeEach(() => {
-    jsonTransfer = new JSONTransfer(new NativeTransfer(rawTransfer, rawMATICData, aTestAddressNative));
+    jsonTransfer = new JSONTransfer(new NativeTransfer(rawTransfer, rawMATICData, aTestAddress));
   });
 
   it('new', () => {
@@ -79,11 +78,16 @@ fdescribe('JSONTransfer', () => {
   });
 
   it('value', () => {
-    expect(jsonTransfer.value()).toEqual(expectedValue);
+    const value = jsonTransfer.value();
+    expect(value.tx_hash).toEqual('0x7d81572bd8028f7a8a1ea1205f825ce0bf5dad2c757b38af4b78ec2e49e03db9');
+    expect(value.type).toEqual('IN');
+    expect(value.icon).toEqual('assets/img/wallet-transactions/received.svg');
   });
 
   it('value when no native transfer', () => {
-    jsonTransfer = new JSONTransfer(new NoNativeTransfer(rawNoNativeTransfer, rawMATICData, aTestAddressNoNative));
-    expect(jsonTransfer.value()).toEqual(expectedValueNoNative);
+    jsonTransfer = new JSONTransfer(new NoNativeTransfer(rawNoNativeTransfer, rawMATICData, aTestAddress));
+    const value = jsonTransfer.value();
+    expect(value.tx_hash).toEqual('0x7d81572bd8028f7a8a1ea1205f825ce0bf5dad2c757b38af4b78ec2e49e03db9');
+    expect(value.type).toEqual('IN');
+    expect(value.icon).toEqual('assets/img/wallet-transactions/received.svg');
   });
-});
