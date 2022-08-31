60154802f0ac8f0615882bcbae048cbb41a7fc99
luscherr14 <lucila@xcapit.com>
2022-08-31 15:13:55 -0300

    Working...

--- src/app/modules/wallets/transaction-details/transaction-details.page.spec.ts
+++ src/app/modules/wallets/transaction-details/transaction-details.page.spec.ts
@@ -22,7 +22,7 @@ fdescribe('TransactionDetailsPage', () => {
       'TransactionDetailsService',
       {},
       {
-        transactionData: { all: () => [] },
+        transactionData: { },
       }
     );
 
