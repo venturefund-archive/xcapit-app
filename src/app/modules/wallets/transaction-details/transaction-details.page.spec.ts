6aa473bbda6745f4d15b577af79ae4d086807775
Matias Genre <matiasgenre@xcapit.com>
2022-08-31 15:28:11 -0300

    Working...

--- src/app/modules/wallets/transaction-details/transaction-details.page.spec.ts
+++ src/app/modules/wallets/transaction-details/transaction-details.page.spec.ts
@@ -19,13 +19,13 @@ fdescribe('TransactionDetailsPage', () => {
   let fakeNavController: FakeNavController;
 
   beforeEach(waitForAsync(() => {
-    // transactionDetailsServiceSpy = jasmine.createSpyObj(
-    //   'TransactionDetailsService',
-    //   {},
-    //   {
-    //     transactionData: (rawTransfer),
-    //   }
-    // );
+    transactionDetailsServiceSpy = jasmine.createSpyObj(
+      'TransactionDetailsService',
+      {},
+      {
+        transactionData: {all():any},
+      }
+    );
 
     browserServiceSpy = jasmine.createSpyObj('BrowserService', {
       open: Promise.resolve(),
