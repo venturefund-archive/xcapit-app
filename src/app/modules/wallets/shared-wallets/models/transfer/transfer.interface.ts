4bd992424645846033f15835686397c209ea7aba
Martin317 <martin@xcapit.com>
2022-08-31 11:24:59 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/transfer/transfer.interface.ts
+++ src/app/modules/wallets/shared-wallets/models/transfer/transfer.interface.ts
@@ -6,6 +6,7 @@ export interface Transfer {
   type: () => 'IN' | 'OUT';
   icon: () => string;
   amount: () => number;
+  quoteAmount: () => number;
   token: () => RawToken;
   raw: () => RawTransfer;
 }
