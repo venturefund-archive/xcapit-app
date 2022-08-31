51a4aa5d6de3e9c2ab7557bba4e15770a5629c37
Martin317 <martin@xcapit.com>
2022-08-31 11:52:16 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/transfers/factory/transfers.factory.ts
+++ src/app/modules/wallets/shared-wallets/models/transfers/factory/transfers.factory.ts
@@ -1,9 +1,15 @@
-import { Transfers } from "../transfers";
+import { Transfers } from '../transfers';
+import { Injectable } from '@angular/core';
+import { RawToken } from '../../../../../swaps/shared-swaps/models/token-repo/token-repo';
+import { DefaultCovalentRepo } from '../../covalent-repo/default/default-covalent-repo';
+import { HttpClient } from '@angular/common/http';
+import { EnvService } from '../../../../../../shared/services/env/env.service';
 
 @Injectable({ providedIn: 'root' })
 export class TransfersFactory {
-  public new(
-  ): Transfers {
-    return TwoPiInvestment.create(_anInvestmentProduct, _aSigner, _anApiWalletService);
+  constructor(private http: HttpClient, private env: EnvService) {}
+
+  public create(_aToken: RawToken, _anAddress: string): Transfers {
+    return new Transfers(_aToken, _anAddress, new DefaultCovalentRepo(this.http, this.env));
   }
 }
