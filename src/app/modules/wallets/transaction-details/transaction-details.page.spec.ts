d41acaae20a52d1ea9f0ccaf8da31469459251e7
Matias Genre <matiasgenre@xcapit.com>
2022-08-31 15:04:53 -0300

    Working...

--- src/app/modules/wallets/transaction-details/transaction-details.page.spec.ts
+++ src/app/modules/wallets/transaction-details/transaction-details.page.spec.ts
@@ -3,21 +3,34 @@ import { IonicModule } from '@ionic/angular';
 import { TransactionDetailsPage } from './transaction-details.page';
 import { TranslateModule } from '@ngx-translate/core';
 import { TransactionDetailsService } from '../shared-wallets/services/transaction-details/transaction-details.service';
+import { BrowserService } from 'src/app/shared/services/browser/browser.service';
 
 fdescribe('TransactionDetailsPage', () => {
   let component: TransactionDetailsPage;
   let fixture: ComponentFixture<TransactionDetailsPage>;
-  let transactionDetailsServiceSpy: jasmine.SpyObj<TransactionDetailsService>
+  let transactionDetailsServiceSpy: jasmine.SpyObj<TransactionDetailsService>;
+  let browserServiceSpy: jasmine.SpyObj<BrowserService>;
 
   beforeEach(waitForAsync(() => {
-    transactionDetailsServiceSpy = jasmine.createSpyObj('TransactionDetailsService', {},{
-      transactionData: {all: ()=> [] },
+    transactionDetailsServiceSpy = jasmine.createSpyObj(
+      'TransactionDetailsService',
+      {},
+      {
+        transactionData: { all: () => [] },
+      }
+    );
+
+    browserServiceSpy = jasmine.createSpyObj('BrowserService', {
+      open: Promise.resolve(),
     });
 
     TestBed.configureTestingModule({
       declarations: [TransactionDetailsPage],
       imports: [IonicModule.forRoot(), TranslateModule.forRoot()],
-      providers:[{ provide: TransactionDetailsService, useValue: transactionDetailsServiceSpy }]
+      providers: [
+        { provide: TransactionDetailsService, useValue: transactionDetailsServiceSpy },
+        { provide: BrowserService, useValue: browserServiceSpy },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TransactionDetailsPage);
