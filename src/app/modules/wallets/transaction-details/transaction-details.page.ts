6b98392dcce5add98bb1b40fd9153cffcafd44a1
luscherr14 <lucila@xcapit.com>
2022-08-31 12:21:56 -0300

    Working...

--- src/app/modules/wallets/transaction-details/transaction-details.page.ts
+++ src/app/modules/wallets/transaction-details/transaction-details.page.ts
@@ -80,7 +80,7 @@ import { JSONTransfer } from '../shared-wallets/models/json-transfer/json-transf
             </div>
             <div class="td__card__item__link">
               <ion-text (click)="this.openTransactionUrl()" class="ux-link-xs">{{
-                'wallets.transaction_details.link' | translate: {network: this.tplTransfer.token.network}
+                'wallets.transaction_details.link' | translate
               }}</ion-text>
             </div>
           </div>
