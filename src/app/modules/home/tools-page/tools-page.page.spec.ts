8ddbb3de28646b992e407cb21ce6ec12b0c7b51b
rassyu <andresmatter@hotmail.com>
2022-09-27 12:20:45 -0300

    test(tools-page.page): add missing tests to improve coverage

--- src/app/modules/home/tools-page/tools-page.page.spec.ts
+++ src/app/modules/home/tools-page/tools-page.page.spec.ts
@@ -1,11 +1,12 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 import { AppStorageService } from 'src/app/shared/services/app-storage/app-storage.service';
 import { TrackService } from 'src/app/shared/services/track/track.service';
 import { FakeFeatureFlagDirective } from 'src/testing/fakes/feature-flag-directive.fake.spec';
+import { FakeNavController } from 'src/testing/fakes/nav-controller.fake.spec';
 import { WalletBackupService } from '../../wallets/shared-wallets/services/wallet-backup/wallet-backup.service';
 import { ToolsPage } from './tools-page.page';
 
@@ -23,6 +24,8 @@ describe('ToolPagePage', () => {
   let walletBackupServiceSpy: jasmine.SpyObj<WalletBackupService>;
   let trackServiceSpy: jasmine.SpyObj<TrackService>;
   let appStorageServiceSpy: jasmine.SpyObj<AppStorageService>;
+  let fakeNavController: FakeNavController;
+  let navControllerSpy: jasmine.SpyObj<NavController>;
 
   beforeEach(waitForAsync(() => {
     walletBackupServiceSpy = jasmine.createSpyObj('WalletBackupService', {
@@ -30,6 +33,8 @@ describe('ToolPagePage', () => {
     });
     trackServiceSpy = jasmine.createSpyObj('TrackServiceSpy', { trackEvent: Promise.resolve(true) });
     appStorageServiceSpy = jasmine.createSpyObj('AppStorageService', { get: dataTest });
+    fakeNavController = new FakeNavController();
+    navControllerSpy = fakeNavController.createSpy();
 
     TestBed.configureTestingModule({
       declarations: [ ToolsPage, FakeFeatureFlagDirective ],
@@ -38,6 +43,7 @@ describe('ToolPagePage', () => {
         { provide: WalletBackupService, useValue: walletBackupServiceSpy },
         { provide: TrackService, useValue: trackServiceSpy },
         { provide: AppStorageService, useValue: appStorageServiceSpy },
+        { provide: NavController, useValue: navControllerSpy }
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
@@ -64,4 +70,28 @@ describe('ToolPagePage', () => {
     component.ionViewWillEnter();
     expect(trackServiceSpy.trackEvent).toHaveBeenCalledTimes(1);
   });
+
+  it('should navigate to objetive page when ux_go_to_planner is clicked', async () => {
+    fixture.detectChanges();
+    component.ionViewWillEnter();
+    fixture.debugElement.query(By.css('app-tools-card')).triggerEventHandler('primaryActionEvent', 'ux_go_to_planner');
+    fixture.detectChanges();
+    expect(navControllerSpy.navigateForward).toHaveBeenCalledWith(['/financial-planner/result-objetive']);
+  });
+
+  it('should navigate to donations when ux_go_to_donations is clicked', async () => {
+    fixture.detectChanges();
+    component.ionViewWillEnter();
+    fixture.debugElement.query(By.css('app-tools-card')).triggerEventHandler('primaryActionEvent', 'ux_go_to_donations');
+    await fixture.whenStable();
+    expect(navControllerSpy.navigateForward).toHaveBeenCalledWith(['/donations/causes']);
+  });
+
+  it('should navigate to objetive page when objetive card is clicked', async () => {
+    component.data = dataTest;
+    component.ionViewWillEnter();
+    fixture.detectChanges();
+    fixture.debugElement.query(By.css('div.card-objetive')).nativeElement.click();;
+    expect(navControllerSpy.navigateForward).toHaveBeenCalledWith(['/financial-planner/result-objetive']);
+  })
 });
