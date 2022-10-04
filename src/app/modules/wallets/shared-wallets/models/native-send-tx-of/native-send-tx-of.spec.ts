6249a61d2d53ec77b5b5423e7143a4577100727d
Diego  Meineri <diego@xcapit.com>
2022-10-04 15:09:08 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/native-send-tx-of/native-send-tx-of.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/native-send-tx-of/native-send-tx-of.spec.ts
@@ -3,22 +3,6 @@ import { BlockchainTx } from 'src/app/modules/swaps/shared-swaps/models/blockcha
 import { FakeWallet, Wallet } from 'src/app/modules/swaps/shared-swaps/models/wallet/wallet';
 
 
-export class NativeSendTxOf implements BlockchainTx {
-
-    constructor(private _wallet: Wallet, private _to: string, private _amount: number) { }
-
-    async value(): Promise<Transaction> {
-      return new Transaction().add(
-        SystemProgram.transfer({
-          fromPubkey: new PublicKey(this._wallet.address()),
-          toPubkey: new PublicKey(this._to),
-          lamports: this._amount * LAMPORTS_PER_SOL,
-        }),
-      );
-    }
-}
-
-
 fdescribe('NativeSendTxOf', () => {
   let transaction: NativeSendTxOf;
 
