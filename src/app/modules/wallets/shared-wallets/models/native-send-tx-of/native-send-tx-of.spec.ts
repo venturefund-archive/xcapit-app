9027e40b085b525f02ecd21f4ea848c79ee794e2
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-10-04 14:42:26 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/native-send-tx-of/native-send-tx-of.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/native-send-tx-of/native-send-tx-of.spec.ts
@@ -1,4 +1,4 @@
-import { Transaction } from '@solana/web3.js';
+import { SystemProgram, Transaction } from '@solana/web3.js';
 import { BlockchainTx } from 'src/app/modules/swaps/shared-swaps/models/blockchain-tx';
 import { FakeWallet, Wallet } from 'src/app/modules/swaps/shared-swaps/models/wallet/wallet';
 
@@ -8,6 +8,14 @@ export class NativeSendTxOf implements BlockchainTx {
     constructor(private _wallet: Wallet, private _to: string, private _amount: number) { }
 
     async value(): Promise<Transaction> {
+      new Transaction().add(
+        SystemProgram.transfer({
+          fromPubkey: wallet1.publicKey,
+          toPubkey: wallet2.publicKey,
+          lamports: 1 * LAMPORTS_PER_SOL,
+        }),
+      );
+
       return null;
     }
 }
