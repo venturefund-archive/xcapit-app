06f564daac2b2592fc14ddd028e40700342bdad3
Diego  Meineri <diego@xcapit.com>
2022-10-04 12:50:29 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/native-send-tx-of/native-send-tx-of.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/native-send-tx-of/native-send-tx-of.spec.ts
@@ -1,9 +1,16 @@
+
+import { FakeWallet, Wallet } from "src/app/modules/swaps/shared-swaps/models/wallet/wallet";
+
 export class NativeSendTxOf {
-    
+    constructor(
+        private _wallet : Wallet,
+        private _to : string,
+        private _amount : number
+    ){}
 }
 
 fdescribe('NativeSendTxOf', ()=>{
     it('new', ()=>{
-        expect(new NativeSendTxOf()).toBeTruthy();
+        expect(new NativeSendTxOf(new FakeWallet(), '', 1)).toBeTruthy();
     })
 })
