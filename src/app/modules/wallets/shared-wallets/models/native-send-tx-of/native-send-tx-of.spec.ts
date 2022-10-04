dda5125f9d9c026ca7d91264f092efd853528981
Diego  Meineri <diego@xcapit.com>
2022-10-04 15:10:11 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/native-send-tx-of/native-send-tx-of.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/native-send-tx-of/native-send-tx-of.spec.ts
@@ -1,18 +1,12 @@
-import { LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
-import { BlockchainTx } from 'src/app/modules/swaps/shared-swaps/models/blockchain-tx';
-import { FakeWallet, Wallet } from 'src/app/modules/swaps/shared-swaps/models/wallet/wallet';
-
+import { FakeWallet } from 'src/app/modules/swaps/shared-swaps/models/wallet/wallet';
+import { NativeSendTxOf } from './native-send-tx-of';
 
 fdescribe('NativeSendTxOf', () => {
   let transaction: NativeSendTxOf;
 
   beforeEach(() => {
     transaction = new NativeSendTxOf(
-      new FakeWallet(
-        Promise.resolve(false),
-        '',
-        'iuwtfpp8yzDrJNQbHXBSufSCZKhGctw5bQFAx23VgBH'
-      ),
+      new FakeWallet(Promise.resolve(false), '', 'iuwtfpp8yzDrJNQbHXBSufSCZKhGctw5bQFAx23VgBH'),
       'iuwtfpp8yzDrJNQbHXBSufSCZKhGctw5bQFAx23VgBH',
       1
     );
