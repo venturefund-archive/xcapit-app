cec47c30cca42ab9bd5e8c603570152da527e038
Federico Marquez <federico@xcapit.com>
2022-10-04 10:13:43 -0300

    test(Wallets): Fix tests

--- src/app/modules/wallets/create-password/create-password.page.spec.ts
+++ src/app/modules/wallets/create-password/create-password.page.spec.ts
@@ -21,7 +21,7 @@ import { IonicStorageService } from 'src/app/shared/services/ionic-storage/ionic
 import { WalletBackupService } from '../shared-wallets/services/wallet-backup/wallet-backup.service';
 import { FakeActivatedRoute } from '../../../../testing/fakes/activated-route.fake.spec';
 import { BlockchainsFactory } from '../../swaps/shared-swaps/models/blockchains/factory/blockchains.factory';
-import { Wallet } from 'ethers';
+import { ethers, Wallet } from 'ethers';
 import { XAuthService } from '../../users/shared-users/services/x-auth/x-auth.service';
 
 const testMnemonic: Mnemonic = {
@@ -86,6 +86,7 @@ describe('CreatePasswordPage', () => {
   let xAuthServiceSpy: jasmine.SpyObj<XAuthService>;
 
   beforeEach(() => {
+    spyOn(ethers.Wallet, 'fromMnemonic').and.returnValue(walletSpy);
     fakeLoadingService = new FakeLoadingService();
     loadingServiceSpy = fakeLoadingService.createSpy();
     fakeNavController = new FakeNavController();
@@ -135,10 +136,7 @@ describe('CreatePasswordPage', () => {
 
     walletServiceSpy = jasmine.createSpyObj(
       'WalletService',
-      {
-        create: Promise.resolve({}),
-        createForDerivedPath: walletSpy,
-      },
+      {},
       {
         coins: [],
       }
