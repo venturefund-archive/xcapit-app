7b28d6128bab419146efe884cab39c7aaf50d171
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-10-04 12:21:16 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/fakes/fake-connection.spec.ts
+++ src/app/modules/swaps/shared-swaps/models/fakes/fake-connection.spec.ts
@@ -5,4 +5,9 @@ fdescribe('Fake Connection', () => {
   it('new', () => {
     expect(new FakeConnection()).toBeTruthy();
   });
+
+it('send transaction', () => {
+  expect(false).toBeTruthy();
+});
+
 });
