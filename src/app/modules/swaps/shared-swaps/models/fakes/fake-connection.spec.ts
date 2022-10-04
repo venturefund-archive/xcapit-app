45c500232c65bb950ce1406ceacecc6ad07ed82f
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-10-04 12:22:24 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/fakes/fake-connection.spec.ts
+++ src/app/modules/swaps/shared-swaps/models/fakes/fake-connection.spec.ts
@@ -1,12 +1,16 @@
-export class FakeConnection {}
+export class FakeConnection {
+
+  sendTransaction() {
+    return true;
+  }
+}
 
 fdescribe('Fake Connection', () => {
   let connection: FakeConnection;
 
-beforeEach(() => {
-  connection = new FakeConnection();
-});
-
+  beforeEach(() => {
+    connection = new FakeConnection();
+  });
 
   it('new', () => {
     expect(connection).toBeTruthy();
