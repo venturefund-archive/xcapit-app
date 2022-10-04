66ec2de381ea47041dc14f0616c65158b0ce767f
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-10-04 14:38:36 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/native-send-tx-of/native-send-tx-of.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/native-send-tx-of/native-send-tx-of.spec.ts
@@ -1,9 +1,14 @@
+import { TransactionRequest } from '@ethersproject/abstract-provider';
+import { Transaction } from '@solana/web3.js';
 import { BlockchainTx } from 'src/app/modules/swaps/shared-swaps/models/blockchain-tx';
 import { FakeWallet, Wallet } from 'src/app/modules/swaps/shared-swaps/models/wallet/wallet';
 
 
 export class NativeSendTxOf implements BlockchainTx {
-  constructor(private _wallet: Wallet, private _to: string, private _amount: number) {}
+    constructor(private _wallet: Wallet, private _to: string, private _amount: number) { }
+    value(): Promise<TransactionRequest | Transaction> {
+        throw new Error('Method not implemented.');
+    }
 }
 
 
