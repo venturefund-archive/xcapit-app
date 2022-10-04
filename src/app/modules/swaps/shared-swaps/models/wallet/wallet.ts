10b0f36768537b0e814b0242229b8cc9e33a1d86
Diego  Meineri <diego@xcapit.com>
2022-10-04 12:13:46 -0300

    Working...

--- src/app/modules/swaps/shared-swaps/models/wallet/wallet.ts
+++ src/app/modules/swaps/shared-swaps/models/wallet/wallet.ts
@@ -110,8 +110,9 @@ export class SolanaWallet implements Wallet {
   }
 
   async sendTxs(transactions: BlockchainTx[]): Promise<boolean> {
+    const derivedWallet = this._derivedWallet(await this._decryptedWallet());
     for (const tx of transactions) {
-      await this._connection.sendTransaction((await tx.value()) as Transaction, [this._derivedWallet(await this._decryptedWallet())]);
+      await this._connection.sendTransaction((await tx.value()) as Transaction, [derivedWallet]);
     }
     return true;
   }
