169ee14fa8eec31204a8d23c2294b5c33f4eb96f
Matias Genre <matiasgenre@xcapit.com>
2022-10-04 14:48:38 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/native-send-tx-of/native-send-tx-of.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/native-send-tx-of/native-send-tx-of.spec.ts
@@ -8,14 +8,13 @@ export class NativeSendTxOf implements BlockchainTx {
     constructor(private _wallet: Wallet, private _to: string, private _amount: number) { }
 
     async value(): Promise<Transaction> {
-      const tx = new Transaction().add(
+      return new Transaction().add(
         SystemProgram.transfer({
           fromPubkey: new PublicKey(this._wallet.address()),
           toPubkey: new PublicKey(this._to),
           lamports: this._amount * LAMPORTS_PER_SOL,
         }),
       );
-      return null;
     }
 }
 
