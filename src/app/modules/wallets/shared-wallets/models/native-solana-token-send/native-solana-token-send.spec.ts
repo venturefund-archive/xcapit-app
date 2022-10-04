3d5d20ce9443b4669f7bcdf91a8b25cdd7ffb6e8
Martin317 <martin@xcapit.com>
2022-10-04 10:53:05 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/native-solana-token-send/native-solana-token-send.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/native-solana-token-send/native-solana-token-send.spec.ts
@@ -1,14 +1,14 @@
 import { Send } from '../../interfaces/send.interface';
+import { TransactionResponse as SolanaTransactionResponse } from '@solana/web3.js';
 
 export class NativeSolanaTokenSend implements Send {
-  send(): Promise<string | TransactionResponse> {
+  send(): Promise<SolanaTransactionResponse> {
     return Promise.resolve(undefined);
   }
 
   get tokenDecimals(): number {
     return 0;
   }
-
 }
 
 fdescribe('NativeSolanaTokenSend', () => {
@@ -16,5 +16,5 @@ fdescribe('NativeSolanaTokenSend', () => {
     expect(new NativeSolanaTokenSend()).toBeTruthy();
   });
 
-  it('')
+  it('');
 });
