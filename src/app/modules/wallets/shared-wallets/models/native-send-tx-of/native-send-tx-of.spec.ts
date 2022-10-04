3649e7e6160f2832f037c70cca9bcc4a8766d7e9
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-10-04 14:38:06 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/models/native-send-tx-of/native-send-tx-of.spec.ts
+++ src/app/modules/wallets/shared-wallets/models/native-send-tx-of/native-send-tx-of.spec.ts
@@ -1,15 +1,21 @@
-import { FakeWallet, Wallet } from "src/app/modules/swaps/shared-swaps/models/wallet/wallet";
+import { FakeWallet, Wallet } from 'src/app/modules/swaps/shared-swaps/models/wallet/wallet';
 
 export class NativeSendTxOf {
-    constructor(
-        private _wallet : Wallet,
-        private _to : string,
-        private _amount : number
-    ){}
+  constructor(private _wallet: Wallet, private _to: string, private _amount: number) {}
 }
-//TODO
-fdescribe('NativeSendTxOf', ()=>{
-    it('new', ()=>{
-        expect(new NativeSendTxOf(new FakeWallet(), '', 1)).toBeTruthy();
-    })
-})
+
+fdescribe('NativeSendTxOf', () => {
+  let transaction: NativeSendTxOf;
+
+  beforeEach(() => {
+    transaction = new NativeSendTxOf(new FakeWallet(), '', 1);
+  });
+
+  it('new', () => {
+    expect(transaction).toBeTruthy();
+  });
+
+  it('value', () => {
+    expect(transaction.value()).toBeTruthy();
+  });
+});
