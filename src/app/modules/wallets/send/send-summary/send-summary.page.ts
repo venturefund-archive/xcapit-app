d5b994ce7e8c7e4f64ea2de1f5180de092d0285f
Diego  Meineri <diego@xcapit.com>
2022-10-05 11:29:27 -0300

    Working...

--- src/app/modules/wallets/send/send-summary/send-summary.page.ts
+++ src/app/modules/wallets/send/send-summary/send-summary.page.ts
@@ -91,8 +91,6 @@ export class SendSummaryPage implements OnInit {
   ionViewWillEnter() {
     this.isSending = false;
     this.summaryData = this.transactionDataService.transactionData;
-    console.log('summary data')
-    console.log(this.summaryData)
     this.blockchain = this.blockchains.create().oneByName(this.summaryData.network);
     this.checkMode();
   }
