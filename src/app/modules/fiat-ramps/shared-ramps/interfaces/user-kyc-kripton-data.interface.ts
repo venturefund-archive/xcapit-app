a1058e179e41a682f504ad77c3f10825cb3b1dac
Alan <alan@xcapit.com>
2022-10-20 16:20:22 -0300

    fix(kripton): ident fixed

--- src/app/modules/fiat-ramps/shared-ramps/interfaces/user-kyc-kripton-data.interface.ts
+++ src/app/modules/fiat-ramps/shared-ramps/interfaces/user-kyc-kripton-data.interface.ts
@@ -1,18 +1,18 @@
 export interface UserKycKriptonData {
-    firstName: string,
-    lastName:string,
-    birthday: string,
-    nationality: string;
-    document: string,
-    document_number: string,
-    gender: string,
-    marital_status: string,
-    country_code: string,
-    phone_number: string,
-    street: string;
-    number: string;
-    floor?: string;
-    apartment?: string;
-    city: string;
-    zipCode: string;
+  firstName: string;
+  lastName: string;
+  birthday: string;
+  nationality: string;
+  document: string;
+  document_number: string;
+  gender: string;
+  marital_status: string;
+  country_code: string;
+  phone_number: string;
+  street: string;
+  number: string;
+  floor?: string;
+  apartment?: string;
+  city: string;
+  zipCode: string;
 }
