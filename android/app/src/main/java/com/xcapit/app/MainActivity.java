0a6ca18b617eb2705527b69eee42c301c47a085b
Martin317 <martin@xcapit.com>
2022-10-27 12:09:59 -0300

    chore: Working

--- android/app/src/main/java/com/xcapit/app/MainActivity.java
+++ android/app/src/main/java/com/xcapit/app/MainActivity.java
@@ -4,14 +4,16 @@ import android.os.Bundle;
 
 import androidx.core.splashscreen.SplashScreen;
 
+import com.epicshaggy.biometric.NativeBiometric;
 import com.getcapacitor.community.firebaseanalytics.FirebaseAnalytics;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
-    registerPlugin(FirebaseAnalytics.class);
     SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
     super.onCreate(savedInstanceState);
+    registerPlugin(FirebaseAnalytics.class);
+    registerPlugin(NativeBiometric.class);
   }
 }
