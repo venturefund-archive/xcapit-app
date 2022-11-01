3c01d3f238fd1e3faa13b33d3d8c5cfef60c3d18
Federico Marquez <federico@xcapit.com>
2022-11-01 15:58:25 -0300

    Working...

--- src/app/modules/fiat-ramps/kripton-operation-detail/kripton-operation-detail.page.ts
+++ src/app/modules/fiat-ramps/kripton-operation-detail/kripton-operation-detail.page.ts
@@ -131,8 +131,7 @@ import { FiatRampProvider } from '../shared-ramps/interfaces/fiat-ramp-provider.
               <div class="kod__card-container__card__date__container__date">
                 <div class="kod__card-container__card__date__container__date__title">
                   <ion-text class="ux-font-titulo-xs">
-                    <!-- {{ 'fiat_ramps.kripton_operation_detail.address' | translate }} -->
-                    Fecha
+                    {{ 'fiat_ramps.kripton_operation_detail.date' | translate }}
                   </ion-text>
                 </div>
                 <div class="kod__card-container__card__date__container__date__content">
