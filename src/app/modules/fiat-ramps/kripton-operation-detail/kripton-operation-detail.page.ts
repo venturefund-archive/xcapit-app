d3fcd8e278363a321e8bcb0d9fad564836128587
rassyu <andresmatter@hotmail.com>
2022-11-01 11:53:31 -0300

    Working...

--- src/app/modules/fiat-ramps/kripton-operation-detail/kripton-operation-detail.page.ts
+++ src/app/modules/fiat-ramps/kripton-operation-detail/kripton-operation-detail.page.ts
@@ -19,7 +19,7 @@ import { FiatRampProvider } from '../shared-ramps/interfaces/fiat-ramp-provider.
       <div class="kod__card-container">
         <ion-card class="kod__card-container__card">
           <div class="kod__card-container__card__title">
-            <ion-text>
+            <ion-text class="ux-font-text-xl">
               {{ 'fiat_ramps.kripton_operation_detail.title' | translate }}
             </ion-text>
           </div>
