d80e5e54a9ab7aad3a46008f2dfde6e770111771
--global <--global>
2022-11-01 10:14:41 -0300

    Working...

---
+++ src/app/shared/components/token-network-badge/token-network-badge.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-token-network-badge',
+  templateUrl: './token-network-badge.component.html',
+  styleUrls: ['./token-network-badge.component.scss'],
+})
+export class TokenNetworkBadgeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
