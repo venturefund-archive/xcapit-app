c2b5ccf57b9a1dabe689eab4db525d666ff385d0
--global <--global>
2022-11-01 10:18:32 -0300

    Working...

--- src/app/shared/components/token-network-badge/token-network-badge.component.ts
+++ src/app/shared/components/token-network-badge/token-network-badge.component.ts
@@ -4,7 +4,7 @@ import { Component, Input, OnInit } from '@angular/core';
   selector: 'app-token-network-badge',
   template: `
     <ion-badge [color]="this.networkColors[this.tplBlockchain.name]" class="ux-badge ux-font-num-subtitulo">{{
-      this.tplBlockchain.name | formattedNetwork | uppercase
+      this.blockchainName | formattedNetwork | uppercase
     }}</ion-badge>
   `,
   styleUrls: ['./token-network-badge.component.scss'],
