5c892b0bf0ca61ba7ac37c9d94186169a94f93a2
--global <--global>
2022-11-01 10:14:41 -0300

    Working...

---
+++ src/app/shared/components/token-network-badge/token-network-badge.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { TokenNetworkBadgeComponent } from './token-network-badge.component';
+
+describe('TokenNetworkBadgeComponent', () => {
+  let component: TokenNetworkBadgeComponent;
+  let fixture: ComponentFixture<TokenNetworkBadgeComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TokenNetworkBadgeComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TokenNetworkBadgeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
