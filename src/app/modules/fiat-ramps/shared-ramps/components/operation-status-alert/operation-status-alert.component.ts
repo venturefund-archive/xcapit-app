a3e6f3a3605fac1cde637fd2181fe0adbb77808a
rassyu <andresmatter@hotmail.com>
2022-11-04 11:25:59 -0300

    feat(kripton-operation-detail): WIP add alert state component

--- src/app/modules/fiat-ramps/shared-ramps/components/operation-status-alert/operation-status-alert.component.ts
+++ src/app/modules/fiat-ramps/shared-ramps/components/operation-status-alert/operation-status-alert.component.ts
@@ -3,15 +3,10 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-operation-status-alert',
   template: `
-    <div
-      class="osa__information"
-      (click)="this.alertAction()"
-      [ngClass]="this.backgroundClass"
-    >
-      <img src="assets/img/wallets/backup-information-circle.svg" class="osa__information__icon" />
+    <div class="osa__information" (click)="this.alertAction()" [ngClass]="this.backgroundClass">
+      <img [src]="this.icon" class="osa__information__icon" />
       <div class="osa__information__text">
         <ion-text [ngClass]="this.textClass" [innerHTML]="this.text | translate"> </ion-text>
-        <!-- TEST TEST TEST -->
       </div>
     </div>
   `,
@@ -19,22 +14,45 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class OperationStatusAlertComponent implements OnInit {
   @Input() operationStatus: string;
-  text = 'fiat_ramps.kripton_operation_detail.state_toast.created';
-  textClass = "ux-home-status-card";
-  backgroundClass = "ux-warning-background-card";
-  
-  constructor() { }
+  textClass = 'ux-home-status-card';
+  icon: string;
+  text: string;
+  backgroundClass: string;
+
+  constructor() {}
 
   ngOnInit() {
+    console.log('operationStatus: ', this.operationStatus);
     this.setState();
   }
 
   alertAction() {
-    // this.cardClicked.emit();
+    if (this.operationStatus === 'incompleta') {
+      console.log('Redireccion a detalle operacion...')
+      // Navegar detalle operacion
+    }
   }
 
   setState() {
-    this.backgroundClass = 'ux-warning-background-card';
+    switch (this.operationStatus) {
+      case 'incompleta':
+        this.icon = 'assets/img/wallets/warning-circle.svg';
+        this.text = 'fiat_ramps.kripton_operation_detail.state_toast.created';
+        this.backgroundClass = 'ux-warning-background-card';
+        // Click: Llevar a detalle de operacion para subir comprobante
+        break;
+      case 'anulada':
+        this.icon = 'assets/img/wallets/error-circle.svg';
+        this.text = 'fiat_ramps.kripton_operation_detail.state_toast.nullified';
+        this.backgroundClass = 'ux-error-background-card';
+        break;
+      case 'cancelada':
+        this.icon = 'assets/img/wallets/error-circle.svg';
+        this.text = 'fiat_ramps.kripton_operation_detail.state_toast.canceled';
+        this.backgroundClass = 'ux-error-background-card';
+        break;
+    }
+    // STATES: Incompleta, En progreso, confirmada, anulada, cancelada
+    // En progreso y Confirmada fuera del scope de este componente
   }
-
 }
