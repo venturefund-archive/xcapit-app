af5e79098feab83fb4e41dd1eece758a4ea09638
Martin317 <martin@xcapit.com>
2022-11-07 16:53:08 -0300

    feat(wallets): Remove wallet exists and fetch balances at same time

--- src/app/modules/defi-investments/shared-defi-investments/models/total-invested-balance-of/injectable/total-invested-balance-of.injectable.ts
+++ src/app/modules/defi-investments/shared-defi-investments/models/total-invested-balance-of/injectable/total-invested-balance-of.injectable.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { EnvService } from '../../../../../../shared/services/env/env.service';
-import { TotalInvestedBalanceOf } from '../default/total-invested-balance-of';
+import { DefaultTotalInvestedBalanceOf } from '../default/default-total-invested-balance-of';
 import { CacheService } from '../../../../../../shared/services/cache/cache.service';
 
 @Injectable({ providedIn: 'root' })
 export class TotalInvestedBalanceOfInjectable {
   constructor(private _cacheService: CacheService, private _http: HttpClient, private _env: EnvService) {}
 
-  create(anAddress: string, pids: number[]): TotalInvestedBalanceOf {
-    return new TotalInvestedBalanceOf(anAddress, pids, this._cacheService, this._http, this._env);
+  create(anAddress: string, pids: number[]): DefaultTotalInvestedBalanceOf {
+    return new DefaultTotalInvestedBalanceOf(anAddress, pids, this._cacheService, this._http, this._env);
   }
 }
