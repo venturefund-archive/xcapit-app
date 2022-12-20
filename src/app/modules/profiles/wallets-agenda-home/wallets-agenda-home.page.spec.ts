35b468e5053c18188e65329ad037cec212dc92ec
Matias Bruno <matiasbruno97@gmail.com>
2022-12-20 16:47:56 -0300

    feat(contacts): finish page and tests

--- src/app/modules/profiles/wallets-agenda-home/wallets-agenda-home.page.spec.ts
+++ src/app/modules/profiles/wallets-agenda-home/wallets-agenda-home.page.spec.ts
@@ -1,24 +1,82 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
+import { By } from '@angular/platform-browser';
+import { IonicModule, NavController } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
+import { IonicStorageService } from 'src/app/shared/services/ionic-storage/ionic-storage.service';
+import { FakeNavController } from 'src/testing/fakes/nav-controller.fake.spec';
+import { FakeTrackClickDirective } from 'src/testing/fakes/track-click-directive.fake.spec';
+import { TrackClickDirectiveTestHelper } from 'src/testing/track-click-directive-test.spec';
 import { WalletsAgendaHomePage } from './wallets-agenda-home.page';
 
 describe('WalletsAgendaHomePage', () => {
   let component: WalletsAgendaHomePage;
   let fixture: ComponentFixture<WalletsAgendaHomePage>;
+  let ionicStorageServiceSpy: jasmine.SpyObj<IonicStorageService>;
+  let navControllerSpy: jasmine.SpyObj<NavController>;
+  let fakeNavController: FakeNavController;
+  let trackClickDirectiveHelper: TrackClickDirectiveTestHelper<WalletsAgendaHomePage>;
 
+  const contacts = [
+    { 
+      address: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', 
+      name: 'TestWallet', 
+      networks: ['ERC20', 'RSK'] 
+    },
+  ];
   beforeEach(waitForAsync(() => {
+    ionicStorageServiceSpy = jasmine.createSpyObj('IonicStorageService', {
+      get: Promise.resolve(contacts),
+    });
+    fakeNavController = new FakeNavController();
+    navControllerSpy = fakeNavController.createSpy();
     TestBed.configureTestingModule({
-      declarations: [ WalletsAgendaHomePage ],
-      imports: [IonicModule.forRoot()]
+      declarations: [WalletsAgendaHomePage, FakeTrackClickDirective],
+      imports: [IonicModule.forRoot(), TranslateModule.forRoot()],
+      providers: [
+        { provide: IonicStorageService, useValue: ionicStorageServiceSpy },
+        { provide: NavController, useValue: navControllerSpy },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(WalletsAgendaHomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    trackClickDirectiveHelper = new TrackClickDirectiveTestHelper(fixture);
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render empty state if not contacts', async () => {
+    ionicStorageServiceSpy.get.and.returnValue(null);
+    await component.ionViewWillEnter();
+    fixture.detectChanges();
+    const imgEl = fixture.debugElement.query(By.css('img.wa__empty-content__img'));
+    const titleEl = fixture.debugElement.query(By.css('ion-text.wa__empty-content__text__title'));
+    const subtitleEl = fixture.debugElement.query(By.css('ion-text.wa__empty-content__text__subtitle'));
+    expect(imgEl.attributes.src).toContain('assets/img/agenda/empty.svg');
+    expect(titleEl.nativeElement.innerHTML).toContain('contacts.home.empty.title');
+    expect(subtitleEl.nativeElement.innerHTML).toContain('contacts.home.empty.subtitle');
+  });
+
+  it('should render contacts', async () => {
+    await component.ionViewWillEnter();
+    fixture.detectChanges();
+    const imgEl = fixture.debugElement.query(By.css('img.wa__content__item__wrapper__img'));
+    const nameEl = fixture.debugElement.query(By.css('div.wa__content__item__wrapper__data__title > ion-text'));
+    expect(imgEl.attributes.src).toContain('assets/img/agenda/wallet.svg');
+    expect(nameEl.nativeElement.innerHTML).toContain(contacts[0].name);
+  });
+
+  it('should call trackEvent on trackService when ux_address_new button was clicked', () => {
+    const el = trackClickDirectiveHelper.getByElementByName('ion-button', 'ux_address_new');
+    const directive = trackClickDirectiveHelper.getDirective(el);
+    const spy = spyOn(directive, 'clickEvent');
+    el.nativeElement.click();
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 });
--- src/app/modules/profiles/wallets-agenda-home/wallets-agenda-home.page.ts
+++ src/app/modules/profiles/wallets-agenda-home/wallets-agenda-home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { IonicStorageService } from 'src/app/shared/services/ionic-storage/ionic-storage.service';
 
 @Component({
   selector: 'app-wallets-agenda-home',
@@ -7,29 +9,72 @@ import { Component, OnInit } from '@angular/core';
         <ion-buttons slot="start">
           <ion-back-button defaultHref=""></ion-back-button>
         </ion-buttons>
-        <ion-title>{{ 'profiles.wallets_agenda.header' | translate }}</ion-title>
+        <ion-title>{{ 'contacts.home.header' | translate }}</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content class="wa">
-      <div class="wa__content">
-        <img class="wa__content__img" src="/assets/img/profiles/wallets-agenda/empty.svg" />
-        <div class="wa__content__text">
-          <ion-text class="ux-font-text-xs wa__content__text__title">Aún no posees wallets en tu agenda.</ion-text>
-          <ion-text class="ux-font-text-xs wa__content__text__subtitle">¡Comienza ahora!</ion-text>
+      <div class="wa__empty-content" *ngIf="this.contacts.length === 0">
+        <img class="wa__empty-content__img" src="/assets/img/agenda/empty.svg" />
+        <div class="wa__empty-content__text">
+          <ion-text class="ux-font-text-xs wa__empty-content__text__title">{{
+            'contacts.home.empty.title' | translate
+          }}</ion-text>
+          <ion-text class="ux-font-text-xs wa__empty-content__text__subtitle">{{
+            'contacts.home.empty.subtitle' | translate
+          }}</ion-text>
+        </div>
+      </div>
+      <div class="wa__content" *ngIf="this.contacts.length > 0">
+        <div class="wa__content__item" lines="none" *ngFor="let contact of this.contacts">
+          <div class="wa__content__item__wrapper">
+            <img class="wa__content__item__wrapper__img" src="/assets/img/agenda/wallet.svg" />
+            <div class="wa__content__item__wrapper__data">
+              <div class="wa__content__item__wrapper__data__title">
+                <ion-text class="ux-font-text-lg">{{ contact.name }}</ion-text>
+                <app-token-network-badge
+                  *ngIf="contact.networks.length === 1"
+                  [blockchainName]="contact.networks[0]"
+                ></app-token-network-badge>
+              </div>
+              <div class="wa__content__item__wrapper__data__networks" *ngIf="contact.networks.length > 1">
+                <app-token-network-badge
+                  *ngFor="let network of contact.networks"
+                  [blockchainName]="network"
+                ></app-token-network-badge>
+              </div>
+              <div class="wa__content__item__wrapper__data__subtitle">
+                <ion-text class="ux-font-text-xs">{{ contact.address }}</ion-text>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </ion-content>
     <ion-footer>
       <div class="ion-padding">
-        <ion-button class="ux_button" appTrackClick name="ux_donations_donate" color="secondary" expand="block">
-          Agendar nueva wallet
+        <ion-button class="ux_button" appTrackClick (click)="create()" name="ux_address_new" color="secondary" expand="block">
+          {{ 'contacts.home.button' | translate }}
         </ion-button>
       </div>
     </ion-footer>`,
   styleUrls: ['./wallets-agenda-home.page.scss'],
 })
 export class WalletsAgendaHomePage implements OnInit {
-  constructor() {}
+  contacts = [];
+  constructor(private ionicService: IonicStorageService, private navController: NavController) {}
 
   ngOnInit() {}
+
+  async ionViewWillEnter() {
+    await this._getContacts();
+  }
+
+  async _getContacts() {
+    const contacts = await this.ionicService.get('contact_list')
+    this.contacts = contacts ? contacts : [];
+  }
+  
+  create(){
+    this.navController.navigateForward(['/contacts/register'])
+  }
 }
