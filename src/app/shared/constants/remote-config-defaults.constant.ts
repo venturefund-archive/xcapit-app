4f7070a277833c1c2a24fa43e046dc91bd92c367
Franco Schillage <franco@xcapit.com>
2023-01-11 16:07:06 -0300

    fix: Remove deprecated buy crypto feature flags

--- src/app/shared/constants/remote-config-defaults.constant.ts
+++ src/app/shared/constants/remote-config-defaults.constant.ts
@@ -1,8 +1,4 @@
 export const REMOTE_CONFIG_DEFAULTS = {
-  ff_buyCryptoHomeWalletButton: false,
-  ff_buyCriptoHomeCard: false,
-  ff_buyCryptoNewInvestmentFooter: false,
-  ff_buyCryptoAddAmountInvestmentFooter: false,
   ff_buyCrypto: false,
   ff_updateAppModal: false,
   referralsMenuUrl_referralPromotionCard: 'referrals/closed',
