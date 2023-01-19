ba8cd5b14dd2e7862914c9cbc44ca8cc3f765e69
Matias Genre <matiasgenre@xcapit.com>
2023-01-19 17:18:22 -0300

    fix(shared-ramps): add condition to hide status in investment

--- src/app/modules/fiat-ramps/shared-ramps/components/operation-status-chip/operation-status-chip.component.ts
+++ src/app/modules/fiat-ramps/shared-ramps/components/operation-status-chip/operation-status-chip.component.ts
@@ -2,13 +2,12 @@ import { Component, Input, OnInit } from '@angular/core';
 import { OPERATION_STATUS } from '../../constants/operation-status';
 import { OperationStatus } from '../../interfaces/operation-status.interface';
 
-
 @Component({
   selector: 'app-operation-status-chip',
   template: `
-    <ion-chip class="chip ion-no-padding ion-no-margin" [ngClass]="this.status.colorCssClass">
+    <ion-chip class="chip ion-no-padding ion-no-margin" *ngIf="this.status" [ngClass]="this.status.colorCssClass">
       <ion-text class="ux-font-num-subtitulo">
-        {{ ('fiat_ramps.operation_status.' + status.textToShow) | translate }}
+        {{ 'fiat_ramps.operation_status.' + status.textToShow | translate }}
       </ion-text>
     </ion-chip>
   `,
@@ -17,9 +16,9 @@ import { OperationStatus } from '../../interfaces/operation-status.interface';
 export class OperationStatusChipComponent implements OnInit {
   @Input() statusName: string;
   status: OperationStatus;
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    this.status = OPERATION_STATUS.find(s => s.name === this.statusName);
+    this.status = OPERATION_STATUS.find((s) => s.name === this.statusName);
   }
 }
