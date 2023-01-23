28356dba6bfe376fba37479407ce8b2b7928326a
Franco Schillage <franco@xcapit.com>
2023-01-23 12:47:20 -0300

    fix(fiat-ramps): Add network to default kripton price request

--- src/app/modules/fiat-ramps/shared-ramps/models/kripton-price/default-kripton-price.spec.ts
+++ src/app/modules/fiat-ramps/shared-ramps/models/kripton-price/default-kripton-price.spec.ts
@@ -1,12 +1,13 @@
-import { Coin } from "src/app/modules/wallets/shared-wallets/interfaces/coin.interface";
-import { FakeHttpClient } from "src/testing/fakes/fake-http.spec";
-import { ProviderPrice } from "../provider-price/provider-price";
-import { DefaultKriptonPrice } from "./default-kripton-price";
-
+import { Coin } from 'src/app/modules/wallets/shared-wallets/interfaces/coin.interface';
+import { ProviderPrice } from '../provider-price/provider-price';
+import { DefaultKriptonPrice } from './default-kripton-price';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('DefaultKriptonPrice', () => {
   let coinSpy: jasmine.SpyObj<Coin>;
   let kriptonPrice: ProviderPrice;
+  let fakeHttpClientSpy: jasmine.SpyObj<HttpClient>;
   const amountOutResponse = {
     data: {
       amount_out: '2',
@@ -15,18 +16,30 @@ describe('DefaultKriptonPrice', () => {
   };
 
   beforeEach(() => {
-    coinSpy = jasmine.createSpyObj('Coin', {}, { value: 'MATIC' });
-    kriptonPrice = new DefaultKriptonPrice('ars', coinSpy, new FakeHttpClient({}, amountOutResponse));
+    coinSpy = jasmine.createSpyObj('Coin', {}, { value: 'MATIC', network: 'MATIC' });
+    fakeHttpClientSpy = jasmine.createSpyObj('HttpClient', {
+      post: of(amountOutResponse),
+    });
+    kriptonPrice = new DefaultKriptonPrice('ars', coinSpy, fakeHttpClientSpy);
   });
 
   it('new', () => {
     expect(kriptonPrice).toBeTruthy();
   });
 
-
   it('value', async () => {
     const price = await kriptonPrice.value().toPromise();
 
     expect(price).toEqual(0.5);
+    expect(fakeHttpClientSpy.post).toHaveBeenCalledOnceWith(
+      'https://app.kriptonmarket.com/public/calculate_amount_out',
+      {
+        currency_in: 'ars',
+        amount_in: 1,
+        currency_out: coinSpy.value,
+        type: 'cash-in',
+        network_out: 'polygon',
+      }
+    );
   });
 });
