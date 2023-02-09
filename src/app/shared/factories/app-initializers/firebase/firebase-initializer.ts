d2c780d7769b8878e21728535b90f42d10bdc417
diegomeineri95 <diego@xcapit.com>
2023-02-09 11:20:25 -0300

    feat(firebase): initialize app without internet connection

--- src/app/shared/factories/app-initializers/firebase/firebase-initializer.ts
+++ src/app/shared/factories/app-initializers/firebase/firebase-initializer.ts
@@ -3,5 +3,5 @@ import { FirebaseService } from '../../../services/firebase/firebase.service';
 import { FirebaseRemoteConfig } from '../../../models/firebase-remote-config/firebase-remote-config';
 
 export function firebaseInitializer(remoteConfig: RemoteConfigService, firebase: FirebaseService) {
-  return () => firebase.init().then(() => remoteConfig.initialize(new FirebaseRemoteConfig(firebase.getApp())));
+  return () => firebase.init().then(() => remoteConfig.initialize(new FirebaseRemoteConfig(firebase.getApp()))).catch(()=>{});
 }
