6481f63b4652ef83125edbcd7df4f9e6604ca48a
Matias Genre <matiasgenre@xcapit.com>
2023-03-23 16:27:20 -0300

    fix(tickets): remove support warranty category

--- src/app/modules/tickets/shared-tickets/constants/ticket-categories.ts
+++ src/app/modules/tickets/shared-tickets/constants/ticket-categories.ts
@@ -1,6 +1,5 @@
 export const TICKET_CATEGORIES = [
   { name: 'Mi cuenta/Registro', value: 'tickets.categories.my_account' },
-  { name: 'Garantía', value: 'tickets.categories.warranty' },
   { name: 'Vincular mi cuenta de Binance', value: 'tickets.categories.link_binance_account' },
   { name: 'Comprar criptomonedas', value: 'tickets.categories.buy_crypto' },
   { name: 'Estrategias de inversión', value: 'tickets.categories.investment_strategies' },
@@ -8,7 +7,7 @@ export const TICKET_CATEGORIES = [
   { name: 'Wallet', value: 'tickets.categories.wallet' },
   { name: 'NFT', value: 'tickets.categories.nft' },
   { name: 'Otros', value: 'tickets.categories.others' },
-  { name: 'Referidos', value: 'tickets.categories.referrals'},
-  { name: 'Transacciones', value: 'tickets.categories.transactions'},
-  { name: 'Eliminar mis datos de Kripton', value: 'tickets.categories.kripton_data'},
+  { name: 'Referidos', value: 'tickets.categories.referrals' },
+  { name: 'Transacciones', value: 'tickets.categories.transactions' },
+  { name: 'Eliminar mis datos de Kripton', value: 'tickets.categories.kripton_data' },
 ];
