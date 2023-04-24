f5eefd671a0773f7e8629e4cc00860f642688318
rassyu <andres@xcapit.com>
2023-04-24 14:53:05 -0300

    feat(backup.page): WIP add basic logic

--- src/app/modules/profiles/backup/backup.page.ts
+++ src/app/modules/profiles/backup/backup.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { IonicStorageService } from 'src/app/shared/services/ionic-storage/ionic-storage.service';
 
 @Component({
   selector: 'app-backup',
@@ -31,9 +32,14 @@ import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
               class="b__item__toggle toggle ux-toggle ion-no-padding"
               mode="ios"
               slot="end"
+              (click)="this.activateBackup()"
+              [disabled]="this.alreadyHasBackup"
+              [checked]="this.alreadyHasBackup"
             ></ion-toggle>
           </ion-item>
         </div>
+        <ion-button (click)="this.mockStorageActivation()">Toggle storage state</ion-button>
+        <ion-button (click)="this.mockStorageSet()">Set mockStorageSet</ion-button>
       </form>
     </ion-content>`,
   styleUrls: ['./backup.page.scss'],
@@ -42,7 +48,39 @@ export class BackupPage implements OnInit {
   form: UntypedFormGroup = this.formBuilder.group({
     backup: [false, []],
   });
-  constructor(private formBuilder: UntypedFormBuilder) {}
+  alreadyHasBackup: boolean;
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+    private ionicStorageService: IonicStorageService,) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.checkToggleStatus();
+  }
+
+  activateBackup() {
+    // if (!this.alreadyHasBackup){
+      console.log('it works')
+      // this.alreadyHasBackup = true;
+    // }
+  }
+
+  async checkToggleStatus() {
+    this.alreadyHasBackup = await this.ionicStorageService.get('wallet_backup')
+  }
+
+  async mockStorageActivation() {
+    const key = await this.ionicStorageService.get('wallet_backup')
+    if (key === true) {
+      console.log('changing storage value to false...')
+      this.ionicStorageService.set('wallet_backup', false)
+    } else {
+      console.log('changing storage value to true...')
+      this.ionicStorageService.set('wallet_backup', true)
+    }
+    this.checkToggleStatus()
+  }
+
+  mockStorageSet() {
+    this.ionicStorageService.set('wallet_backup', false)
+  }
 }
