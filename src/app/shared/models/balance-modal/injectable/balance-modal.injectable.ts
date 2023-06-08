1c50e6fae056cc9639d5be4d07017e417fba23ed
Martin317 <martin@xcapit.com>
2023-06-08 17:29:51 -0300

    Working...

--- src/app/shared/models/balance-modal/injectable/balance-modal.injectable.ts
+++ src/app/shared/models/balance-modal/injectable/balance-modal.injectable.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 import BalanceModal from '../balance-modal';
+import { TranslateService } from '@ngx-translate/core';
+import { Token } from '../../../../modules/swaps/shared-swaps/models/token/token';
 
 @Injectable({ providedIn: 'root' })
 export default class BalanceModalInjectable {
-    constructor(
-        private readonly _aModalController: ModalController | FakeModalController,
-        private readonly _aTranslateService: TranslateService | FakeTranslateService
-    ){}
+  constructor(
+    private readonly _aModalController: ModalController,
+    private readonly _aTranslateService: TranslateService
+  ) {}
   public create(
     _aToken: Token,
     _aDescriptionKey: string,
     _aPrimaryButtonKey: string,
     _aSecondaryButtonKey: string,
-    _aModalController: ModalController,
-    _aTranslateService: TranslateService
+    _aModalController: ModalController = this._aModalController,
+    _aTranslateService: TranslateService = this._aTranslateService
   ): BalanceModal {
-    return new BalanceModal(null, null, null, null, null, null);
+    return new BalanceModal(
+      _aToken,
+      _aDescriptionKey,
+      _aPrimaryButtonKey,
+      _aSecondaryButtonKey,
+      _aModalController,
+      _aTranslateService
+    );
   }
 }
