fdcabaee537aba9412b40c67da735b78e17ada4b
Martin317 <martin@xcapit.com>
2023-06-08 17:30:05 -0300

    Working...

--- src/app/shared/models/balance-modal/injectable/balance-modal.injectable.spec.ts
+++ src/app/shared/models/balance-modal/injectable/balance-modal.injectable.spec.ts
@@ -1,17 +1,15 @@
-import { TestBed } from "@angular/core/testing";
-import BalanceModalInjectable from "./balance-modal.injectable";
-import BalanceModal from "../balance-modal";
+import { TestBed } from '@angular/core/testing';
+import BalanceModalInjectable from './balance-modal.injectable';
+import BalanceModal from '../balance-modal';
 
-
-fdescribe('BalanceModalInjectable', ()=>{
-
+fdescribe('BalanceModalInjectable', () => {
   let service: BalanceModalInjectable;
 
-  beforeEach(()=>{
+  beforeEach(() => {
     service = TestBed.inject(BalanceModalInjectable);
-  })
+  });
 
-  it('create',()=>{
-    expect(service.create()).toBeInstanceOf(BalanceModal);
-  })
-})
+  it('create', () => {
+    expect(service.create(null, null, null, null, null, null)).toBeInstanceOf(BalanceModal);
+  });
+});
