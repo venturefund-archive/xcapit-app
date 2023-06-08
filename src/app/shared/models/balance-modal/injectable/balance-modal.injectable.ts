0289cbee324fc95bf6c1a34d2abb5a21ce47f51d
Matias Bruno <matiasbruno97@gmail.com>
2023-06-08 17:28:16 -0300

    Working...

--- src/app/shared/models/balance-modal/injectable/balance-modal.injectable.ts
+++ src/app/shared/models/balance-modal/injectable/balance-modal.injectable.ts
@@ -1,10 +1,20 @@
-import { Injectable } from "@angular/core";
-import BalanceModal from "../balance-modal";
+import { Injectable } from '@angular/core';
+import BalanceModal from '../balance-modal';
 
 @Injectable({ providedIn: 'root' })
 export default class BalanceModalInjectable {
-    public create(): BalanceModal {
-      return new BalanceModal(null,null,null,null,null,null);
-    }
-
+    constructor(
+        private readonly _aModalController: ModalController | FakeModalController,
+        private readonly _aTranslateService: TranslateService | FakeTranslateService
+    ){}
+  public create(
+    _aToken: Token,
+    _aDescriptionKey: string,
+    _aPrimaryButtonKey: string,
+    _aSecondaryButtonKey: string,
+    _aModalController: ModalController,
+    _aTranslateService: TranslateService
+  ): BalanceModal {
+    return new BalanceModal(null, null, null, null, null, null);
+  }
 }
