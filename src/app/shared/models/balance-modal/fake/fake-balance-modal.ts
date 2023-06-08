179ef7397a7782e09b986a63a1b4932e622c5324
Matias Bruno <matiasbruno97@gmail.com>
2023-06-08 17:53:56 -0300

    Working...

--- src/app/shared/models/balance-modal/fake/fake-balance-modal.ts
+++ src/app/shared/models/balance-modal/fake/fake-balance-modal.ts
@@ -1,7 +1,10 @@
 import { BalanceModal } from '../balance-modal.interface';
 
 export class FakeBalanceModal implements BalanceModal {
+  public calls = 0;
+
   show(): Promise<void> {
+    this.calls++;
     return Promise.resolve();
   }
   onDidDismiss(): Promise<any> {
