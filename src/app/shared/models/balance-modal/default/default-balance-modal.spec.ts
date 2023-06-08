88dceb888050cf2304aa29eb1f6e45550b69aed5
Matias Bruno <matiasbruno97@gmail.com>
2023-06-08 17:41:12 -0300

    Working...

--- src/app/shared/models/balance-modal/default/default-balance-modal.spec.ts
+++ src/app/shared/models/balance-modal/default/default-balance-modal.spec.ts
@@ -1,1 +1,43 @@
 
+import { rawMATICData } from 'src/app/modules/swaps/shared-swaps/models/fixtures/raw-tokens-data';
+import { DefaultToken} from 'src/app/modules/swaps/shared-swaps/models/token/token';
+
+class FakeModalController {
+  create(): Promise<any> {
+    return Promise.resolve({ present: () => Promise.resolve() , onDidDismiss:() => ({ role: 'closed', data: 'aData'})});
+  }
+}
+class FakeTranslateService {
+  instant(aTextToTranslate: string, aParams: Object): string {
+    return 'translatedText';
+  }
+}
+
+
+fdescribe('DefaultBalanceModal', () => {
+  let DefaultBalanceModal: DefaultBalanceModal;
+
+  beforeEach(() => {
+    DefaultBalanceModal = new DefaultBalanceModal(
+      new DefaultToken(rawMATICData),
+      'text',
+      'primaryButtonText',
+      'secondaryButtonText',
+      new FakeModalController(),
+      new FakeTranslateService()
+    );
+  });
+
+  it('new', () => {
+    expect(DefaultBalanceModal).toBeTruthy();
+  });
+
+  it('show', async () => {
+    await expectAsync(DefaultBalanceModal.show()).toBeResolved();
+  });
+
+  it('onDidDismisss', async () => {
+    await DefaultBalanceModal.show();
+    expect((await DefaultBalanceModal.onDidDismiss()).role).toEqual('closed');
+  });
+});
