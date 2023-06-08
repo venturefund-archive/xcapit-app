e50c6081ad75f632364724d9d41d4890a997e75c
Martin317 <martin@xcapit.com>
2023-06-08 17:33:57 -0300

    Working...

--- src/app/shared/models/balance-modal/injectable/balance-modal.injectable.spec.ts
+++ src/app/shared/models/balance-modal/injectable/balance-modal.injectable.spec.ts
@@ -1,11 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 import BalanceModalInjectable from './balance-modal.injectable';
 import BalanceModal from '../balance-modal';
+import { TranslateModule } from '@ngx-translate/core';
+import { IonicStorageService } from '../../../services/ionic-storage/ionic-storage.service';
+import { ModalController } from '@ionic/angular';
+import { FakeModalController } from '../../../../../testing/fakes/modal-controller.fake.spec';
 
 fdescribe('BalanceModalInjectable', () => {
   let service: BalanceModalInjectable;
+  let modalControllerSpy: jasmine.SpyObj<ModalController>;
+  let fakeModalController: FakeModalController;
 
   beforeEach(() => {
+    fakeModalController = new FakeModalController();
+    modalControllerSpy = fakeModalController.createSpy();
+    TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot()],
+      providers: [{ provide: ModalController, useValue: modalControllerSpy }],
+    }).compileComponents();
+
     service = TestBed.inject(BalanceModalInjectable);
   });
 
