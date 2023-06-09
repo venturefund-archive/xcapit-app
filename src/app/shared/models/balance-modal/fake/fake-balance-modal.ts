14a3903e333522ad3415c43143640cb86d93a8d9
Martin317 <martin@xcapit.com>
2023-06-09 15:31:41 -0300

    Working...

--- src/app/shared/models/balance-modal/fake/fake-balance-modal.ts
+++ src/app/shared/models/balance-modal/fake/fake-balance-modal.ts
@@ -3,7 +3,7 @@ import { BalanceModal } from '../balance-modal.interface';
 export class FakeBalanceModal implements BalanceModal {
   public calls = 0;
 
-  show(): Promise<void> {
+  show(config = {}): Promise<void> {
     this.calls++;
     return Promise.resolve();
   }
