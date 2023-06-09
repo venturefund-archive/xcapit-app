c5f2764d0902a43254fcbf3f12ca5f5caba3b05d
Matias Bruno <matiasbruno97@gmail.com>
2023-06-09 11:23:08 -0300

    Working...

--- src/app/modules/wallets/send/send-detail/send-detail.page.ts
+++ src/app/modules/wallets/send/send-detail/send-detail.page.ts
@@ -196,7 +196,6 @@ export class SendDetailPage {
     await this.setTokenDetail();
     this.getPrices();
     await this.tokenBalances();
-    this.openModalBalance();
   }
 
   setFormData(amount: string) {
