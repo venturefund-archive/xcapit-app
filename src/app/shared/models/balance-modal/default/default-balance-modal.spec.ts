7c9bcffa693a64e80b128f41b64f47a0116512d1
Martin317 <martin@xcapit.com>
2023-06-09 11:11:57 -0300

    Working...

--- src/app/shared/models/balance-modal/default/default-balance-modal.spec.ts
+++ src/app/shared/models/balance-modal/default/default-balance-modal.spec.ts
@@ -5,7 +5,7 @@ import { BalanceModal } from '../balance-modal.interface';
 import { FakeModalController } from '../../modal-controller/fake/fake-modal-controller';
 import { FakeTranslateService } from '../../translate-service/fake/fake-translate-service';
 
-describe('DefaultBalanceModal', () => {
+fdescribe('DefaultBalanceModal', () => {
   let defaultBalanceModal: BalanceModal;
 
   beforeEach(() => {
