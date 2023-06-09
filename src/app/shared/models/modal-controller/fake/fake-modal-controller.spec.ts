2ec070ab465549feda6fe598ab636921437f0bc1
Matias Bruno <matiasbruno97@gmail.com>
2023-06-09 11:05:28 -0300

    Working...

--- src/app/shared/models/modal-controller/fake/fake-modal-controller.spec.ts
+++ src/app/shared/models/modal-controller/fake/fake-modal-controller.spec.ts
@@ -14,4 +14,14 @@ fdescribe('FakeModalController', () => {
   it('create', async () => {
     await expectAsync(fakeModalController.create()).toBeResolved();
   });
+
+  it('present', async () => {
+    const controller = await fakeModalController.create();
+    await expectAsync(controller.present()).toBeResolved();
+  });
+
+  it('onDidDismiss', async () => {
+    const controller = await fakeModalController.create();
+    expect((await controller.onDidDismiss()).role).toEqual('closed');
+  });
 });
