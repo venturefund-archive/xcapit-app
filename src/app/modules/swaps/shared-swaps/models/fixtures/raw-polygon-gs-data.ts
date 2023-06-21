300351b1b59d564e7f03b06276503c00a5c7403f
Martin317 <martin@xcapit.com>
2023-06-21 12:06:18 -0300

    fix: Update polygon gas station

--- src/app/modules/swaps/shared-swaps/models/fixtures/raw-polygon-gs-data.ts
+++ src/app/modules/swaps/shared-swaps/models/fixtures/raw-polygon-gs-data.ts
@@ -1,4 +1,16 @@
-import { RawPolygonGS } from "../polygon-gas-price/polygon-gas-price";
+import { RawPolygonGS } from '../polygon-gas-price/polygon-gas-price';
 
-
-export const rawPolygonGasStation: RawPolygonGS = {"safeLow":{"maxPriorityFee":33.664479934266666,"maxFee":35.494281265266665},"standard":{"maxPriorityFee":34.10264011906666,"maxFee":35.93244145006666},"fast":{"maxPriorityFee":35.6459197674,"maxFee":37.4757210984},"estimatedBaseFee":1.829801331,"blockTime":2,"blockNumber":30375091};
+export const rawPolygonGasStation: RawPolygonGS = {
+  safeLow: { maxPriorityFee: 33.664479934266666, maxFee: 35.494281265266665 },
+  standard: {
+    maxPriorityFee: 30,
+    maxFee: '178.885791131',
+  },
+  fast: {
+    maxPriorityFee: '38.447176140',
+    maxFee: '187.332967271',
+  },
+  estimatedBaseFee: 1.829801331,
+  blockTime: 2,
+  blockNumber: 30375091,
+};
--- src/app/modules/swaps/shared-swaps/models/polygon-gas-price/polygon-gas-price.ts
+++ src/app/modules/swaps/shared-swaps/models/polygon-gas-price/polygon-gas-price.ts
@@ -6,51 +6,53 @@ import { Blockchain } from '../blockchain/blockchain';
 import { GasPrice } from '../gas-price/gas-price';
 import { BigNumberOf } from './big-number-of';
 
-
 export type RawGSFee = {
-  maxPriorityFee: number,
-  maxFee: number
+  maxPriorityFee: string | number;
+  maxFee: string | number;
 };
-
 
 export type RawPolygonGS = {
   safeLow: RawGSFee;
   standard: RawGSFee;
   fast: RawGSFee;
-  estimatedBaseFee: number;
+  estimatedBaseFee: string | number;
   blockTime: number;
   blockNumber: number;
-}
-
+};
 
 export class PolygonGasPrice implements GasPrice {
-
   private readonly _url = environment.POLYGON_GAS_STATION;
 
-  constructor(private _aBlockchain: Blockchain, private _httpClient: HttpClient | FakeHttpClient) { }
+  constructor(private _aBlockchain: Blockchain, private _httpClient: HttpClient | FakeHttpClient) {}
 
   async safeLow(): Promise<AmountOf> {
     return new AmountOf(
-      this._bigNumberOf((await this._gasData()).safeLow.maxFee).value().toString(),
+      this._bigNumberOf((await this._gasData()).safeLow.maxFee)
+        .value()
+        .toString(),
       this._aBlockchain.nativeToken()
     );
   }
 
   async standard(): Promise<AmountOf> {
     return new AmountOf(
-      this._bigNumberOf((await this._gasData()).standard.maxFee).value().toString(),
+      this._bigNumberOf((await this._gasData()).standard.maxFee)
+        .value()
+        .toString(),
       this._aBlockchain.nativeToken()
     );
   }
 
   async fast(): Promise<AmountOf> {
     return new AmountOf(
-      this._bigNumberOf((await this._gasData()).fast.maxFee).value().toString(),
+      this._bigNumberOf((await this._gasData()).fast.maxFee)
+        .value()
+        .toString(),
       this._aBlockchain.nativeToken()
     );
   }
 
-  private _bigNumberOf(aGweiAmount: number) {
+  private _bigNumberOf(aGweiAmount: number | string) {
     return new BigNumberOf(aGweiAmount);
   }
 
