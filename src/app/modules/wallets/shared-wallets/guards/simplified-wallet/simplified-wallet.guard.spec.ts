5f9a876ca616c8c650b1f21a976c278a73d81518
Matias Bruno <matiasbruno97@gmail.com>
2023-10-05 17:42:39 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/guards/simplified-wallet/simplified-wallet.guard.spec.ts
+++ src/app/modules/wallets/shared-wallets/guards/simplified-wallet/simplified-wallet.guard.spec.ts
@@ -27,18 +27,16 @@ fdescribe('SimplifiedWalletGuard', () => {
         { provide: NavController, useValue: navControllerSpy },
       ],
     });
-    simplifiedWalletGuard = TestBed.inject(SimplifiedWalletGuard);
+    simplifiedWalletGuard = TestBed.inject(SimplifiedWalletGuardService);
   });
 
   it('should create', () => {
     expect(simplifiedWalletGuard).toBeTruthy();
   });
 
-  fit('should navigate to home wallet when the user is simplified Wallet', async () => {
-    
-    expect(SimplifiedWalletGuard(null,null)).toBeFalse();
-    // expect(navControllerSpy.navigateRoot).toHaveBeenCalledOnceWith(['/simplified-home-wallet']);
-  
+  it('should navigate to home wallet when the user is simplified Wallet', async () => {
+    expect(await simplifiedWalletGuard.canActivate()).toBeFalse();
+    expect(navControllerSpy.navigateRoot).toHaveBeenCalledOnceWith(['/simplified-home-wallet']);
   });
 
   it('should not navigate to home wallet when the user is simplified wallet', async () => {
