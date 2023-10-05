e99143e537303cca740b5f7b3bb3869d028e2a62
Matias Bruno <matiasbruno97@gmail.com>
2023-10-05 17:29:26 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/guards/simplified-wallet/simplified-wallet.guard.ts
+++ src/app/modules/wallets/shared-wallets/guards/simplified-wallet/simplified-wallet.guard.ts
@@ -1,34 +1,29 @@
-import { inject } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { SimplifiedWallet } from '../../models/simplified-wallet/simplified-wallet';
 import { IonicStorageService } from 'src/app/shared/services/ionic-storage/ionic-storage.service';
 
-export const simplifiedWalletGuard: CanActivateFn = async () => {
-  const storage = inject(IonicStorageService);
-  const navController = inject(NavController);
-  const hasSimplifiedWallet = await new SimplifiedWallet(storage).value();
+@Injectable({
+  providedIn: 'root',
+})
+export class SimplifiedWalletGuardService  {
+  constructor(private _aStorage: IonicStorageService, private navController: NavController) {}
 
-  if (hasSimplifiedWallet) return await navController.navigateRoot(['/simplified-home-wallet']);
+  async canActivate(): Promise<boolean> {
+    const hasSimplifiedWallet = await new SimplifiedWallet(this._aStorage).value();
 
-  return !hasSimplifiedWallet;
+    if (hasSimplifiedWallet) await this.redirectToPage();
+
+    return !hasSimplifiedWallet;
+  }
+
+  async redirectToPage() {
+    return await this.navController.navigateRoot(['/simplified-home-wallet']);
+  }
+}
+
+export const SimplifiedWalletGuard: CanActivateFn = async () => {
+  return inject(SimplifiedWalletGuard).canActivate();
 };
 
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class SimplifiedWalletGuard implements CanActivate {
-//   constructor(private _aStorage: IonicStorageService, private navController: NavController) {}
-//
-//   async canActivate(): Promise<boolean> {
-//     const hasSimplifiedWallet = await new SimplifiedWallet(this._aStorage).value();
-//
-//     if (hasSimplifiedWallet) await this.redirectToPage();
-//
-//     return !hasSimplifiedWallet;
-//   }
-//
-//   async redirectToPage() {
-//     return await this.navController.navigateRoot(['/simplified-home-wallet']);
-//   }
-// }
