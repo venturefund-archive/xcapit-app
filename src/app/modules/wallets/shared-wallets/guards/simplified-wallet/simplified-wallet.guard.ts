43fcc99c294a29061c6ec336bd55a5838d9a7d5f
Matias Genre <matiasgenre@xcapit.com>
2023-10-05 17:21:44 -0300

    Working...

--- src/app/modules/wallets/shared-wallets/guards/simplified-wallet/simplified-wallet.guard.ts
+++ src/app/modules/wallets/shared-wallets/guards/simplified-wallet/simplified-wallet.guard.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, CanActivateFn } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { SimplifiedWallet } from '../../models/simplified-wallet/simplified-wallet';
 import { IonicStorageService } from 'src/app/shared/services/ionic-storage/ionic-storage.service';
+
+export const simplifiedWalletGuard: CanActivateFn = async (storage) => {
+  
+    const hasSimplifiedWallet = await new SimplifiedWallet(this._aStorage).value();
+
+    if (hasSimplifiedWallet) await this.redirectToPage();
+
+    return !hasSimplifiedWallet;
+  
+}
 
 @Injectable({
   providedIn: 'root',
