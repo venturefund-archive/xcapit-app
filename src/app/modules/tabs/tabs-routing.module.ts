5a88677bef2e0961643f721942ae99c89ab0f00a
Martin317 <martin@xcapit.com>
2023-10-05 17:25:44 -0300

    Working...

--- src/app/modules/tabs/tabs-routing.module.ts
+++ src/app/modules/tabs/tabs-routing.module.ts
@@ -5,10 +5,7 @@ import { HasWallet } from 'src/app/shared/guards/has-wallet/has-wallet';
 import { IntroductionCompletedGuard } from '../financial-education/shared-financial-education/guards/introduction-completed';
 import { AuthGuard } from '../users/shared-users/guards/auth/auth.guard';
 import { NewLogin } from '../users/shared-users/guards/new-login/new-login.guard';
-import {
-  simplifiedWalletGuard,
-  SimplifiedWalletGuard,
-} from '../wallets/shared-wallets/guards/simplified-wallet/simplified-wallet.guard';
+import { simplifiedWalletGuard } from '../wallets/shared-wallets/guards/simplified-wallet/simplified-wallet.guard';
 
 const routes: Routes = [
   {
