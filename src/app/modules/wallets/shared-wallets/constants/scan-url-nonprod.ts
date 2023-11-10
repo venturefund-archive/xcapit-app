eb1838bf88155741892adf001593d1817fa6b66e
Andrés Matterson <andres@xcapit.com>
2023-11-10 14:14:06 -0300

    feat(scan-url-nonprod): scan url change

--- src/app/modules/wallets/shared-wallets/constants/scan-url-nonprod.ts
+++ src/app/modules/wallets/shared-wallets/constants/scan-url-nonprod.ts
@@ -1,6 +1,6 @@
 export const NONPROD_SCAN_URLS = {
   MATIC: 'https://mumbai.polygonscan.com/',
-  ERC20: 'https://kovan.etherscan.io/',
+  ERC20: 'https://sepolia.etherscan.io/',
   BSC_BEP20: 'https://testnet.bscscan.com/',
   RSK: 'https://explorer.testnet.rsk.co/',
 };
